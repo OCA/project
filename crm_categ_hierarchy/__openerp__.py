e336cb6f8adc7304d5c53609fd9861c672902d70
Daniel Reis <dreis.pt@hotmail.com>
2012-11-22 13:26:40 +0000

    ADD: hierarchy structure to categories used by issues and tasks

---
+++ crm_categ_hierarchy/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2012 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Category hierarchical structure',
+    'version': '1',
+    "category": "Customer Relationship Management",
+    'description': """Add a tree-like structure to CRM categories""",
+    'author': 'Daniel Reis',
+    'website': 'daniel.reis@securitas.pt',
+    'depends': ['crm'],
+    'update_xml': ['crm_categ_view.xml'],
+    'installable': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ crm_categ_hierarchy/crm_categ.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2012 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class crm_case_categ(osv.osv):
+    _inherit = "crm.case.categ"
+    _order   = 'parent_id, name' 
+    
+    def name_get(self, cr, uid, ids, context=None):
+        res = []
+        for row in self.read(cr, uid, ids, ['name','parent_id'], context=context):
+            parent = row['parent_id'] and (row['parent_id'][1]+' / ') or ''
+            res.append((row['id'], parent + row['name']))
+        return res
+        
+    def _name_get_fnc(self, cr, uid, ids, prop, unknow_none, context=None):
+        res = self.name_get(cr, uid, ids, context=context)
+        return dict(res)
+
+    _columns = {
+        'note': fields.text('Description', size=64),
+        'parent_id': fields.many2one('crm.case.categ', 'Parent'),
+        'child_ids': fields.many2many('crm.case.categ', 'crm_case_categ_parent_rel', 'parent_id', 'categ_id', 'Child Categories'),
+        'complete_name': fields.function(_name_get_fnc, method=True, type="char", string='Name'),
+        'code': fields.char('Code', size=10),
+    }
+    _constraints = [
+        (osv.osv._check_recursion, 'Error! Cannot create recursive cycle.', ['parent_id'])
+    ]
+
+crm_case_categ()
+    
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
