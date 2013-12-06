01bc0bc7911c05d364814f23f2936f3f2d3a06a8
Sebastien Beau <sebastien.beau@akretion.com>
2013-12-06 20:19:33 +0100

    [MERGE] merge change done in the branch https://code.launchpad.net/~akretion-team/+junk/advanced-project-management-70. We plan to remove this module from our branch

--- __unported__/project_functional_blocks/project.py
+++ __unported__/project_functional_blocks/project.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 Akretion LDTA (<http://www.akretion.com>).
+#    
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 from osv import fields, osv
+
 
 class project_functional_block(osv.osv):
     _name = 'project.functional_block'
@@ -17,13 +39,14 @@ class project_functional_block(osv.osv):
 
     _columns = {
         'name': fields.char('Name', size=64, required=True, translate=True),
+        'sequence': fields.integer('Sequence'),
         'description': fields.text('Description', translate=True),
         'parent_id': fields.many2one('project.functional_block', 'Parent block', select=True),
         'child_id': fields.one2many('project.functional_block', 'parent_id', 'Child block'),
         'complete_name': fields.function(_name_get_fnc, method=True, type='char', string='Name'),
         'code': fields.char('Code', size=10),
     }
-    _order = 'parent_id,name'
+    _order = 'parent_id, sequence, name'
 
     
 project_functional_block()
