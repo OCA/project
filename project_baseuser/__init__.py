61b8fbcd4c311bc88b23866e8ab06502e552a417
Daniel Reis <dgreis@sapo.pt>
2013-12-16 09:26:32 +0000

    Fix encoding -> coding

--- project_baseuser/__init__.py
+++ project_baseuser/__init__.py
@@ -1,3 +1,2 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 pass
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- project_issue_baseuser/__init__.py
+++ project_issue_baseuser/__init__.py
@@ -1,3 +1,3 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import project_issue
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
