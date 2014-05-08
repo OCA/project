598354fc0b85a74385bda88ebd3335c487e9925b
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2014-05-08 14:43:09 +0200

    [IMP] Add the project/account department on analytic line. You can now group and search on both user's or account's related department

--- analytic_department/analytic.py
+++ analytic_department/analytic.py
@@ -5,12 +5,26 @@ from openerp.osv import fields, orm
 class AnalyticAccount(orm.Model):
     _inherit = "account.analytic.account"
     _columns = {
-        'department_id': fields.many2one('hr.department', 'Department'),
+        'department_id': fields.many2one(
+            'hr.department', 
+            'Department'),
     }
 
 
 class AnalyticLine(orm.Model):
     _inherit = "account.analytic.line"
     _columns = {
-        'department_id': fields.many2one('hr.department', 'Department'),
+        'department_id': fields.many2one(
+            'hr.department', 
+            'Department',
+            help="User's related department"),
+        'account_department_id': fields.related(
+            'account_id',
+            'department_id',
+            type='many2one',
+            relation='hr.department',
+            string='Account Department',
+            store=True,
+            readonly=True
+            help="Account's related department"),
     }
