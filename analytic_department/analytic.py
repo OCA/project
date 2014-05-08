0a9dca72676600ead631a51e5773783a1911472c
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2014-05-08 15:07:24 +0200

    [IMP] Add department_id in analytic form view + add default value for user's employee department field

--- analytic_department/analytic.py
+++ analytic_department/analytic.py
@@ -13,6 +13,19 @@ class AnalyticAccount(orm.Model):
 
 class AnalyticLine(orm.Model):
     _inherit = "account.analytic.line"
+
+    def _get_department(self, cr, uid, ids, context=None):
+        employee_obj = self.pool['hr.employee']
+        department_id = False
+        employee_ids = employee_obj.search(cr, uid, [('user_id','=', uid)])
+        if employee_ids:
+            employee = employee_obj.browse(cr, uid, 
+                                                employee_ids[0], 
+                                                context=context)
+            if employee.department_id:
+                department_id = employee.department_id.id
+        return department_id
+
     _columns = {
         'department_id': fields.many2one(
             'hr.department', 
@@ -28,3 +41,9 @@ class AnalyticLine(orm.Model):
             readonly=True
             help="Account's related department"),
     }
+
+    _defaults = {
+        'department_id': _get_department,
+    }
+
+       
