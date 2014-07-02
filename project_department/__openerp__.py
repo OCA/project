3a2b2484d7f83bc5c7f38ff90567bf3c1c1a3a0b
Daniel Reis <dgreis@sapo.pt>
2014-07-02 15:23:15 +0100

    Port project_department to v8

---
+++ project_department/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Project Department Categorization",
+    "version": "1.0",
+    "author": "Camptocamp, Daniel Reis",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Projects & Services",
+    "description": """\
+Add Department to Projects and task to corresponding tree, search and form
+views.
+""",
+    "website": "http://camptocamp.com",
+    "depends": ["project", "analytic_department"],
+    "data": ["project_view.xml"],
+    "auto_install": True,
+    "installable": True,
+}
