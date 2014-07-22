22614c3234909da8fada1610c76207e17408d87c
Daniel Reis <dgreis@sapo.pt>
2014-07-22 10:45:02 +0100

    Split analytic_department into analytic_base_department to isolate dependency on account

--- analytic_base_department/__openerp__.py
+++ analytic_base_department/__openerp__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Analytic Account Department Categorization",
-    "version": "1.0",
+    "name": "Analytic Department Categorization",
+    "version": "2.0",
     "author": "Camptocamp, Daniel Reis",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
     "description": """\
-Add Department to Analytic Account and Analytical Line models, and to
-corresponding tree, search and form  views.
+Add Department to Analytic Account.
+No required dependency on Accounting modules.
 """,
     "website": "http://camptocamp.com",
-    "depends": ["account", "hr"],
+    "depends": ["analytic", "hr"],
     "data": ["analytic_view.xml"],
     "installable": True,
 }
