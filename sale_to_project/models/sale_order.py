dfbb68a2689c23492440460ca5aeb55e197d5efc
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-05-21 11:48:35 +0200

    Wizard that generates a contract from a sales order

---
+++ sale_to_project/models/sale_order.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Authors: Guewen Baconnier
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+from openerp import models, api, exceptions, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_contract_line(self, line):
+        return {
+            'product_id': line.product_id.id,
+            'name': line.name,
+            'quantity': line.product_uom_qty,
+            'uom_id': line.product_uom.id,
+            'price_unit': line.price_unit,
+        }
+
+    @api.multi
+    def _prepare_contract(self, lines):
+        values = {'name': self.name,
+                  'partner_id': self.partner_id.id,
+                  'manager_id': self.user_id.id,
+                  'code': self.name,
+                  'company_id': self.company_id.id,
+                  'date_start': self.date_order,
+                  'recurring_invoices': True,
+                  'type': 'contract',
+                  }
+        values['recurring_invoice_line_ids'] = [
+            (0, 0, self._prepare_contract_line(line))
+            for line in lines
+        ]
+        return values
+
+    @api.multi
+    def create_contract(self, lines=None):
+        self.ensure_one()
+        if lines and any(line.order_id != self for line in lines):
+            raise exceptions.Warning(
+                _('Cannot create a contract for lines of different orders')
+            )
+        elif not lines:
+            lines = self.order_line
+        contract_model = self.env['account.analytic.account']
+        return contract_model.create(self._prepare_contract(lines))
---
+++ sale_to_project/wizard/sale_to_project.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Authors: Guewen Baconnier
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+from openerp import models, fields, api, _
+
+
+class SaleToProject(models.TransientModel):
+    _name = 'sale.to.project'
+    _description = 'Create Contract from Sales Order'
+
+    create_contract_method = fields.Selection(
+        selection=[('all', 'Create Contract from all the lines'),
+                   ('lines', 'Create Contract from a selection of lines')],
+        string='Method',
+    )
+
+    @api.multi
+    def open_lines(self):
+        sale_ids = self.env.context.get('active_ids')
+        action_xmlid = 'sale.action_order_line_tree2'
+        action = self.env.ref(action_xmlid).read()[0]
+        action['context'] = {
+            'search_default_uninvoiced': 1,
+            'search_default_order_id': sale_ids[0] if sale_ids else False,
+        }
+        return action
+
+    @api.multi
+    def open_contract(self, contract):
+        action_xmlid = ('account_analytic_analysis.'
+                        'action_account_analytic_overdue_all')
+        action = self.env.ref(action_xmlid).read()[0]
+        action['context'] = {}
+        action['views'] = [(False, 'form')]
+        action['res_id'] = contract.id
+        return action
+
+    @api.multi
+    def create_contract(self):
+        self.ensure_one()
+        sale_ids = self.env.context.get('active_ids')
+        sale = self.env['sale.order'].browse(sale_ids)
+        sale.ensure_one()
+        contract = sale.create_contract()
+
+        if self.env.context.get('open_contract'):
+            return self.open_contract(contract)
+        return {'type': 'ir.actions.act_window_close'}
