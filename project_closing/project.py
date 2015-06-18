448df000f103fc467514116e2571afe4d4ca4635
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-06-18 16:04:33 +0200

    converted to new API, moved description to README.rst

--- project_closing/project.py
+++ project_closing/project.py
@@ -1,34 +1,20 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import orm
+from openerp import api, models
 
 
-class project_project(orm.Model):
+class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    def set_done(self, cr, uid, ids, context=None):
+    @api.multi
+    def set_done(self):
         """ We will close related analytic account """
-        analytic_account_obj = self.pool.get('account.analytic.account')
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        projects = self.browse(cr, uid, ids, context=context)
-        analytic_account_ids = [x.analytic_account_id.id for x in projects]
-        if analytic_account_ids:
-            analytic_account_obj.write(cr, uid,
-                                       analytic_account_ids,
-                                       {'state': 'close'}, context=context)
-        return super(project_project, self).set_done(cr, uid,
-                                                     ids, context=context)
 
-    def set_open(self, cr, uid, ids, context=None):
+        self.mapped('analytic_account_id').write({'state': 'close'})
+        return super(ProjectProject, self).set_done()
+
+    @api.multi
+    def set_open(self):
         """ We will re-open related analytic account """
-        analytic_account_obj = self.pool.get('account.analytic.account')
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        projects = self.browse(cr, uid, ids, context=context)
-        analytic_account_ids = [x.analytic_account_id.id for x in projects]
-        if analytic_account_ids:
-            analytic_account_obj.write(cr, uid,
-                                       analytic_account_ids,
-                                       {'state': 'open'}, context=context)
-        return super(project_project, self).set_open(cr, uid,
-                                                     ids, context=context)
+
+        self.mapped('analytic_account_id').write({'state': 'open'})
+        return super(ProjectProject, self).set_open()
