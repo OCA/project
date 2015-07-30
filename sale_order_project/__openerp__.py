cb13474ef32f35842d723c3a430efb8085ff438d
oihane <oihanecruce@gmail.com>
2015-07-30 15:49:55 +0200

    [IMP] <sale_order_project> Some suggested changes * Version 1.0 is shown in Odoo as version 8.0.1.0 * Code cleaned

--- sale_order_project/__openerp__.py
+++ sale_order_project/__openerp__.py
@@ -23,10 +23,13 @@
 
 {
     'name': 'Sale Order Project',
-    'version': '7.0',
+    'version': '1.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    'author': 'Akretion, '
+              'AvanzOSC, '
+              'Serv. Tecnol. Avanzados - Pedro M. Baeza, '
+              'Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com/',
     'depends': [
         'project',
--- sale_order_project/models/sale.py
+++ sale_order_project/models/sale.py
@@ -32,33 +32,33 @@ class SaleOrder(models.Model):
     @api.one
     @api.depends('project_id')
     def _compute_related_project_id(self):
-        if self.project_id.use_tasks:
-            related_project_id = self.env['project.project'].search(
-                [('analytic_account_id', '=', self.project_id.id)], limit=1)
-        else:
-            related_project_id = False
-        self.related_project_id = related_project_id and related_project_id[0]
+        self.related_project_id = (
+            self.project_id.use_tasks and
+            self.env['project.project'].search(
+                [('analytic_account_id', '=', self.project_id.id)],
+                limit=1)[:1])
 
     related_project_id = fields.Many2one(
         comodel_name='project.project', string='Project',
         compute='_compute_related_project_id')
 
-    def _prepare_project_vals(self):
+    @api.model
+    def _prepare_project_vals(self, order):
         name = u" %s - %s - %s" % (
-            self.partner_id.name,
+            order.partner_id.name,
             date.today().year,
-            self.name)
+            order.name)
         return {
-            'user_id': self.user_id.id,
+            'user_id': order.user_id.id,
             'name': name,
-            'partner_id': self.partner_id.id,
+            'partner_id': order.partner_id.id,
         }
 
     @api.multi
     def action_create_project(self):
         project_obj = self.env['project.project']
         for order in self:
-            vals = order._prepare_project_vals()
+            vals = self._prepare_project_vals(order)
             project = project_obj.create(vals)
             order.write({
                 'project_id': project.analytic_account_id.id
