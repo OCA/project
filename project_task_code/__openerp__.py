5b99bbeb6ebb0233edc2d5c6b64c842d8dd5455f
oihane <oihanecruce@gmail.com>
2015-07-30 13:50:26 +0200

    [IMP] <project_task_code> adapted to OCA

--- project_task_code/__openerp__.py
+++ project_task_code/__openerp__.py
@@ -9,33 +9,36 @@
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
     "name": "Sequential Code for Tasks",
     "version": "1.0",
+    "category": "Project Management",
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.avanzosc.es",
+    "license": "AGPL-3",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristo@gmail.com>",
+    ],
     "depends": [
         "project",
     ],
-    "author": "OdooMRP team",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-    ],
-    "category": "Custom Module",
-    "website": "http://www.odoomrp.com",
-    "summary": "Task sequential code generator",
-    "description": """
-    This module creates a sequential code for tasks
-    """,
     "data": [
         "data/task_sequence.xml",
         "views/project_view.xml",
     ],
     "installable": True,
-    "auto_install": False,
+    "pre_init_hook": "create_code_equal_to_id",
+    "post_init_hook": "assign_old_sequences",
 }
