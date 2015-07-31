c199f7e1df3a235bba84a181d0db54904e6fd6c9
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-07-31 10:10:21 +0200

    correct typos

--- project_closing/__openerp__.py
+++ project_closing/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2013 - 2015 Camtpcaomp
+#    Copyright (C) 2013 - 2015 Camptocamp
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
