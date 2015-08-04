edc0c38fa07ce0fb7d26fe0cff25bb3c3d412a16
oihane <oihanecruce@gmail.com>
2015-08-04 09:31:44 +0200

    [IMP] <sale_order_project> Replaced camel case

--- sale_order_project/tests/test_sale_order_project.py
+++ sale_order_project/tests/test_sale_order_project.py
@@ -15,7 +15,7 @@ class TestSaleOrderProject(common.TransactionCase):
         self.sale_order = self.env.ref('sale.sale_order_2')
         self.project = self.env.ref('project.project_project_1')
 
-    def testCreateProjectFromOrder(self):
+    def test_create_project_from_order(self):
         self.sale_order.action_create_project()
         project_dict = self.sale_order_model._prepare_project_vals(
             self.sale_order)
@@ -25,7 +25,7 @@ class TestSaleOrderProject(common.TransactionCase):
         self.assertEqual(project_name, project_dict['name'])
         self.assertEqual(self.sale_order.user_id.id, project_dict['user_id'])
 
-    def testAssignedAnalyticAccountProject(self):
+    def test_assigned_analytic_account_project(self):
         self.sale_order.write({
             'project_id': self.project.analytic_account_id.id
         })
