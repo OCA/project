fd7104fe292bcaf7e01bbc594d9eeec52ca507b7
Damien Crier <damien.crier@camptocamp.com>
2015-08-18 13:44:46 +0200

    [FIX] fix pre init script according to code review

--- project_classification/__init__.py
+++ project_classification/__init__.py
@@ -23,60 +23,47 @@ import psycopg2
 
 
 def fill_classification_id(cr):
-        query_nb_records = 'SELECT count(id) from project_project;'
-        cr.execute(query_nb_records)
-        res = cr.fetchall()
-        if res[0][0] != 0:
-            try:
-                query_classification_id = (
-                    'SELECT count(id) from project_project '
-                    'where classification_id is null;'
-                    )
-                cr.execute(query_classification_id)
-                res2 = cr.fetchall()
-                if res[0][0] == res2[0][0]:
-                    # all project have a classification_id set
-                    return
+    cr.execute("SELECT id FROM project_project LIMIT 1")
+    if not cr.fetchone():
+        return
 
-            except psycopg2.ProgrammingError:
-                cr.rollback()
-                # project exist but
-                # classification_id column does not exist in database
-                # 1. create column nullable
-                query = (
-                    'ALTER TABLE project_project '
-                    'ADD COLUMN classification_id integer;'
-                    )
-                cr.execute(query)
-                # 2. create default classification
-                query = """CREATE TABLE project_classification (
-                id serial,
-                name varchar,
-                project_id integer,
-                primary key(id)
-                );"""
-                cr.execute(query)
-                query = (
-                    "INSERT INTO project_classification (name, project_id) "
-                    "VALUES ('Unclassified', "
-                    "(select id from project_project limit 1));"
-                    )
-                cr.execute(query)
-                # 3. assign this classification to all projects
-                query = 'select id from project_classification limit 1;'
-                cr.execute(query)
-                res_create = cr.fetchall()
-                if res_create:
-                    query = (
-                        'UPDATE project_project '
-                        'set classification_id=%d;' % res_create[0][0]
-                        )
-                    cr.execute(query)
-                # 4. alter column from nullable to not null
-                query = (
-                    'ALTER TABLE project_project '
-                    'ALTER COLUMN classification_id SET NOT NULL;')
-                cr.execute(query)
-                return
-        else:
-            return
+    cr.execute("select exists(select * from information_schema.tables "
+               "where table_name = 'project_classification')")
+    exi = cr.fetchone()
+    if not exi[0]:
+        # project exist but
+        # classification_id column does not exist in database
+        # 1. create column nullable
+        query = (
+            'ALTER TABLE project_project '
+            'ADD COLUMN classification_id integer;'
+            )
+        cr.execute(query)
+        # 2. create default classification
+        query = """CREATE TABLE project_classification (
+        id serial,
+        name varchar,
+        project_id integer,
+        primary key(id)
+        );"""
+        cr.execute(query)
+        query = (
+            "INSERT INTO project_classification (name, project_id) "
+            "VALUES ('Unclassified', "
+            "(select id from project_project limit 1)) RETURNING id;"
+            )
+        cr.execute(query)
+        # 3. assign this classification to all projects
+        classification_id = cr.fetchone()[0]
+        if classification_id:
+            query = (
+                'UPDATE project_project '
+                'set classification_id=%d;' % classification_id
+                )
+            cr.execute(query)
+        # 4. alter column from nullable to not null
+        query = (
+            'ALTER TABLE project_project '
+            'ALTER COLUMN classification_id SET NOT NULL;')
+        cr.execute(query)
+        return
