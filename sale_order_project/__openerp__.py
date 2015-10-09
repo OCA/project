d822088112a2e9ce9b8acba560019e761b2c4e43
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:58 +0200

    [UPD] prefix versions with 8.0

--- sale_order_project/__openerp__.py
+++ sale_order_project/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Sale Order Project',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'author': 'Akretion, '
