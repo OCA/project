f21874f429aaf561473153ef1c0c231c2c39ed15
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-11-13 19:59:30 +0800

    [ADD] gap_analysis_price

---
+++ gap_analysis_price/models/business.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(2015)>
+# <AUTHOR(Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class BusinessRequirement(models.Model):
+    _inherit = "business.requirement"
+
+    estimated_cost_total = fields.Float(
+        compute='_get_estimated_cost_total',
+        string='Total Estimated Cost',
+        store=True
+    )
+
+    @api.one
+    @api.depends(
+        'rough_estimation_lines.price_total'
+    )
+    def _get_estimated_cost_total(self):
+        cost_total = 0
+        for line in self.rough_estimation_lines:
+            cost_total += line.price_total
+        self.estimated_cost_total = cost_total
+
+
+class BusinessEstimationLine(models.Model):
+    _inherit = "business.estimation.line"
+
+    unit_price = fields.Float(
+        string='Unit Price'
+    )
+    price_total = fields.Float(
+        compute='_get_price_total',
+        string='Total estimated cost'
+    )
+    cost_structure_id = fields.Many2one(
+        comodel_name='business.requirement.cost.structure',
+        string='Cost Structure'
+    )
+
+    @api.one
+    @api.depends('estimated_time', 'unit_price')
+    def _get_price_total(self):
+        self.price_total = self.estimated_time * self.unit_price
+
+    @api.one
+    @api.onchange('cost_structure_id', 'type_id')
+    def cost_structure_id_change(self):
+        unit_price = 0
+        user_id = False
+        structure = self.cost_structure_id
+        for line in structure.structure_lines:
+            if line.type_id.id == self.type_id.id:
+                unit_price = line.unit_price or 0
+                user_id = line.user_id.id
+        self.unit_price = unit_price
+        self.user_id = user_id
+
+
+class BusinessRequirementCostStructure(models.Model):
+    _name = "business.requirement.cost.structure"
+    _description = "Bus. Req. Cost Structure"
+
+    name = fields.Char('Name', required=True)
+    structure_lines = fields.One2many(
+        comodel_name='business.requirement.cost.structure.line',
+        inverse_name='structure_id',
+        string='Bus. Req. Cost Structure Lines',
+        copy=True,
+    )
+
+
+class BusinessRequirementCostStructureLine(models.Model):
+    _name = "business.requirement.cost.structure.line"
+    _description = "Bus. Req. Cost Structure Line"
+
+    type_id = fields.Many2one(
+        comodel_name='business.estimation.type',
+        string='Estimation Type',
+        ondelete='restrict',
+        required=True,
+    )
+    product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Product',
+        required=True
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Default User'
+    )
+    unit_price = fields.Float(
+        string='Unit Price',
+        required=True
+    )
+    uom_id = fields.Many2one(
+        comodel_name='product.uom',
+        string='Default UoM'
+    )
+    structure_id = fields.Many2one(
+        comodel_name='business.requirement.cost.structure',
+        string='Bus. Req. Cost Structure',
+        ondelete='cascade'
+    )
+
+    @api.one
+    @api.onchange('product_id')
+    def cost_structure_id_change(self):
+        unit_price = 0
+        uom_id = False
+        product = self.product_id
+        if product:
+            unit_price = product.list_price or 0
+            uom_id = product.uom_id.id
+        self.unit_price = unit_price
+        self.uom_id = uom_id
---
+++ gap_analysis_price/tests/test_br.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(2015)>
+# <AUTHOR(Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests import common
+
+
+@common.at_install(False)
+@common.post_install(True)
+class BusinessRequirementTestCase(common.TransactionCase):
+    def setUp(self):
+        super(BusinessRequirementTestCase, self).setUp()
+        self.br = self.env['business.requirement']
+
+    def test_get_estimated_cost_total(self):
+        vals = {
+            'name': ' test',
+            'rough_estimation_lines': [
+                (
+                    0, 0,
+                    {
+                        'name': 'line1', 'estimated_time': 15, 'unit_price': 1,
+                    }
+                ),
+                (
+                    0, 0,
+                    {
+                        'name': 'line1', 'estimated_time': 25, 'unit_price': 1,
+                    }
+                ),
+                (
+                    0, 0,
+                    {
+                        'name': 'line1', 'estimated_time': 35, 'unit_price': 1,
+                    }
+                ),
+                (
+                    0, 0,
+                    {
+                        'name': 'line1', 'estimated_time': 45, 'unit_price': 1,
+                    }
+                ),
+            ]
+        }
+        br = self.br.create(vals)
+        cost_total = br.estimated_cost_total
+        self.assertEqual(cost_total, 15 + 25 + 35 + 45)
