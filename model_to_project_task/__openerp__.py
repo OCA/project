0230c9d8d637705e1bfb2409b4bcd44a1a4b87e8
David Beal <david.beal@akretion.com>
2015-11-25 19:01:14 +0100

    [ADD] module model_to_project_task

---
+++ model_to_project_task/__openerp__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# © 2015 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Model to Project Task',
+    'version': '0.3',
+    'category': 'project',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'depends': [
+        'project',
+    ],
+    'demo': [],
+    'website': 'https://www.akretion.com',
+    'data': [
+        'project_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
