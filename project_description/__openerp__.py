5edc8c38e47dc233652857aa6eb1551aacc85a44
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-04 18:10:48 +0100

    [8.0][project_description] New module to have project descriptions.
    Description is in HTML format, and quick search added too.

---
+++ project_description/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Project Description",
+    "summary": "Add a description to projects",
+    "version": "8.0.1.0.0",
+    "category": "Project Management",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_view.xml",
+    ],
+}
