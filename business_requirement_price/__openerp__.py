9366de2fbbf33375300489d2db8c7a3a3b540caf
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-04 10:46:50 +0800

    [UPD] update author information formate in file header

--- business_requirement_price/__openerp__.py
+++ business_requirement_price/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © <YEAR(2015)>
-# <Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng(S)>
+# © 2015
+# Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Business Requirement - Financial evaluation',
