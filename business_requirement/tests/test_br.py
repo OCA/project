e37403c1fb971b2acf97c19291afa0036da65498
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-14 14:49:44 +0800

    [UPD] modify unittest

--- business_requirement/tests/test_br.py
+++ business_requirement/tests/test_br.py
@@ -12,49 +12,41 @@ class BusinessRequirementTestCase(common.TransactionCase):
         super(BusinessRequirementTestCase, self).setUp()
         self.br = self.env['business.requirement']
 
-    def test_get_resource_time_total(self):
-        vals = {
-            'name': ' test',
-            'deliverable_lines': [
-                (0, 0, {'name': 'line1', 'resource_time': 15.0}),
-                (0, 0, {'name': 'line2', 'resource_time': 25.0}),
-                (0, 0, {'name': 'line3', 'resource_time': 35.0}),
-                (0, 0, {'name': 'line4', 'resource_time': 45.0}),
-            ]
-        }
-        br = self.br.create(vals)
-        br._get_resource_time_total()
-        time_total = br.resource_time_total
-        self.assertEqual(time_total, 15.0 + 25.0 + 35.0 + 45.0)
-
-    def test_compute_level(self):
+    def test_get_level(self):
         br_vals1 = {
             'name': ' test',
+            'description': 'test',
             'parent_id': False,
         }
         br1 = self.br.create(br_vals1)
-        level1 = br1._compute_level()
+        br1._get_level()
+        level1 = br1.level
         self.assertEqual(level1, 1)
 
         br_vals2 = {
             'name': ' test',
+            'description': 'test',
             'parent_id': br1.id,
         }
         br2 = self.br.create(br_vals2)
-        level2 = br2._compute_level()
+        br2._get_level()
+        level2 = br2.level
         self.assertEqual(level2, 2)
 
         br_vals3 = {
             'name': ' test',
+            'description': 'test',
             'parent_id': br2.id,
         }
         br3 = self.br.create(br_vals3)
-        level3 = br3._compute_level()
+        br3._get_level()
+        level3 = br3.level
         self.assertEqual(level3, 3)
 
     def test_action_button_confirm(self):
         br_vals = {
             'name': ' test',
+            'description': 'test',
             'parent_id': False,
         }
         br = self.br.create(br_vals)
@@ -64,6 +56,7 @@ class BusinessRequirementTestCase(common.TransactionCase):
     def test_action_button_back_draft(self):
         br_vals = {
             'name': 'test',
+            'description': 'test',
             'parent_id': False,
         }
         br = self.br.create(br_vals)
@@ -73,6 +66,7 @@ class BusinessRequirementTestCase(common.TransactionCase):
     def test_action_button_approve(self):
         br_vals = {
             'name': ' test',
+            'description': 'test',
             'parent_id': False,
         }
         br = self.br.create(br_vals)
@@ -82,23 +76,9 @@ class BusinessRequirementTestCase(common.TransactionCase):
     def test_action_button_done(self):
         br_vals = {
             'name': ' test',
+            'description': 'test',
             'parent_id': False,
         }
         br = self.br.create(br_vals)
         br.action_button_done()
         self.assertEqual(br.state, 'done')
-
-    def test_get_states(self):
-        br_vals = {
-            'name': ' test',
-            'parent_id': False,
-        }
-        br = self.br.create(br_vals)
-        states = br._get_states()
-        states2 = [
-            ('draft', 'Draft'),
-            ('confirmed', 'Confirmed'),
-            ('approved', 'Approved'),
-            ('done', 'Done')
-        ]
-        self.assertEqual(states, states2)
