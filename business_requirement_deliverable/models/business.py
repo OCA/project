2a76aa7c57d8dde8d97a02c34657004472f20486
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-14 14:59:44 +0800

    [UPD] modify unittest

--- business_requirement_deliverable/models/business.py
+++ business_requirement_deliverable/models/business.py
@@ -5,97 +5,6 @@
 from openerp import api, fields, models
 
 
-class BusinessRequirement(models.Model):
-    _inherit = "business.requirement"
-
-    deliverable_lines = fields.One2many(
-        comodel_name='business.requirement.deliverable',
-        inverse_name='business_requirement_id',
-        string='Deliverable Lines',
-        copy=False,
-        readonly=True,
-        states={'draft': [('readonly', False)]}
-    )
-
-    resource_time_total = fields.Float(
-        compute='_get_resource_time_total',
-        string='Total Resource Time',
-        store=True,
-    )
-
-    @api.one
-    @api.depends(
-        'deliverable_lines.resource_time')
-    def _get_resource_time_total(self):
-        time_total = sum(
-            line.resource_time * line.qty for line in self.deliverable_lines)
-        self.resource_time_total = time_total
-
-
-class BusinessRequirementTaskType(models.Model):
-    _name = "business.requirement.task.type"
-    _description = "Business Requirement Task Type"
-
-    name = fields.Char(string='Name', required=True)
-
-
-class BusinessRequirementDeliverable(models.Model):
-    _name = "business.requirement.deliverable"
-    _description = "Business Requirement Deliverable"
-
-    sequence = fields.Integer('Sequence')
-    description = fields.Char('Description', required=True)
-    product_id = fields.Many2one(
-        comodel_name='product.product',
-        string='Product',
-        required=True
-    )
-    uom_id = fields.Many2one(
-        comodel_name='product.uom',
-        string='UoM',
-        required=True
-    )
-    qty = fields.Integer(
-        string='Quantity',
-        store=True,
-        default=1,
-    )
-    resource_time = fields.Float(
-        compute='_get_resource_time_total',
-        string='Resource Time',
-        store=True,
-    )
-    resource_ids = fields.One2many(
-        comodel_name='business.requirement.resource',
-        inverse_name='business_requirement_deliverable_id',
-        string='Business Requirement Resource',
-        copy=False,
-    )
-    business_requirement_id = fields.Many2one(
-        comodel_name='business.requirement',
-        string='Business Requirement',
-        ondelete='cascade'
-    )
-
-    @api.one
-    @api.depends(
-        'resource_ids.qty',
-        'resource_ids.resource_time')
-    def _get_resource_time_total(self):
-        time_total = sum(
-            line.resource_time for line in self.resource_ids)
-        self.resource_time = time_total
-
-    @api.one
-    @api.onchange('product_id')
-    def product_id_change(self):
-        uom_id = False
-        product = self.product_id
-        if product:
-            uom_id = product.uom_id.id
-        self.uom_id = uom_id
-
-
 class BusinessRequirementResource(models.Model):
     _name = "business.requirement.resource"
     _description = "Business Requirement Resource"
@@ -164,3 +73,94 @@ class BusinessRequirementResource(models.Model):
         elif resource_type == "task":
             vals.update({'qty': 0})
         return super(BusinessRequirementResource, self).write(vals)
+
+
+class BusinessRequirementDeliverable(models.Model):
+    _name = "business.requirement.deliverable"
+    _description = "Business Requirement Deliverable"
+
+    sequence = fields.Integer('Sequence')
+    description = fields.Char('Description', required=True)
+    product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Product',
+        required=True
+    )
+    uom_id = fields.Many2one(
+        comodel_name='product.uom',
+        string='UoM',
+        required=True
+    )
+    qty = fields.Integer(
+        string='Quantity',
+        store=True,
+        default=1,
+    )
+    resource_time = fields.Float(
+        compute='_get_resource_time_total',
+        string='Resource Time',
+        store=True,
+    )
+    resource_ids = fields.One2many(
+        comodel_name='business.requirement.resource',
+        inverse_name='business_requirement_deliverable_id',
+        string='Business Requirement Resource',
+        copy=False,
+    )
+    business_requirement_id = fields.Many2one(
+        comodel_name='business.requirement',
+        string='Business Requirement',
+        ondelete='cascade'
+    )
+
+    @api.one
+    @api.depends(
+        'resource_ids.qty',
+        'resource_ids.resource_time')
+    def _get_resource_time_total(self):
+        time_total = sum(
+            line.resource_time for line in self.resource_ids)
+        self.resource_time = time_total
+
+    @api.one
+    @api.onchange('product_id')
+    def product_id_change(self):
+        uom_id = False
+        product = self.product_id
+        if product:
+            uom_id = product.uom_id.id
+        self.uom_id = uom_id
+
+
+class BusinessRequirement(models.Model):
+    _inherit = "business.requirement"
+
+    deliverable_lines = fields.One2many(
+        comodel_name='business.requirement.deliverable',
+        inverse_name='business_requirement_id',
+        string='Deliverable Lines',
+        copy=False,
+        readonly=True,
+        states={'draft': [('readonly', False)]}
+    )
+
+    resource_time_total = fields.Float(
+        compute='_get_resource_time_total',
+        string='Total Resource Time',
+        store=True,
+    )
+
+    @api.one
+    @api.depends(
+        'deliverable_lines.resource_time')
+    def _get_resource_time_total(self):
+        time_total = sum(
+            line.resource_time * line.qty for line in self.deliverable_lines)
+        self.resource_time_total = time_total
+
+
+class BusinessRequirementTaskType(models.Model):
+    _name = "business.requirement.task.type"
+    _description = "Business Requirement Task Type"
+
+    name = fields.Char(string='Name', required=True)
--- business_requirement_deliverable/tests/test_br.py
+++ business_requirement_deliverable/tests/test_br.py
@@ -10,11 +10,12 @@ from openerp.tests import common
 class BusinessRequirementTestCase(common.TransactionCase):
     def setUp(self):
         super(BusinessRequirementTestCase, self).setUp()
-        self.br = self.env['business.requirement']
+        self.br = self.registry['business.requirement']
 
     def test_get_resource_time_total(self):
         vals = {
             'name': ' test',
+            'description': 'test',
             'deliverable_lines': [
                 (0, 0, {'description': 'line1', 'resource_time': 15.0}),
                 (0, 0, {'description': 'line2', 'resource_time': 25.0}),
@@ -22,7 +23,9 @@ class BusinessRequirementTestCase(common.TransactionCase):
                 (0, 0, {'description': 'line4', 'resource_time': 45.0}),
             ]
         }
-        br = self.br.create(vals)
-        br._get_resource_time_total()
-        time_total = br.resource_time_total
+        br_id = self.br.create(self.cr, self.uid, vals)
+        br_obj = self.br.browse(self.cr, self.uid, br_id)
+        br_obj.resource_time_total
+        # br._get_resource_time_total()
+        time_total = br_obj.resource_time_total
         self.assertEqual(time_total, 15.0 + 25.0 + 35.0 + 45.0)
