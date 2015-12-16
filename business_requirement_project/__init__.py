f3f4ae004371c22576a85b6757364ab906fe59be
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-16 10:41:10 +0800

    [UPD] modify for the company copyright

--- business_requirement_project/__init__.py
+++ business_requirement_project/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015
-# Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
+# © 2015 Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
 from . import wizard
