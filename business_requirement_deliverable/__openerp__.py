81bf7f64c1f3fbf3b98dfe86dfb59004bec89b5f
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-16 10:37:12 +0800

    [UPD] modify for the company copyright

--- business_requirement_deliverable/__openerp__.py
+++ business_requirement_deliverable/__openerp__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015
-# Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
+# © 2015 Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Business Requirement Deliverable',
@@ -8,7 +7,7 @@
     'summary': 'Business Requirement Deliverable',
     'version': '8.0.1.0.0',
     'website': 'www.elico-corp.com',
-    "author": "Elico Corp(S), Odoo Community Association (OCA)",
+    "author": "Elico Corp, Odoo Community Association (OCA)",
     'depends': [
         'business_requirement',
         'base',
