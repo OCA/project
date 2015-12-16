493d3e0984b397e2022de5d3b136bf923f11a1de
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-16 10:31:34 +0800

    [UPD] modify for the company copyright

--- business_requirement/tests/__init__.py
+++ business_requirement/tests/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015
-# Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
+# © 2015 Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import test_br
