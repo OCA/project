c9977fa8664b2662625ca207fc797b8423a24301
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2015-12-24 19:34:17 +0800

    [UPD] update customer when project has onchanged

--- business_requirement_crm/models/business.py
+++ business_requirement_crm/models/business.py
@@ -12,21 +12,18 @@ class CrmLead(models.Model):
     )
     resource_cost_total = fields.Float(
         compute='_get_resource_cost_total',
-        string='Total Revenue from BR',
+        string='Total Revenue from BR'
     )
 
     @api.one
     def _get_resource_cost_total(self):
-        cost_total = 0
         linked_brs = self.project_id and self.project_id.br_ids or []
-        for br in linked_brs:
-            if br.state in ('drop', 'cancel'):
-                continue
-            cost_total += br.resource_cost_total
-        self.resource_cost_total = cost_total
+        self.resource_cost_total = sum(
+            [br.resource_cost_total for br in linked_brs
+                if br.state not in ('drop', 'cancel')]) or 0
 
     @api.one
     @api.onchange('project_id')
     def project_id_change(self):
-        self.partner_id = self.project_id and self.project_id.partner_id \
-            or False
+        if self.project_id:
+            self.partner_id = self.project_id.partner_id.id
