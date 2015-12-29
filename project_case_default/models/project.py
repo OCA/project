2b17e961deb71ddcec56cc454a4b8feeb47c4bd1
sergio-incaser <sergio@incaser.es>
2015-12-29 12:46:56 +0100

    [NEW][9.0] project_case_default: Default stages for new projects.

--- project_case_default/models/project.py
+++ project_case_default/models/project.py
@@ -3,8 +3,7 @@
 # (c) 2015 Incaser Informatica S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
-from openerp.tools.float_utils import float_round
+from openerp import models, fields
 
 
 class ProjectTaskType(models.Model):
@@ -21,16 +20,15 @@ class ProjectProject(models.Model):
     _inherit = 'project.project'
 
     def _get_type_common(self):
-        ids = self.env['project.task.type'].search(
-                [('case_default', '=', True)])
+        ids = self.env['project.task.type'].search([
+            ('case_default', '=', True)])
         return ids
 
     type_ids = fields.Many2many(
-            comodel_name='project.task.type', relation='project_task_type_rel',
-            column1='project_id', column2='type_id', string='Tasks Stages',
-            states={
-                'close': [('readonly', True)],
-                'cancelled': [('readonly', True)]
-            }, default=_get_type_common
+        comodel_name='project.task.type', relation='project_task_type_rel',
+        column1='project_id', column2='type_id', string='Tasks Stages',
+        states={
+            'close': [('readonly', True)],
+            'cancelled': [('readonly', True)]
+        }, default=_get_type_common
     )
-
