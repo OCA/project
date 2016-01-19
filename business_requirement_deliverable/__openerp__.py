fb13d60b6a09fcbe0986797ff1fe3743cd7ae639
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-01-19 14:44:06 +0800

    [UPD] add ACL in BusinessRequirementResource

--- business_requirement_deliverable/__openerp__.py
+++ business_requirement_deliverable/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Elico Corp, contributor: Eric Caudal, Alex Duan, Xie XiaoPeng
+# © 2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Business Requirement Deliverable',
