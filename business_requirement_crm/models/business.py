cfcf597443eaa22f781e3e4a33d2ccb1262a3082
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-01-19 14:39:18 +0800

    [UPD] midify License in py file header

--- business_requirement_crm/models/business.py
+++ business_requirement_crm/models/business.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import api, fields, models
 
 
