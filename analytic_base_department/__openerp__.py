f230d843d6a862659af3ce03b7e77e61749ba030
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-28 17:11:01 +0100

    [PORT][9.0] analytic_base_department

--- analytic_base_department/__openerp__.py
+++ analytic_base_department/__openerp__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+# © 2011 Joël Grand-Guillaume (Camptocamp)
+# © 2013 Daniel Reis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Analytic Department Categorization",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Camptocamp, Daniel Reis,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
@@ -11,7 +14,7 @@ No required dependency on Accounting modules.
 """,
     "website": "http://camptocamp.com",
     "depends": ["analytic", "hr"],
-    "data": ["analytic_view.xml"],
+    "data": ["views/analytic.xml"],
     'test': ['test/analytic.yml'],
-    'installable': False,
+    'installable': True,
 }
--- analytic_base_department/models/__init__.py
+++ analytic_base_department/models/__init__.py
@@ -2,4 +2,4 @@
 # © 2011 Joël Grand-Guillaume (Camptocamp)
 # © 2013 Daniel Reis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import analytic
