d704355a88ec3d6d4df755c5a08068a9ae28ccc3
Luke Zheng <luke.zheng@elico-corp.com>
2016-03-14 16:00:45 +0800

    [ADD] Fixed business requirement cost & sales price related issues

--- business_requirement/models/business.py
+++ business_requirement/models/business.py
@@ -2,7 +2,8 @@
 # © 2016 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import api, fields, models, _
+from openerp.exceptions import except_orm
 from openerp import tools
 from openerp import SUPERUSER_ID
 
@@ -11,7 +12,7 @@ class BusinessRequirement(models.Model):
     _inherit = ['mail.thread', 'ir.needaction_mixin']
     _name = "business.requirement"
     _description = "Business Requirement"
-    _order = 'name desc'
+    _order = 'id desc'
 
     sequence = fields.Char(
         'Sequence',
