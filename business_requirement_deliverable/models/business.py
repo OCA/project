ae37f6292a9090288409f9023201e0a6a9a29a42
Luke Zheng <luke.zheng@elico-corp.com>
2016-03-21 12:03:14 +0800

    [FIX] Rename

--- business_requirement_deliverable/models/business.py
+++ business_requirement_deliverable/models/business.py
@@ -241,8 +241,6 @@ class BusinessRequirement(models.Model):
         'project_id.pricelist_id.currency_id',
     )
     def _compute_deliverable_total(self):
-        import pdb
-        pdb.set_trace()
         for br in self:
             if br.deliverable_lines:
                 total_revenue_origin = sum(
