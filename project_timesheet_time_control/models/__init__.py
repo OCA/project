7bcc8feef9765b0ef492a3232ddb5137bd6da2c6
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-23 13:46:55 +0100

    project_timesheet_time_control
    * Decorate methods correctly + README imp
    * Add tests
    * Add button on tasks
    * Better README
    * open/close behaviour

--- project_timesheet_time_control/models/__init__.py
+++ project_timesheet_time_control/models/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antonio Espinosa
-# © 2016 Pedro M. Baeza
-# © 2016 Sergio Teruel
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import account_analytic_line
