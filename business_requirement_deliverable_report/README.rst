a69efd2cefe5ac0d5cc571816c65b21fa04dc893
victor <victor.martin@elico-corp.com>
2016-03-25 18:50:08 +0800

    [ADD] business_requirement_deliverable_report: New module based on BR000061 - Task 12215 and 12214

---
+++ business_requirement_deliverable_report/README.rst
@@ -0,0 +1,56 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+   
+=======================================
+Business Requirement Report Docx Module
+=======================================
+
+This module generate the pdf report based on the docx template and data from current business requirement.
+
+Installation
+============
+
+* install the module: report_docx
+
+Usage
+=====
+
+This module pass the data from current object to the docx report engine to create the pdf report.
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/
+project/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback `here <https://github.com/OCA/
+project/issues/new?body=module:%20
+business_requirement_deliverable_report%0Aversion:%20
+8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Siyuan Gu <gu.siyuan@elico-corp.com>
+* Victor M. Martin <victor.martin@elico-corp.com>
+
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
---
+++ business_requirement_deliverable_report/models/business_requirement_deliverable_report.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.report import report_sxw
+from openerp.addons.report_docx.report.report_docx import ReportDocx
+import html2text
+
+
+class BRDeliverableReportDocxParser(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(BRDeliverableReportDocxParser, self).__init__(
+            cr, uid, name, context=context)
+
+
+class BRDeliverableReport(ReportDocx):
+    def __init__(
+        self, name, table, rml=False, parser=False, header=True, store=False
+    ):
+        super(BRDeliverableReport, self).__init__(
+            name, table, rml, parser, header, store
+        )
+
+    def generate_docx_data(self, cr, uid, ids, context):
+        active_module = context['active_model']
+
+        data = []
+        for id in ids:
+            module = self.pool.get(active_module).browse(
+                cr, uid, id, context)
+            data.append(self._obj2dict(module))
+
+        return data
+
+    def _parse_html(self, obj):
+        if isinstance(obj, str):
+            return html2text.html2text(obj)
+        elif isinstance(obj, unicode):
+            return html2text.html2text(obj)
+        else:
+            return obj
+
+    def _obj2dict(self, obj):
+        memberlist = [m for m in dir(obj)]
+        context = {}
+        for m in memberlist:
+            if m[0] != "_" and not callable(m):
+                context[m] = self._parse_html(getattr(obj, m))
+        return context
+
+BRDeliverableReport(
+    'report.report.docx', 'report.docx.template',
+    parser=BRDeliverableReportDocxParser)
