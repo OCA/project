6c60c8cb32f32183187d18d80efd7054cae8033c
Matjaž Mozetič <info@matmoz.si>
2016-03-25 06:33:28 +0100

    Proper PEP-8

--- project_wbs_task/model/project_project.py
+++ project_wbs_task/model/project_project.py
@@ -4,7 +4,7 @@
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import api, models
 
 
 class Project(models.Model):
@@ -16,14 +16,17 @@ class Project(models.Model):
         :return dict: dictionary value for created view
         """
         project = self[0]
-        task = self.env['project.task'].search([('project_id', '=',
-                                                 project.id)])
-        res = self.env['ir.actions.act_window'].\
-            for_xml_id('project_wbs_task', 'action_task_tree_view')
+        task = self.env['project.task'].search(
+            [('project_id', '=', project.id)]
+        )
+        res = self.env['ir.actions.act_window'].for_xml_id(
+            'project_wbs_task', 'action_task_tree_view'
+        )
         res['context'] = {
             'default_project_id': project.id,
         }
-        res['domain'] = "[('id', 'in', [" + ','.join(map(str,
-                                                         task.ids)) + "])]"
+        res['domain'] = "[('id', 'in', [" + ','.join(
+            map(str, task.ids)
+        ) + "])]"
         res['nodestroy'] = False
         return res
