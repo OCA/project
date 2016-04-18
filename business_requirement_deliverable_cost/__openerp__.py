8cac9c3106b922372ea1270d49e735c32d7c8a84
eric <3licoC0rp>
2016-04-18 18:12:12 +0800

    update openerp.py

--- business_requirement_deliverable_cost/__openerp__.py
+++ business_requirement_deliverable_cost/__openerp__.py
@@ -14,6 +14,13 @@
     "depends": [
         "business_requirement_deliverable",
     ],
+    'image': [
+		'static/img/bus_req_tree.png',
+		'static/img/bus_req_acl1.png',
+		'static/img/bus_req_acl2.png',
+		'static/img/bus_req_acl3.png',
+		'static/img/bus_req_control.png'
+	],
     "data": [
         "security/business_requirement_deliverable_security.xml",
         "views/business.xml",
