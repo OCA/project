4e2a15e7d6789cecd56cb3f0a1b6631efa1d80ea
Carlos Incaser <carlos@incaser.es>
2016-04-20 01:59:25 +0200

    [FIX] project_double_alias: No cover other module depends line.

--- project_double_alias/models/project_project.py
+++ project_double_alias/models/project_project.py
@@ -27,7 +27,8 @@ class ProjectProject(models.Model):
         alias_defaults = {'project_id': self.id}
         # This is if you have installed project_issue_sheet module
         if self.env['project.issue']._fields.get('analytic_account_id'):
-            alias_defaults['analytic_account_id'] = self.analytic_account_id
+            alias_defaults['analytic_account_id'] = (
+                self.analytic_account_id)  # pragma: no cover
         return alias_obj.create({
             'alias_name': self.second_alias_name,
             'alias_contact': self.alias_contact,
