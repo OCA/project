5f7cfd42c8773346305371c87b0baa8589aec351
Jairo Llopis <yajo.sk8@gmail.com>
2016-07-27 11:19:19 +0200

    [9.0][project_description] Migrate from v8.
    Almost no changes.

--- project_description/__openerp__.py
+++ project_description/__openerp__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Description",
     "summary": "Add a description to projects",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Project Management",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
+    "author": "Antiun Ingeniería S.L., "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
