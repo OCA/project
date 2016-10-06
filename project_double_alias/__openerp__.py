d0f88d399e2ffff9374f48c01749be603c593c5b
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-06 16:05:38 +0200

    [MIG] Make modules uninstallable

--- project_double_alias/__openerp__.py
+++ project_double_alias/__openerp__.py
@@ -11,7 +11,7 @@
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "installable": True,
+    'installable': False,
     "depends": [
         "project_issue",
     ],
