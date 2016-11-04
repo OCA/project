8e730e16795ea69318d8aa41881be31ce385e8a7
Andrea <a.stirpe@onestein.nl>
2016-11-04 13:48:13 +0100

    Porting project_description to V10

--- project_description/models/project.py
+++ project_description/models/project.py
@@ -2,7 +2,7 @@
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
