b6c85dcfc6ad011c5712d9f4cdd7c81d53ce9fbf
Kelly Lougheed <kelly@smdrugstore.com>
2016-12-17 15:28:41 +0100

    [MIG] project_task_code: Migrate to 10.0
    OCA Transbot updated translations from Transifex
    
    [MIG] project_task_code: Migrate to 10.0
    
    Edit init and XML
    
    Add env
    
    Change task_ids to tasks
    
    Change to @api.multi before def copy

--- project_task_code/__manifest__.py
+++ project_task_code/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Sequential Code for Tasks",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Project Management",
     "author": "OdooMRP team, "
               "AvanzOSC, "
