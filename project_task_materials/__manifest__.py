326c8cc09d6d5c5e350fb1bbc3d1d21882021dee
cubells <vicent@vcubells.net>
2016-12-18 11:48:48 +0100

    project_task_materials module
    * Added tests
    * Change warning in write by constraint

--- project_task_materials/__manifest__.py
+++ project_task_materials/__manifest__.py
@@ -1,47 +1,25 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2012 - 2013 Daniel Reis
-#    Copyright (C) 2015 - Antiun Ingeniería S.L. - Sergio Teruel
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2012 - 2013 Daniel Reis
+# Copyright 2015 - Antiun Ingeniería S.L. - Sergio Teruel
+# Copyright 2016 - Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Project Task Materials',
-    'summary': 'Record products spent in a Task',
-    'version': '8.0.1.0.0',
-    'category': "Project Management",
-    'author': "Daniel Reis,"
-              "Antiun Ingeniería S.L.,"
+    "name": "Project Task Materials",
+    "summary": "Record products spent in a Task",
+    "version": "9.0.1.0.0",
+    "category": "Project Management",
+    "author": "Daniel Reis,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'description': """\
-Project Tasks allow to record time spent, but some activities, such as
-Field Service, often require you to keep a record of the materials spent.
-
-This module adds the ability to also this material spending.
-To use it, make sure the "Log work activities on tasks" Project setting is
-activated.
-
-Note that only a simple record is made and no accounting or stock moves are
-actually performed.""",
-    'depends': ['project', 'product'],
-    'data': [
-        'views/project_view.xml',
-        'security/ir.model.access.csv',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project_timesheet",
+        "product"
     ],
-    'installable': False,
+    "data": [
+        "views/project_view.xml",
+        "security/ir.model.access.csv",
+    ],
 }
--- project_task_materials/models/project.py
+++ project_task_materials/models/project.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2012 - 2013 Daniel Reis
-#    Copyright (C) 2015 - Antiun Ingeniería S.L. - Sergio Teruel
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012 - 2013 Daniel Reis
+# Copyright 2015 - Antiun Ingeniería S.L. - Sergio Teruel
+# Copyright 2016 - Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import api, fields, models, _
+from openerp.exceptions import ValidationError
 
 
 class Task(models.Model):
     _inherit = "project.task"
+
     material_ids = fields.One2many(
         comodel_name='project.task.materials', inverse_name='task_id',
         string='Materials used')
@@ -32,9 +19,19 @@ class Task(models.Model):
 class ProjectTaskMaterials(models.Model):
     _name = "project.task.materials"
     _description = "Task Materials Used"
+
     task_id = fields.Many2one(
         comodel_name='project.task', string='Task', ondelete='cascade',
         required=True)
     product_id = fields.Many2one(
         comodel_name='product.product', string='Product', required=True)
     quantity = fields.Float(string='Quantity')
+
+    @api.multi
+    @api.constrains('quantity')
+    def _check_quantity(self):
+        for material in self:
+            if not material.quantity > 0.0:
+                raise ValidationError(
+                    _('Quantity of materials consumed must be greater than 0.')
+                )
