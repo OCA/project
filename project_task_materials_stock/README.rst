1debb3e013b77fe8cde8ba2aa69b958c89078b69
cubells <vicent@vcubells.net>
2016-12-25 13:15:50 +0100

    project_task_materials_stock module

--- project_task_materials_stock/README.rst
+++ project_task_materials_stock/README.rst
@@ -8,26 +8,29 @@ Project Task Materials Stock
 Project Tasks allow to record time spent, but some activities, such as
 Field Service, often require you to keep a record of the materials spent.
 
-This module extends project_task_material module to consume products spent in 
-a task and create analytic lines to manage costs and create invoices.  
+This module extends project_task_material module to consume products spent in
+a task and create analytic lines to manage costs and create invoices.
 
 Configuration
 =============
 
-Check option 'Consume Material' in Task Stage to generate a stock move when
-the task is in that stage.
-
-Go to Setting -> Configuration - > Projects and enable option
-"Manage time estimation on tasks"
+#. If you are a project manager, go to Project > Configuration > Stages and
+   check option 'Consume Material' in Task Stage to generate a stock move when
+   the task is in that stage.
+#. Go to Project -> Configuration - > Settings and enable option
+   "Manage time estimation on tasks"
 
 Usage
 =====
+
+#. Go to a task, edit, and add materials to be consumed on tab "Materials".
+#. Move task to an stage on consume material are activated and moves and
+   analytic lines will be created.
+
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/140/8.0
+   :target: https://runbot.odoo-community.org/runbot/140/9.0
 
-There is a new field named "Consume Material" in "Task stage" to indicate
-when you want to consume materials.
 
 Known issues / Roadmap
 ======================
@@ -39,26 +42,8 @@ Bug Tracker
 
 Bugs are tracked on `GitHub Issues <https://github.com/OCA/project/issues>`_.
 In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/project/issues/new?body=module:%20project_task_materials_stock%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
-
-
-License
-=======
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/agpl-3.0-standalone.html>.
-
+If you spotted it first, help us smashing it by providing a detailed and
+welcomed feedback.
 
 Credits
 =======
@@ -70,6 +55,7 @@ Contributors
 * Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 * Carlos Dauden <carlos@incaser.es>
 * Sergio Teruel <sergio@incaser.es>
+* Vicent Cubells <vicent.cubells@tecnativa.com>
 
 Maintainer
 ----------
--- project_task_materials_stock/tests/test_task_materials.py
+++ project_task_materials_stock/tests/test_task_materials.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# Copyright 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# Copyright 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp.exceptions import UserError
+
 from openerp.tests.common import TransactionCase
 
 
@@ -9,29 +12,65 @@ class TestTaskMaterials(TransactionCase):
     # Use case : Prepare some data for current test case
     def setUp(self):
         super(TestTaskMaterials, self).setUp()
-        self.stage_deployed = self.env.ref('project.project_tt_deployment')
+        self.stage_deployed = self.env.ref('project.project_stage_1')
         self.stage_deployed.consume_material = True
         self.project = self.env.ref('project.project_project_1')
-        # import pdb; pdb.set_trace()
-        self.product = self.env.ref('product.product_product_40')
+        self.product1 = self.env.ref('product.product_product_40')
+        self.product2 = self.env.ref('stock.product_icecream')
+        self.product1_uom = self.env.ref('product.product_uom_unit')
+        self.product2_uom = self.env.ref('product.product_uom_kgm')
         self.task = self.env['project.task'].create(
             {'name': 'task test 1',
              'project_id': self.project.id})
         self.task_materials = self.env['project.task.materials'].create(
             {'task_id': self.task.id,
-             'product_id': self.product.id,
+             'product_id': self.product1.id,
+             'product_uom_id': self.product1_uom.id,
              'quantity': 3})
-        self.product.product_tmpl_id.standard_price = 100.0
+        self.product1.product_tmpl_id.standard_price = 100.0
         self.expected_amount = -(100.0 * 3)
 
     def test_task_materials(self):
+
+        self.assertEqual(self.task_materials.product_uom_id.id,
+                         self.product1_uom.id)
+        self.task.material_ids.write({'product_id': self.product2.id})
+        self.task_materials._onchange_product_id()
+        self.assertEqual(self.task_materials.product_uom_id.id,
+                         self.product2_uom.id)
+        self.task_materials.write({'product_id': self.product1.id})
+        result_onchange = self.task_materials._onchange_product_id()
+        self.assertEqual(result_onchange['domain']['product_uom_id'],
+                         [('category_id', '=', self.product1_uom.id)])
         self.assertEqual(self.task.stock_state, 'pending')
-        self.task.stage_id = self.stage_deployed.id
-        self.assertEqual(len(self.task.stock_move_ids), 1)
-        self.assertEqual(len(self.task.analytic_line_ids), 1)
-        self.task.stage_id = self.env.ref('project.project_tt_design').id
+        self.task.stage_id = self.env.ref('project.project_stage_0').id
         self.assertEqual(len(self.task.stock_move_ids), 0)
         self.assertEqual(len(self.task.analytic_line_ids), 0)
+        self.task.stage_id = self.stage_deployed.id
+        self.assertEqual(len(self.task.stock_move_ids), 1)
+        self.task2 = self.task.copy()
+        self.task_materials2 = self.env['project.task.materials'].create(
+            {'task_id': self.task2.id,
+             'product_id': self.product1.id,
+             'product_uom_id': self.product1_uom.id,
+             'quantity': 3})
+        self.task2.stage_id = self.stage_deployed.id
+        self.task2.stock_move_ids.write({'state': 'draft'})
+        self.assertEqual(len(self.task2.stock_move_ids), 1)
+        self.assertEqual(len(self.task2.analytic_line_ids), 1)
+        moves = self.task2.stock_move_ids.ids
+        analytics = self.task2.analytic_line_ids.ids
+        self.task2.unlink()
+        self.assertEqual(
+            len(self.env['stock.move'].search([('id', 'in', moves)])), 0)
+        self.assertEqual(
+            len(self.env['account.analytic.line'].search(
+                [('id', 'in', analytics)])), 0)
+        self.task.stock_move_ids.write({'state': 'confirmed'})
+        self.assertEqual(self.task.stock_state, 'confirmed')
+        self.assertEqual(len(self.task.analytic_line_ids), 1)
+        with self.assertRaises(UserError):
+            self.task.stage_id = self.env.ref('project.project_stage_0').id
         self.task.stage_id = self.stage_deployed.id
         analytic_line = self.task.analytic_line_ids[0]
         self.assertAlmostEqual(analytic_line.amount, self.expected_amount)
