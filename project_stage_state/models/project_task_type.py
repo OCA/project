6ca8cdfe551c9b8632b31730a3a607a7ec68d258
Kelly Lougheed <kellylougheed@gmail.com>
2017-02-27 09:34:48 -0800

    project_stage_state: Upgrade to v10

---
+++ project_stage_state/models/project_task_type.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Daniel Reis, 2014
+# GNU Affero General Public License <http://www.gnu.org/licenses/>
+
+from odoo import models, fields
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+    state = fields.Selection(
+        related='stage_id.state', store=True, readonly=True)
