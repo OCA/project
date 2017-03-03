bf7abc1ee5c25517ef95a31a7dcce837a1eb12e0
Kelly Lougheed <kellylougheed@gmail.com>
2017-03-03 09:02:13 -0800

    [10.0] project_task_pull_request: Adds PR URI to project.task

---
+++ project_task_pull_request/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Specialty Medical Drugstore
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import project_task
+from . import project_project
