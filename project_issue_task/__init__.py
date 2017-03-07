496dc12a5f6996ccedcfcb58b308322b770ae4f6
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-07 14:35:40 -0500

    .

--- project_issue_task/__init__.py
+++ project_issue_task/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# (c) 2012 - 2013 Daniel Reis
-# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
