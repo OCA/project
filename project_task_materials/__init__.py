68f84882d5b3126c31a2e7dd16bcb400becb3709
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-07 14:39:59 -0500

    .

--- project_task_materials/__init__.py
+++ project_task_materials/__init__.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
-# © 2012 - 2013 Daniel Reis
-# © 2015 - Antiun Ingeniería S.L. - Sergio Teruel
-# © 2017 - Rigoberto Martínez <rigo1985@gmail.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
