7f4072e5711d59acfdb6f2af5005c1b9f7dd770a
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-07 14:38:54 -0500

    .

--- project_timesheet_time_control/__init__.py
+++ project_timesheet_time_control/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antonio Espinosa
-# © 2016 Pedro M. Baeza
-# © 2016 Sergio Teruel
-# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
 from .hooks import post_init_hook
