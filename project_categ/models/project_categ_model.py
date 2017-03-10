35035eb8dd67602e2c144d9fd4e05bcd4a08541b
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-10 16:09:55 -0500

    [MIG] project_categ: Migrated to 10.0

--- project_categ/models/project_categ_model.py
+++ project_categ/models/project_categ_model.py
@@ -3,24 +3,26 @@
 # © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class ProjectProject(models.Model):
     _inherit = 'project.project'
-    
+
     task_tag_id = fields.Many2one('project.tags', string='Root Tag for Tasks')
-    
+
 
 class ProjectCategory(models.Model):
     _inherit = 'project.tags'
     _order = 'parent_id,name'
     _rec_name = 'complete_name'
 
-    parent_id = fields.Many2one('project.tags', string='Parent Tag', index=True)
-    child_ids = fields.One2many('project.tags', 'parent_id', 
+    parent_id = fields.Many2one('project.tags', string='Parent Tag', 
+                                index=True)
+    child_ids = fields.One2many('project.tags', 'parent_id',
                                 string='Child Categories')
-    complete_name = fields.Char(string='Name', compute='_compute_complete_name')
+    complete_name = fields.Char(string='Name', 
+                                compute='_compute_complete_name')
     code = fields.Char(string='Code', size=10)
 
     @api.multi
@@ -29,7 +31,7 @@ class ProjectCategory(models.Model):
         for tag in self:
             parent_name = tag.parent_id.name and tag.parent_id.name + ' / '
             tag.complete_name = (parent_name or '') + tag.name
-    
+
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
@@ -42,9 +44,8 @@ class ProjectTask(models.Model):
     def _compute_project_task_tag(self):
         for tag in self:
             tag.tag_project = self.project_id.task_tag_id or False
-    
+
     @api.onchange('project_id')
     def _onchange_project(self):
-        res = super(ProjectTask, self)._onchange_project()
+        super(ProjectTask, self)._onchange_project()
         self.tag_ids &= self.project_id.task_tag_id.child_ids
-
