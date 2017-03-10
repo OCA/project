aa89dad3641f768955f6c9d4a2b767d570bdfac2
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-10 12:32:27 -0500

    [MIG] project_categ_issue: Migrated to 10.0

--- project_categ_issue/__manifest__.py
+++ project_categ_issue/__manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-# (c) 2013 Daniel Reis
-# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
+# © 2013 Daniel Reis
+# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Per Project Configurable Categorie on Issues',
     'summary': 'Projects Issues can have an allowed category list',
-    'version': '0.1.0',
+    'version': '10.0.0.1.0',
     "category": "Project Management",
     'description': """\
 Adds to Issues the ability to limit selectable Categories to a Project's
 specific list.
 """,
-    'author': 'Daniel Reis, Tecnativa, Odoo Community Association (OCA)',
+    'author': "Daniel Reis, Tecnativa, Odoo Community Association (OCA)",
     'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
     'depends': [
