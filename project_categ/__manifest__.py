8cb7b185390360bed029c4d8e2bc39605f799032
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-10 12:26:36 -0500

    [MIG] project_categ: Migrated to 10.0

--- project_categ/__manifest__.py
+++ project_categ/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# (c) 2013 Daniel Reis
-# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
+# © 2013 Daniel Reis
+# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Project Configurable Categories',
     'summary': 'Allow for Project specific category lists for Tasks',
-    'version': '0.1.0',
+    'version': '10.0.0.1.0',
     "category": "Project Management",
     'description': """\
 To setup:
@@ -19,7 +19,7 @@ To setup:
 Now make this feature available on Issues or Tasks by installing the
 corresponding extension module.
 """,
-    'author': 'Daniel Reis, Tecnativa, Odoo Community Association (OCA)',
+    'author': "Daniel Reis, Tecnativa, Odoo Community Association (OCA)",
     'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
     'depends': [
--- project_categ/models/project_categ_model.py
+++ project_categ/models/project_categ_model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2013 Daniel Reis
-# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
+# © 2013 Daniel Reis
+# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models, _
