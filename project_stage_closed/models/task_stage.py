d489e9cecb0f559124efe9e95c0af195e42b6e0e
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-03-13 11:59:16 +0100

    project_stage_closed: Migration to 9.0

--- project_stage_closed/models/task_stage.py
+++ project_stage_closed/models/task_stage.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, fields, models
@@ -10,21 +11,19 @@ class ProjectTaskType(models.Model):
 
     closed = fields.Boolean(
         help="Tasks in this stage are considered closed.",
-        default=False)
-
+        default=False,
+    )
     # a small hack to avoid having two fields with the
     # same name, but show the field in the view
     # when sale_service is not installed
-    closed_alias = fields.Boolean(
-        string='Closed', related='closed')
+    closed_alias = fields.Boolean(string='Closed', related='closed')
     closed_alias_visible = fields.Boolean(
-        compute='_compute_closed_alias_visible')
+        compute='_compute_closed_alias_visible',
+    )
 
     @api.multi
     def _compute_closed_alias_visible(self):
         ir_module = self.env['ir.module.module']
         installed = ir_module.search([('name', '=', 'sale_service'),
                                       ('state', '=', 'installed')])
-        visible = not bool(installed)
-        for o in self:
-            o.closed_alias_visible = visible
+        self.update({'closed_alias_visible': not bool(installed)})
