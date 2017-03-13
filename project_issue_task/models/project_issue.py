b34538f691845f6ab8927c6cb806c9a543e0958d
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-13 16:26:40 -0400

    travis error

--- project_issue_task/models/project_issue.py
+++ project_issue_task/models/project_issue.py
@@ -12,13 +12,13 @@ class ProjectIssue(models.Model):
     @api.multi
     def action_create_task(self):
         values = {
-                  'name': _('Report for %s') % self.name,
-                  'issue_id': self.id,
-                  'tag_ids': [(6, False, self.tag_ids.mapped('id'))]
-                  }
+            'name': _('Report for %s') % self.name,
+            'issue_id': self.id,
+            'tag_ids': [(6, False, self.tag_ids.mapped('id'))]
+        }
         fields = ['project_id']    # For optional relational fields
         values.update((field, getattr(self, field).id) for field in fields
-                      if hasattr(self, field) and getattr(self, field))
+            if hasattr(self, field) and getattr(self, field))
         rec_task = self.env['project.task']
         task_id = rec_task.create(values)
         self.task_id = task_id
