3fbe7adfbb70499fa0bc8ec5013f05b3df6af61a
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-13 16:11:39 -0400

    travis error

--- project_issue_task/models/project_issue.py
+++ project_issue_task/models/project_issue.py
@@ -3,7 +3,7 @@
 # © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import  api, models, _
+from odoo import api, models, _
 
 
 class ProjectIssue(models.Model):
@@ -12,13 +12,13 @@ class ProjectIssue(models.Model):
     @api.multi
     def action_create_task(self):
         values = {
-                      'name': _('Report for %s') % self.name,
-                      'issue_id': self.id,
-                      'tag_ids': [(6, False, self.tag_ids.mapped('id'))]
+                  'name': _('Report for %s') % self.name,
+                  'issue_id': self.id,
+                  'tag_ids': [(6, False, self.tag_ids.mapped('id'))]
                   }
         fields = ['project_id']    # For optional relational fields
         values.update((field, getattr(self, field).id) for field in fields
-                            if hasattr(self, field) and getattr(self, field))
+                      if hasattr(self, field) and getattr(self, field))
         rec_task = self.env['project.task']
         task_id = rec_task.create(values)
         self.task_id = task_id
