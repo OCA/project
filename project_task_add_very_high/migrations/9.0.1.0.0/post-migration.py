33bec2e518c29e618d5127dd7abd4c110816a7dd
Andrea <a.stirpe@onestein.nl>
2017-03-15 10:42:20 +0100

    Fix flake8 complaining

--- project_task_add_very_high/migrations/9.0.1.0.0/post-migration.py
+++ project_task_add_very_high/migrations/9.0.1.0.0/post-migration.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+
 def migrate(cr, version):
     # convert priority to avoid inconsistency
     cr.execute(
