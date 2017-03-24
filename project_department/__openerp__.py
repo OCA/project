6ad5216c1970cb848e796840922cba09ede05116
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-03-24 16:59:13 +0100

    Move description in README.rst

--- project_department/__openerp__.py
+++ project_department/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2014 Joël Grand-Guillaume (Camptocamp)
+# Copyright 2014 Joël Grand-Guillaume (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
@@ -7,10 +7,6 @@
     "author": "Camptocamp, Daniel Reis,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "description": """\
-Add Department to Projects and task to corresponding tree, search and form
-views.
-""",
     "website": "http://camptocamp.com",
     "depends": [
         "analytic_base_department",
