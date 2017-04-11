f18a9012560233585dee61ca522a4bd2557916c6
David Vidal <chienandalu@gmail.com>
2017-04-11 09:29:28 +0200

    [MIG] project_task_material: Migration to 10.0 and rename

--- project_task_materials/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Tecnativa - Vicent Cubells
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
-
-from . import test_create_materials_lines
