2fb2caa14466cffd6afd3829e95f9d91ace1ad16
Jerther <jerther@gmail.com>
2017-05-09 10:26:50 -0400

    project_closing: Migration to Odoo 10 and addition of unit tests.

---
+++ project_closing/models/project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+    
+    @api.multi
+    def toggle_active(self):
+        for record in self:
+            record.analytic_account_id.active = not record.active
+        return super(ProjectProject, self).toggle_active()
+
---
+++ project_closing/tests/test_project.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import tests
+
+
+class TestProject(tests.TransactionCase):
+    def setUp(self):
+        tests.TransactionCase.setUp(self)
+        self._project = self.env['project.project'].create({'name': "Some test project"})
+    
+    def test_toggle_active(self):
+        self.assertTrue(self._project.active)
+        self.assertTrue(self._project.analytic_account_id.active)
+        self._project.toggle_active()
+        self.assertFalse(self._project.active)
+        self.assertFalse(self._project.analytic_account_id.active)
+        self._project.toggle_active()
+        self.assertTrue(self._project.active)
+        self.assertTrue(self._project.analytic_account_id.active)
