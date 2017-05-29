7a615fe69ef4e5523e9fcd4e06ae4e795e7f3480
Damien Crier <damdam-s@users.noreply.github.com>
2017-05-29 10:30:56 +0200

    10.0 project timesheet currency (#275)
    * [ADD] initial rev
    
    * [IMP] per project currency cost computation
    
    * [IMP] override default record rules
    
    * [ADD] readme
    
    * [IMP] dependencies
    
    * [FIX] amount_currency computation
    
    * [ADD] tests
    
    * fix comments on #272
    
    * proof read README.rst
    
    * FIX typo in record rule
    
    * Change some stylistic details

---
+++ project_timesheet_currency/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Project Timesheet Currency',
+    'summary': """Multi-currency analytic costs""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'website': 'https://www.camptocamp.com',
+    'depends': [
+        'analytic',
+        'sale_timesheet',
+        'project',
+    ],
+    'data': [
+        'security/record_rules.yml',
+        'security/analytic_security.xml',
+        'security/project_security.xml',
+    ]
+}
