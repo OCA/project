b2a05fcdafb883ff18ceee81946f0cc13d84f8dc
Dave Burkholder <dave@compassfoundation.io>
2017-06-15 08:48:18 -0400

    Remove description key from manifest

--- project_task_department/__manifest__.py
+++ project_task_department/__manifest__.py
@@ -8,14 +8,6 @@
     "author": "Daniel Reis,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "description": """\
-Enables Tasks to store the Department they correspond to.
-By default this will be the Project Department, but it is editable.
-
-(Note that the ``project_department`` also adds a Department field on
-Tasks, but with the sole purpose of making the Project Department
-available on them.)
-""",
     "website": "https://github.com/OCA/project-service",
     "depends": [
         "project_department",
