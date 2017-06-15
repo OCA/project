b009826cd783ac93b006af97ab33682f4de2e6f7
Dave Burkholder <dave@compassfoundation.io>
2017-06-15 08:24:53 -0400

    Updates per @lreficent comments

--- project_department/models/project.py
+++ project_department/models/project.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 # © 2014 Joël Grand-Guillaume (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
