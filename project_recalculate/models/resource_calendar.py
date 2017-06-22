4b75288849c66c4e21a6479ce507fa18e356b375
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-22 14:10:32 +0200

    [MIG] project_recalculate: Migration to 10.0

--- project_recalculate/models/resource_calendar.py
+++ project_recalculate/models/resource_calendar.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 # See README.rst file on addon root folder for license details
 
-from openerp import models, api
+from odoo import models
 from datetime import datetime, timedelta
 
 
 class ResourceCalendar(models.Model):
     _inherit = 'resource.calendar'
 
-    @api.v7
-    def get_working_days_of_date(self, cr, uid, id, start_dt=None, end_dt=None,
+    def get_working_days_of_date(self, start_dt=None, end_dt=None,
                                  leaves=None, compute_leaves=False,
-                                 resource_id=None, default_interval=None,
-                                 context=None):
-        context = dict(context) or {}
-        context['tz'] = 'UTC'
+                                 resource_id=None, default_interval=None):
+        self.ensure_one()
         if start_dt is None:
             start_dt = datetime.now().replace(hour=0, minute=0, second=0)
         if end_dt is None:
@@ -27,10 +24,12 @@ class ResourceCalendar(models.Model):
             else:
                 end_day = current.replace(hour=23, minute=59, second=59)
                 end = end_dt if end_day > end_dt else end_day
-                working_intervals = self.get_working_intervals_of_day(
-                    cr, uid, id, start_dt=current, end_dt=end, leaves=leaves,
+                obj = self.with_context(tz='UTC')
+                working_intervals = obj.get_working_intervals_of_day(
+                    start_dt=current, end_dt=end, leaves=leaves,
                     compute_leaves=compute_leaves, resource_id=resource_id,
-                    default_interval=default_interval, context=context)
+                    default_interval=default_interval,
+                )
                 if working_intervals:
                     days += 1
             next = current + timedelta(days=1)
