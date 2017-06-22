5a9974817757ebb33fd180aeb55ff4d061bb0ea7
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-22 14:10:32 +0200

    [MIG] project_recalculate: Migration to 10.0

--- project_recalculate/__manifest__.py
+++ project_recalculate/__manifest__.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
-# See README.rst file on addon root folder for license details
+# Copyright 2015 Antonio Espinosa
+# Copyright 2015 Endika Iglesias
+# Copyright 2015 Javier Espìnosa
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Project Recalculate",
-    "version": "8.0.1.0.0",
-    "author": "Antiun Ingeniería S.L., "
+    "version": "10.0.1.0.0",
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.antiun.com",
+    "website": "https://www.tecnativa.com",
     "license": "AGPL-3",
     "category": "Project",
-    "depends": ['project'],
+    "depends": [
+        'hr_timesheet',
+    ],
     'data': [
         "views/project_project_view.xml",
         "views/project_task_view.xml",
         "views/project_task_stage_view.xml",
         "wizard/recalculate_wizard.xml",
     ],
-    'installable': False,
+    'installable': True,
 }
