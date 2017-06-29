c7118d61429a17ccf882cf854062c9bf7ad020f8
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-06-29 17:16:01 +0200

    [MIG] project_timeline: Migration to 10.0

--- project_timeline/__manifest__.py
+++ project_timeline/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Project timeline",
     "summary": "Timeline view for projects",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Project Management",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
