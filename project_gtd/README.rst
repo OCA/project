0549a7be18729bd7a9d6069242a541a254345b29
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-08-16 17:15:54 +0200

    [MIG] project_gtd: migration 8.0 > 10.0

---
+++ project_gtd/README.rst
@@ -0,0 +1,71 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+==============
+Project gtd
+==============
+
+This module implements a simple personal to-do list based on tasks. It adds an
+editable list of tasks simplified to the minimum required fields in the project
+application.
+
+The to-do list is based on the *Getting Things Done* methodology. This
+world-wide used methodology is used for personal time management improvement.
+
+*Getting Things Done* (commonly abbreviated as *GTD*) is an action management
+method created by David Allen, and described in a book of the same name.
+
+*GTD* rests on the principle that a person needs to move tasks out of the mind
+by recording them externally. That way, the mind is freed from the job of
+remembering everything that needs to be done, and can concentrate on actually
+performing those tasks.
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to `Project / Configuration / GTD / Timeboxes` to configure your timeboxes.
+#. Here, your new timebox is empty and not visible yet. You need to add tasks in it to make it visible.
+#. Go to `Project / Tasks` and edit a task to set the 'Timebox' field.
+#. Now go to `Project / Tasks (GTD)` to drag and drop your tasks between timeboxes.
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/140/10.0
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/project/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* OpenERP SA
+* Sébastien ALIX <sebastien.alix@osiell.com>
+* Matthieu MÉQUIGNON <matthieu.mequignon@osiell.com>
+
+Do not contact contributors directly about support or help with technical issues.
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- project_gtd/__manifest__.py
+++ project_gtd/__manifest__.py
@@ -1,50 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2004-2010 Tiny SPRL <http://tiny.be>.
+# Copyright 2017 ABF OSIELL <http://osiell.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Todo Lists',
-    'version': '8.0.2.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Project Management',
     'sequence': 100,
     'summary': 'Personal Tasks, Contexts, Timeboxes',
-    'description': """
-Implement the *Getting Things Done* methodology
-===============================================
-
-This module implements a simple personal to-do list based on tasks. It adds an
-editable list of tasks simplified to the minimum required fields in the project
-application.
-
-The to-do list is based on the *Getting Things Done* methodology. This
-world-wide used methodology is used for personal time management improvement.
-
-*Getting Things Done* (commonly abbreviated as *GTD*) is an action management
-method created by David Allen, and described in a book of the same name.
-
-*GTD* rests on the principle that a person needs to move tasks out of the mind
-by recording them externally. That way, the mind is freed from the job of
-remembering everything that needs to be done, and can concentrate on actually
-performing those tasks.
-    """,
-    'author': "OpenERP SA,Odoo Community Association (OCA)",
+    'author': "Odoo SA, Odoo Community Association (OCA), ABF OSIELL",
     'license': 'AGPL-3',
     'depends': ['project'],
     'data': [
@@ -55,9 +20,6 @@ performing those tasks.
         'wizard/project_gtd_fill_view.xml',
     ],
     'demo': ['project_gtd_demo.xml'],
-    'test': ['test/task_timebox.yml'],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
