9d5e1850edfe43c3dc0175dbd62e3be3fd47e7f4
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-08-16 17:15:54 +0200

    [MIG] project_gtd: migration 8.0 > 10.0

--- project_gtd/project_gtd.py
+++ project_gtd/project_gtd.py
@@ -1,156 +1,87 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2004-2010 Tiny SPRL <http://tiny.be>.
+# Copyright 2017 ABF OSIELL <http://osiell.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import fields, osv
-from openerp import tools
+from odoo import api, fields, models, tools
 
 
-class project_gtd_context(osv.Model):
+class ProjectGtdContext(models.Model):
     _name = "project.gtd.context"
     _description = "Context"
-    _columns = {
-        'name': fields.char(
-            'Context', size=64, required=True, translate=True),
-        'sequence': fields.integer(
-            'Sequence',
-            help=("Gives the sequence order when displaying "
-                  "a list of contexts.")),
-    }
-    _defaults = {
-        'sequence': 1
-    }
     _order = "sequence, name"
 
+    name = fields.Char(
+        'Context', required=True, translate=True)
+    sequence = fields.Integer(
+        'Sequence',
+        help="Gives the sequence order when displaying a list of contexts.",
+        default=1)
 
-class project_gtd_timebox(osv.Model):
+
+class ProjectGtdTimebox(models.Model):
     _name = "project.gtd.timebox"
     _order = "sequence"
-    _columns = {
-        'name': fields.char(
-            'Timebox', size=64, required=True, select=1, translate=1),
-        'sequence': fields.integer(
-            'Sequence',
-            help="Gives the sequence order when displaying "
-                 "a list of timebox."),
-    }
+
+    name = fields.Char(
+        'Timebox', size=64, required=True, index=1, translate=1)
+    sequence = fields.Integer(
+        'Sequence',
+        help="Gives the sequence order when displaying "
+             "a list of timebox.")
 
 
-class project_task(osv.Model):
+class ProjectTask(models.Model):
     _inherit = "project.task"
-    _columns = {
-        'timebox_id': fields.many2one(
-            'project.gtd.timebox',
-            "Timebox",
-            help="Time-laps during which task has to be treated"),
-        'context_id': fields.many2one(
-            'project.gtd.context',
-            "Context",
-            help="The context place where user has to treat task"),
-    }
 
-    def _get_context(self, cr, uid, context=None):
-        ids = self.pool.get('project.gtd.context').search(
-            cr, uid, [], context=context)
-        return ids and ids[0] or False
+    @api.model
+    def _get_context(self):
+        contexts = self.env['project.gtd.context'].search([])
+        return contexts and contexts[0] or False
+
+    timebox_id = fields.Many2one(
+        'project.gtd.timebox',
+        "Timebox",
+        help="Time-laps during which task has to be treated")
+    context_id = fields.Many2one(
+        'project.gtd.context',
+        "Context",
+        help="The context place where user has to treat task",
+        default=_get_context)
 
     def _read_group_timebox_ids(
-            self, cr, uid, ids, domain,
-            read_group_order=None, access_rights_uid=None, context=None):
+            self, domain, read_group_order=None, access_rights_uid=None):
         """Used to display all timeboxes on the view."""
-        timebox_obj = self.pool.get('project.gtd.timebox')
-        order = timebox_obj._order
-        access_rights_uid = access_rights_uid or uid
-        timebox_ids = timebox_obj._search(
-            cr, uid, [],
-            order=order, access_rights_uid=access_rights_uid, context=context)
-        result = timebox_obj.name_get(
-            cr, access_rights_uid, timebox_ids, context=context)
+        timebox_model = self.env['project.gtd.timebox']
+        timeboxes = timebox_model.search([])
+        timebox_ids = timeboxes.ids
+        result = timeboxes.name_get()
         # Restore order of the search
         result.sort(
             lambda x, y: cmp(timebox_ids.index(x[0]), timebox_ids.index(y[0])))
-        fold = dict.fromkeys(timebox_ids, False)
+        fold = dict.fromkeys(timeboxes, False)
         return result, fold
-
-    _defaults = {
-        'context_id': _get_context
-    }
 
     _group_by_full = {
         'timebox_id': _read_group_timebox_ids,
     }
 
-    def copy_data(self, cr, uid, id, default=None, context=None):
-        if context is None:
-            context = {}
+    @api.model
+    def copy_data(self, default=None):
         if not default:
             default = {}
-        if not default.get('timebox_id'):    
+        if not default.get('timebox_id'):
             default['timebox_id'] = False
-        if not default.get('context_id'):    
+        if not default.get('context_id'):
             default['context_id'] = False
-        return super(project_task, self).copy_data(
-            cr, uid, id, default, context)
+        return super(ProjectTask, self).copy_data(default)
 
-    def next_timebox(self, cr, uid, ids, *args):
-        timebox_obj = self.pool.get('project.gtd.timebox')
-        timebox_ids = timebox_obj.search(cr, uid, [])
-        if not timebox_ids:
-            return True
-        for task in self.browse(cr, uid, ids):
-            timebox = task.timebox_id
-            if not timebox:
-                self.write(cr, uid, task.id, {'timebox_id': timebox_ids[0]})
-            elif timebox_ids.index(timebox) != len(timebox_ids)-1:
-                index = timebox_ids.index(timebox)
-                self.write(
-                    cr, uid, task.id, {'timebox_id': timebox_ids[index+1]})
-        return True
-
-    def prev_timebox(self, cr, uid, ids, *args):
-        timebox_obj = self.pool.get('project.gtd.timebox')
-        timebox_ids = timebox_obj.search(cr, uid, [])
-        for task in self.browse(cr, uid, ids):
-            timebox = task.timebox_id
-            if timebox:
-                if timebox_ids.index(timebox):
-                    index = timebox_ids.index(timebox)
-                    self.write(
-                        cr, uid, task.id,
-                        {'timebox_id': timebox_ids[index - 1]})
-                else:
-                    self.write(cr, uid, task.id, {'timebox_id': False})
-        return True
-
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
-                        context=None, toolbar=False, submenu=False):
-        if not context:
-            context = {}
-        res = super(project_task, self).fields_view_get(
-            cr, uid, view_id, view_type, context,
-            toolbar=toolbar, submenu=submenu)
-        search_extended = False
-        timebox_obj = self.pool.get('project.gtd.timebox')
-        if (res['type'] == 'search') and context.get('gtd', False):
-            timeboxes = timebox_obj.browse(
-                cr, uid, timebox_obj.search(cr, uid, []), context=context)
+    @api.model
+    def fields_view_get(self, *args, **kwargs):
+        res = super(ProjectTask, self).fields_view_get(*args, **kwargs)
+        timebox_model = self.env['project.gtd.timebox']
+        if (res['type'] == 'search') and self.env.context.get('gtd', False):
+            timeboxes = timebox_model.search([])
             search_extended = ''
             for timebox in timeboxes:
                 filter_ = u"""
@@ -163,5 +94,3 @@ class project_task(osv.Model):
                 '<separator name="gtdsep"/>', search_extended)
 
         return res
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
