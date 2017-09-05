369f1ec5735e4a37ab7f621eeac72653115e5d1e
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-09-05 16:03:43 +0530

    [ADD] project_task_category module

---
+++ project_task_category/models/project_category.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class ProjectCategory(models.Model):
+    """Data Model for Project Category."""
+
+    _name = 'project.category'
+    _inherit = 'project.tags'
