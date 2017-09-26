0e31b4b7b1e3884a374ea2c3038ed2a1a8cc2149
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-09-26 10:45:55 +0200

    cleanup

--- project_definition/models/project_project.py
+++ project_definition/models/project_project.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    term_ids = fields.Many2many('document.definition' , string='Project Terms')
+    term_ids = fields.Many2many('document.definition', string='Project Terms')
