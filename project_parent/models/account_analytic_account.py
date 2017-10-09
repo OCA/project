39f6aa52d022f0232961afcfe703db65cb5146f4
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-10-09 11:44:36 +0530

    [IMP]Improved code

--- project_parent/models/account_analytic_account.py
+++ project_parent/models/account_analytic_account.py
@@ -2,14 +2,13 @@
 # © 2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
     parent_project_id = fields.Many2one(
-        'project.project',
+        comodel_name='project.project',
         string='Parent Analytic Account'
     )
