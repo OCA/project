37a774a5373654f14b36e8d96463f8c2e2a7aa55
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-10-10 19:11:54 +0530

    [ADD] - Added Project Parent module (#326)

---
+++ project_parent/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Project Parent',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'project',
+    'author': 'Elico Corp, Odoo Community Association (OCA)',
+    'website': 'https://www.elico-corp.com/',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_parent.xml',
+    ]
+}
