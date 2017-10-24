8456a89b0e8a9c13eecf2ab14354c88e541472ef
emagdalena <emagdalena@c2i.es>
2017-10-25 00:41:31 +0200

    [11.0][MIG]project_description

---
+++ project_description/models/project.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    description = fields.Html()
