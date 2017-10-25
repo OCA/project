8647e47c9e1bf092b6ee9e24d2f02b5238700b93
emagdalena <emagdalena@c2i.es>
2017-10-25 21:00:38 +0200

    restaurar previo

--- project_task_add_very_high/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import models
-from .hooks import uninstall_hook
