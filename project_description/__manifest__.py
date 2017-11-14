ce0510265b059f77d9663d96729bae3b4e0d7b1a
Eduardo Magdalena <emagdalena@c2i.es>
2017-11-14 11:22:23 +0100

    [MIG] [11.0] project_description

--- project_description/__manifest__.py
+++ project_description/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     "name": "Project Description",
     "summary": "Add a description to projects",
     "version": "11.0.1.0.0",
-    "category": "Project Management",
+    "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Antiun Ingeniería S.L., "
               "Tecnativa, "
