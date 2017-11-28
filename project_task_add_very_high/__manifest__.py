254f9bff29d90ffbe97493ce5efc2db494cb9aaa
Andrea <a.stirpe@onestein.nl>
2017-11-28 16:25:25 +0100

    [MIG] project_task_add_very_high: port to V11

--- project_task_add_very_high/__manifest__.py
+++ project_task_add_very_high/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Project Task Add Very High",
-    "summary": "Adds an extra option 'Very High' on tasks",
-    "version": "10.0.1.0.0",
+    "summary": "Adds extra options 'High' and 'Very High' on tasks",
+    "version": "11.0.1.0.0",
     "author": "Onestein, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "category": "Project Management",
-    "website": "http://www.onestein.eu",
+    "category": "Project",
+    "website": "https://github.com/OCA/project/",
     "depends": ["project"],
     "installable": True,
     "uninstall_hook": "uninstall_hook"
