81c00f08275c702864230eba9159c869c774d8bd
emagdalena <emagdalena@c2i.es>
2017-12-01 02:54:47 +0100

    [11.0][MIG] project_description

--- project_description/models/project.py
+++ project_description/models/project.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2015-2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
