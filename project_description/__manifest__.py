9cedfe70bec22465bb8ccc406b751070dbc5e1c2
emagdalena <emagdalena@c2i.es>
2017-12-01 02:54:47 +0100

    [11.0][MIG] project_description

--- project_description/__manifest__.py
+++ project_description/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2015-2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Project Description",
     "summary": "Add a description to projects",
-    "version": "10.0.1.0.0",
-    "category": "Project Management",
-    "website": "https://www.tecnativa.com",
-    "author": "Antiun Ingeniería S.L., "
-              "Tecnativa, "
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "Tecnativa, "
+              "C2i Change 2 improve, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
