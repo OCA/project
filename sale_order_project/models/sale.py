ab4f45df77e826066f6d6cb83b6761c592142d7e
Abraham Anes <abraham@studio73.es>
2017-12-05 10:44:07 +0100

    [FIX] sale_order_project: Flake8

--- sale_order_project/models/sale.py
+++ sale_order_project/models/sale.py
@@ -3,7 +3,7 @@
 # © 2014 Akretion - Benoît GUILLOT <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 from datetime import date
 
 
