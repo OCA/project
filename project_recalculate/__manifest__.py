81a45b50c0a5fc25b6998d61d414f1c68acde959
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-20 10:15:37 +0100

    [FIX] project_recalculate: Avoid error on no resource nor calendar

--- project_recalculate/__manifest__.py
+++ project_recalculate/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Project Recalculate",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "website": "https://www.tecnativa.com",
