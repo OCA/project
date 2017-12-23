6a2d2f7c72934acbc0e286c35b092fc4267425af
Duc Dao <ducdd@komit-consulting.com>
2017-12-24 01:15:25 +0700

    [MIG] project_task_default_stage: Migration to 11.0

--- project_task_default_stage/models/project_task_type.py
+++ project_task_default_stage/models/project_task_type.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Incaser Informatica S.L. - Sergio Teruel
 # (c) 2015 Incaser Informatica S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectTaskType(models.Model):
