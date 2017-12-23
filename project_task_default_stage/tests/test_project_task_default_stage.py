3f4285de166163b20778128eb5b9c50ef67caf2a
Duc Dao <ducdd@komit-consulting.com>
2017-12-24 01:15:25 +0700

    [MIG] project_task_default_stage: Migration to 11.0

--- project_task_default_stage/tests/test_project_task_default_stage.py
+++ project_task_default_stage/tests/test_project_task_default_stage.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Incaser Informatica S.L. - Sergio Teruel
 # (c) 2015 Incaser Informatica S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo.tests.common import TransactionCase
 
 
