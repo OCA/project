a703136fc70db08e7ba40558fb7dbb7b2ca030a7
Artem Kostyuk <a.kostyuk@mobilunity.com>
2017-12-27 12:12:33 +0200

    project_department: Migrate to 11.0

--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Joël Grand-Guillaume (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
-    "version": "10.0.1.0.0",
-    "author": "Camptocamp, Daniel Reis,Odoo Community Association (OCA)",
+    "version": "11.0.1.0.0",
+    "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "website": "http://camptocamp.com",
+    "website": "https://github.com/OCA/project",
     "depends": [
         "analytic_base_department",
         "project",
     ],
     "data": ["views/project.xml"],
-    "auto_install": True,
-    'installable': True,
 }
