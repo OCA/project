309f219ead536f224b2b396c1244375395999b07
Martin Nicolas Cuesta <cuesta.martin.n@hotmail.com>
2018-04-13 17:43:53 -0300

    Project Timeline to V11

--- project_timeline/__manifest__.py
+++ project_timeline/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 {
     "name": "Project timeline",
     "summary": "Timeline view for projects",
-    "version": "10.0.1.1.0",
+    "version": "11.0.1.0.0",
     "category": "Project Management",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
