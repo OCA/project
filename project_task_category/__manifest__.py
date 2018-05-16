a0dc03213fe82810d092b3bfc65c897a92a11ff8
Joyce <joycelhh@163.com>
2018-05-16 14:04:24 +0800

    add external_dependencies openupgradelib

--- project_task_category/__manifest__.py
+++ project_task_category/__manifest__.py
@@ -8,6 +8,11 @@
     'version': '10.0.1.1.0',
     'author': 'Elico Corp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
+    'external_dependencies': {
+         'python': [
+             'openupgradelib',
+         ]
+    },
     'category': 'Project Management',
     'website': 'https://www.elico-corp.com',
     'depends': [
