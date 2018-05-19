9efe5aeccf3613d4f35198c1879d280fe127859b
ernesto <ernesto.tejeda87@gmail.com>
2018-05-18 23:28:55 -0400

    [MIG] project_stage_closed: Migration to 11.0

--- project_stage_closed/__manifest__.py
+++ project_stage_closed/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -10,9 +9,9 @@
     'author': 'ACSONE SA/NV,'
               'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': "http://acsone.eu",
+    'website': "https://github.com/OCA/project",
     'category': 'Project Management',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'project',
--- project_stage_closed/models/task_stage.py
+++ project_stage_closed/models/task_stage.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
