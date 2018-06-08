029b15a128539b828d7793d97b7a4c58596f7f13
Joyce <joycelhh@163.com>
2018-06-08 13:16:12 +0800

    2018

--- project_task_category/migrations/10.0.1.0.0/pre-migration.py
+++ project_task_category/migrations/10.0.1.0.0/pre-migration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
+# © 2018 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
