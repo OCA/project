cbb03f0e46e0cd76248ecb729e5d21e44c67d803
aheficent <ahenriquez@eficent.com>
2018-08-03 14:08:12 +0200

    [MIG]project_wbs_task to v11

--- project_wbs_task/model/__init__.py
+++ project_wbs_task/model/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import project_task
--- project_wbs_task/model/project_task.py
+++ project_wbs_task/model/project_task.py
@@ -1,47 +1,12 @@
-# -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2015 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
-
-
-# class TaskType(models.Model):
-#     _inherit = 'project.task.type'
-
-#     _TASK_STATE = [
-#         ('draft', 'New'),
-#         ('open', 'In Progress'),
-#         ('pending', 'Pending'),
-#         ('done', 'Done'),
-#         ('cancelled', 'Cancelled')
-#     ]
-
-#     state = fields.Selection(
-#         _TASK_STATE, 'Related Status', required=False,
-#         help="The status of your document is automatically changed regarding "
-#              "the selected stage. For example, if a stage is related to the "
-#              "status 'Close', when your document reaches this stage, it is "
-#              "automatically closed."
-#     )
+from odoo import api, fields, models
 
 
 class Task(models.Model):
     _inherit = 'project.task'
-
-    # @api.model
-    # def _get_composition_mode_selection(self):
-    #     return [
-    #         ('comment', 'Post on a document'),
-    #         ('mass_mail', 'Email Mass Mailing'),
-    #         ('mass_post', 'Post on Multiple Documents')
-    #     ]
-    #
-    # composition_mode = fields.Selection(
-    #     selection=_get_composition_mode_selection,
-    #     string='Composition mode', default='comment'
-    # )
 
     @api.multi
     def _project_complete_wbs_name(self):
