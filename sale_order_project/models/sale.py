a486ecf5c8c686c27e2d3396fd4628be9ac56ac2
José L. S. A <alagunasalahaddin@live.com>
2018-08-06 15:39:23 +0300

    :fire: reverted method to model file.

--- sale_order_project/models/sale.py
+++ sale_order_project/models/sale.py
@@ -1,8 +1,10 @@
 # © 2014 Akretion - Sébastien BEAU <sebastien.beau@akretion.com>
 # © 2014 Akretion - Benoît GUILLOT <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from datetime import date
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning
 
 
 class SaleOrder(models.Model):
@@ -23,3 +25,30 @@ class SaleOrder(models.Model):
         string='Project',
         compute='_compute_related_project_id'
     )
+
+    @api.model
+    def _prepare_project_vals(self, order):
+        name = "%s - %s - %s" % (
+            order.partner_id.name,
+            date.today().year,
+            order.name)
+        return {
+            'user_id': order.user_id.id,
+            'name': name,
+            'partner_id': order.partner_id.id,
+        }
+
+    @api.multi
+    def action_create_project(self):
+        project_obj = self.env['project.project']
+        for order in self:
+            if order.related_project_id:
+                raise Warning(_(
+                    'There is a project already related with this sale order.'
+                ))
+            vals = self._prepare_project_vals(order)
+            project = project_obj.create(vals)
+            order.write({
+                'analytic_account_id': project.analytic_account_id.id
+            })
+        return True
--- sale_order_project/wizard/sale_order_createproject.py
+++ sale_order_project/wizard/sale_order_createproject.py
@@ -18,29 +18,4 @@ class SaleOrderCreateProject(models.TransientModel):
     sale_order_id = fields.Many2one('crm.lead', string='Lead', domain=[('type', '=', 'lead')])
     related_project_id = fields.Many2one('project.project', string='Project')
 
-    @api.model
-    def _prepare_project_vals(self, order):
-        name = "%s - %s - %s" % (
-            order.partner_id.name,
-            date.today().year,
-            order.name)
-        return {
-            'user_id': order.user_id.id,
-            'name': name,
-            'partner_id': order.partner_id.id,
-        }
 
-    @api.multi
-    def action_create_project(self):
-        project_obj = self.env['project.project']
-        for order in self:
-            if order.related_project_id:
-                raise Warning(_(
-                    'There is a project already related with this sale order.'
-                ))
-            vals = self._prepare_project_vals(order)
-            project = project_obj.create(vals)
-            order.write({
-                'analytic_account_id': project.analytic_account_id.id
-            })
-        return True
