95df77e661e06fde1a4cda31c98917959d721ed5
José L. S. A <alagunasalahaddin@live.com>
2018-08-07 09:10:44 +0300

    :sunny: added wizard to init file.

--- sale_order_project/wizard/sale_order_createproject.py
+++ sale_order_project/wizard/sale_order_createproject.py
@@ -1,6 +1,4 @@
-from datetime import date
 from odoo import api, fields, models, _
-from odoo.exceptions import Warning
 
 
 class SaleOrderCreateProject(models.TransientModel):
@@ -15,7 +13,25 @@ class SaleOrderCreateProject(models.TransientModel):
             result['sale_order_id'] = sale_order_id
         return result
 
-    sale_order_id = fields.Many2one('crm.lead', string='Lead', domain=[('type', '=', 'lead')])
+    sale_order_id = fields.Many2one('sale.order', string='Order', domain=[('type', '=', 'order')])
     related_project_id = fields.Many2one('project.project', string='Project')
+
+    @api.multi
+    def action_create_project_task(self):
+        self.ensure_one()
+        # get the lead to transform
+        order = self.sale_order_id
+        project = self.related_project_id
+
+        # if related_project_id is empty
+            # create new project.project
+            return action_create_project(self)
+        # else
+            # update sales.order.related_project_id with the selected project.project.id
+            vals = {
+                'related_project_id': self.related_project_id,
+            }
+            order.write(vals)
+            return True
 
 
