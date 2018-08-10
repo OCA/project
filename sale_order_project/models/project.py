40c13258e7b20e30c7ae3b7e0cbaa8dc5e3c9135
José L. S. A <alagunasalahaddin@live.com>
2018-08-10 16:53:06 +0300

    :sunny: [WIP] Creating the action for button.

--- sale_order_project/models/project.py
+++ sale_order_project/models/project.py
@@ -8,13 +8,34 @@ from odoo import (
 class Project(models.Model):
     _inherit = 'project.project'
 
-    sale_order_count = fields.Integer(compute='_compute_sale_orders_count', string="Number of sale orders")
-
     def _compute_sale_orders_count(self):
         SaleOrder = self.env['sale.order']
         for project in self:
             project.sale_order_count = SaleOrder.search_count([
-                '&',
-                ('res_model', '=', 'project.project'), ('res_id', '=', project.id),
+                ('related_project_id', '=', project.analytic_account_id.id)
             ])
 
+    @api.multi
+    def sale_order_tree_view(self):
+        self.ensure_one()
+        domain = [
+            ('related_project_id', '=', self.ids)
+        ]
+        return {
+            'name': _('Sale Orders'),
+            'domain': domain,
+            'related_project_id': 'sale.order',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'help': _('''<p class="oe_view_nocontent_create">
+                            Sale Orders can be related to your project.</p><p>
+                            Go to your Sale Order and to click in the respective button 
+                            to relate Sale Orders with your Project.
+                        </p>'''),
+            'limit': 80,
+            'context': "{'default_related_project_id': '%s'}" % (self.id)
+        }
+
+    sale_order_count = fields.Integer(compute='_compute_sale_orders_count', string="Number of sale orders")
