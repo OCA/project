e78bfe3da915112d7cb0e3a56d885778ab7b5c31
aheficent <ahenriquez@eficent.com>
2018-09-05 10:00:14 +0200

    [FIX]Readme

--- project_wbs_task/model/__init__.py
+++ project_wbs_task/model/__init__.py
@@ -1,5 +1,1 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-# Copyright 2015 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import project_task
