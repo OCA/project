9655a0a03ccca37f098739440407e3b4e544c336
cubells <vicent@vcubells.net>
2018-09-21 12:34:26 +0200

    [MIG] project_task_material_stock: Migration to 11.0
    [UPD] README.rst
    
    [UPD] Update project_task_material_stock.pot

--- project_task_material_stock/models/project.py
+++ project_task_material_stock/models/project.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# Copyright 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# Copyright 2015 Tecnativa - Sergio Teruel
+# Copyright 2015 Tecnativa - Carlos Dauden
 # Copyright 2016-2017 Tecnativa - Vicent Cubells
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import fields, models
