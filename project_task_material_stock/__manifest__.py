6aaed364f4c2be711704ef90486923d4fe5da200
cubells <vicent@vcubells.net>
2018-09-21 12:34:26 +0200

    [MIG] project_task_material_stock: Migration to 11.0

--- project_task_material_stock/__manifest__.py
+++ project_task_material_stock/__manifest__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# Copyright 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# Copyright 2015 Tecnativa - Sergio Teruel
+# Copyright 2015 Tecnativa - Carlos Dauden
 # Copyright 2016-2017 Tecnativa - Vicent Cubells
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -8,9 +7,9 @@
     "name": "Project Task Material Stock",
     "summary": "Create stock and analytic moves from "
                "record products spent in a Task",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Project Management",
-    "website": "https://www.tecnativa.com/",
+    "website": "https://github.com/OCA/project/",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
--- project_task_material_stock/models/__init__.py
+++ project_task_material_stock/models/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 from . import project
 from . import project_task
