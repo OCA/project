7920b43fdd68ea86ac76d6d0dba24a52caec0e0a
ernesto <ernesto.tejeda87@gmail.com>
2018-09-25 09:56:08 -0400

    project_task_employee: Add unit test

--- project_task_employee/tests/test_project_task.py
+++ project_task_employee/tests/test_project_task.py
@@ -23,3 +23,23 @@ class TestProjectTask(TransactionCase):
         self.assertEqual(self.task_2.employee_domain_ids, jth + root)
         self.assertEqual(self.task_7.employee_domain_ids, root)
         self.assertEqual(self.task_1.employee_domain_ids, jth)
+
+    def test_change_employee_domain_ids_and_employee_id(self):
+        with self.env.do_in_onchange():
+
+            self.task_3.user_id = False
+            self.assertFalse(self.task_3.user_id)
+
+            self.task_3.employee_id = self.env.ref("hr.employee_root")
+            self.task_3._onchange_employee_id()
+            user = self.env.ref("base.user_root")
+            self.assertEqual(self.task_3.user_id, user)
+
+            category_5 = self.env.ref("hr.employee_category_5")
+            self.task_3.employee_category_id = category_5
+            self.task_3._onchange_employee_domain_ids()
+            self.assertFalse(self.task_3.employee_id)
+
+            self.assertEqual(self.task_3.user_id, user)
+            self.task_3._onchange_employee_id()
+            self.assertFalse(self.task_3.user_id)
