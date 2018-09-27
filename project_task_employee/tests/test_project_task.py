4ea656a5564c514577e08898226c9ae88ee5e157
ernesto <ernesto.tejeda87@gmail.com>
2018-09-27 07:50:27 -0400

    project_task_employee: Refactoring code

--- project_task_employee/tests/test_project_task.py
+++ project_task_employee/tests/test_project_task.py
@@ -6,19 +6,14 @@ from odoo.tests.common import TransactionCase
 class TestProjectTask(TransactionCase):
     def setUp(self):
         super(TestProjectTask, self).setUp()
-
-        self.task_3 = self.env['project.task'].browse(
-            self.ref("project_task_employee.restricted_task_3"))
-        self.task_2 = self.env['project.task'].browse(
-            self.ref("project_task_employee.restricted_task_2"))
-        self.task_7 = self.env['project.task'].browse(
-            self.ref("project_task_employee.restricted_task_7"))
-        self.task_1 = self.env['project.task'].browse(
-            self.ref("project_task_employee.restricted_task_1"))
+        self.task_3 = self.env.ref("project_task_employee.restricted_task_3")
+        self.task_2 = self.env.ref("project_task_employee.restricted_task_2")
+        self.task_7 = self.env.ref("project_task_employee.restricted_task_7")
+        self.task_1 = self.env.ref("project_task_employee.restricted_task_1")
 
     def test_employee_domain_ids(self):
-        jth = self.env['hr.employee'].browse(self.ref("hr.employee_jth"))
-        root = self.env['hr.employee'].browse(self.ref("hr.employee_root"))
+        jth = self.env.ref("hr.employee_jth")
+        root = self.env.ref("hr.employee_root")
 
         self.assertEqual(self.task_3.employee_domain_ids, jth + root)
         self.assertEqual(self.task_2.employee_domain_ids, jth + root)
