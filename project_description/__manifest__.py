0b5d2a5c5629c05612435723e42175595aec1b06
emagdalena <emagdalena@c2i.es>
2018-10-09 22:17:45 +0200

    [MIG] project_description: Migration to 12.0

--- project_description/__manifest__.py
+++ project_description/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Description",
     "summary": "Add a description to projects",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Tecnativa, "
