1251d9294d42c2089b234ff9dbddd5536c258ce5
Andrea <a.stirpe@onestein.nl>
2018-10-09 09:00:45 +0200

    [12.0][MIG] project_task_add_very_high

--- project_task_add_very_high/models/project_task.py
+++ project_task_add_very_high/models/project_task.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
+# Copyright 2016-2018 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
