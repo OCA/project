ad1e30d19c46b524d6c0ea328a9b74507a0e9fef
Dennis Sluijk <d.sluijk@onestein.nl>
2018-10-11 10:12:33 +0200

    [11.0][FIX] Copying dependencies in project_task_dependency (#425)

--- project_task_dependency/models/project_task.py
+++ project_task_dependency/models/project_task.py
@@ -72,3 +72,14 @@ class ProjectTask(models.Model):
             raise ValidationError(
                 _('You cannot create recursive dependencies between tasks.')
             )
+
+    @api.multi
+    def copy(self, default=None):
+        self.ensure_one()
+        res = super(ProjectTask, self).copy(default)
+        if self.env.context.get('project_copy'):
+            self.env['project.task.copy.map'].create({
+                'old_task_id': self.id,
+                'new_task_id': res.id
+            })
+        return res
