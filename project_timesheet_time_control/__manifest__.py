db7da2f6e54c8ffc485685e2ab10292ad33b12f0
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 08:29:16 -0500

    [MIG] project_timesheet_time_control: Migration to 12.0

--- project_timesheet_time_control/__manifest__.py
+++ project_timesheet_time_control/__manifest__.py
@@ -1,18 +1,21 @@
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2016 Tecnativa - Sergio Teruel
 # Copyright 2016-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Project timesheet time control',
-    'version': '11.0.1.1.0',
+    'version': '12.0.1.0.0',
+    'development_status': 'Mature',
     'category': 'Project',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
+    'maintainers': ['ernestotejeda'],
     'website': 'https://github.com/OCA/project',
     'depends': [
-        'hr_timesheet',
-        'project_stage_closed',
+        'hr_timesheet_task_stage',
+        'hr_timesheet_task_domain',
     ],
     'data': [
         'views/account_analytic_line_view.xml',
