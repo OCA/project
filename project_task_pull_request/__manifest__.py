e45b323c24b0e9512595e6a1e331370fdde45b5b
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 01:52:43 -0500

    [MIG] project_task_pull_request: Migration to 12.0

--- project_task_pull_request/__manifest__.py
+++ project_task_pull_request/__manifest__.py
@@ -1,12 +1,15 @@
 # Copyright 2017 Specialty Medical Drugstore
+# Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Task Pull Request",
     "summary": "Adds a field for a PR URI to project tasks",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Project Management",
-    "website": "https://odoo-community.org/",
-    "author": "SMDrugstore, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "author": "SMDrugstore, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
