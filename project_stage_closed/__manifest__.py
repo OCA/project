5a1c7d5ced27d9a14c8e4bb805fb653fa0a936dd
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 01:06:33 -0500

    [MIG] project_stage_closed: Migration to 12.0

--- project_stage_closed/__manifest__.py
+++ project_stage_closed/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2015 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -11,7 +12,7 @@
               'Odoo Community Association (OCA)',
     'website': "https://github.com/OCA/project",
     'category': 'Project Management',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'project',
