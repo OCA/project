7a0819f8b3712d6cb57bf04278f219dd09b19fe7
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-17 18:20:28 +0100

    [ADD] project_hr: Link HR with project
    Project HR
    ==========
    
    This is a technical module for adding some HR information to projects and
    provide technical fields with HR information:
    
    - Employee categories at project and task level.
    - Employee linked to the user assigned to a task.
    - Employee categories at user level.
    
    These fields can be used later for extra features like task scheduling or
    security restrictions.
    
    It also provides some facilities on project management:
    
    * Limit selection of assigned users according selected employee category at
      task level.
    * Limit selection of employee categories at task level depending on the
      selected categories at project level. All are shown if no categories at
      project level.
    
    Configuration
    =============
    
    For setting employee categories at project level:
    
    1. Go to *Project > Dashboard*.
    2. Click on the 3 vertical dots for unfolding options and select "Settings".
    3. Put the wanted employee categories on the field "Employee Categories".
    
    For setting employee categories:
    
    1. Go to *Project > Search > Tasks*.
    2. Select or create a new task.
    3. Put the wanted employee categories on the field "Employee categories".
    4. If there's already some employee categories selected at project level, those
       will be the only selectable ones in the task.
    
    Usage
    =====
    
    1. Go to *Project > Search > Tasks*.
    2. If there's an employee category selected in the task, you will only be able
       to select those users whose employee belongs to that category.
    
    Known issues / Roadmap
    ======================
    
    * Limit project and task visibility according employee categories through
      overriding security methods (as through record rules is not possible without
      modifying existing ones).

---
+++ project_hr/tests/test_project_hr.py
@@ -0,0 +1,77 @@
+# Copyright 2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import SavepointCase
+from odoo.exceptions import ValidationError
+
+
+class TestProjectHr(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user_model = cls.env['res.users']
+        cls.user = cls.user_model.create({
+            'login': 'test_project_hr',
+            'name': 'Test user',
+        })
+        cls.user_2 = cls.user_model.create({
+            'login': 'test_project_hr_2',
+            'name': 'Test user 2',
+        })
+        cls.hr_category = cls.env['hr.employee.category'].create({
+            'name': 'Test employee category',
+        })
+        cls.hr_category_2 = cls.env['hr.employee.category'].create({
+            'name': 'Test employee category 2',
+        })
+        cls.employee = cls.env['hr.employee'].create({
+            'name': 'Test employee',
+            'user_id': cls.user.id,
+            'category_ids': [(6, 0, cls.hr_category.ids)],
+        })
+        cls.project = cls.env['project.project'].create({
+            'name': 'Test project',
+            'hr_category_ids': [(4, cls.hr_category.id)]
+        })
+        cls.task = cls.env['project.task'].create({
+            'name': 'Test task',
+            'project_id': cls.project.id,
+            'hr_category_ids': [(4, cls.hr_category.id)],
+            'user_id': cls.user.id,
+        })
+
+    def test_user(self):
+        self.assertEqual(self.user.hr_category_ids, self.hr_category)
+        self.employee.category_ids = [(4, self.hr_category_2.id)]
+        self.assertEqual(
+            self.user.hr_category_ids, self.hr_category + self.hr_category_2,
+        )
+
+    def test_task(self):
+        self.assertEqual(self.task.employee_id, self.employee)
+        self.assertEqual(self.task.allowed_hr_category_ids, self.hr_category)
+        self.project.hr_category_ids = [(4, self.hr_category_2.id)]
+        self.assertEqual(self.task.allowed_hr_category_ids,
+                         self.hr_category + self.hr_category_2)
+        self.assertEqual(self.task.allowed_user_ids, self.user)
+        self.env['hr.employee'].create({
+            'name': 'Test employee 2',
+            'user_id': self.user_2.id,
+            'category_ids': [(6, 0, self.hr_category.ids)],
+        })
+        self.assertEqual(self.task.allowed_user_ids, self.user + self.user_2)
+        # Test _check_employee_category_user constraint
+        with self.assertRaises(ValidationError):
+            self.task.hr_category_ids = [(4, self.hr_category_2.id)]
+        with self.assertRaises(ValidationError):
+            self.task.user_id = self.user_2.id
+        # Test _check_employee_category_project constraint
+        self.project.hr_category_ids = [(4, self.hr_category_2.id)]
+        with self.assertRaises(ValidationError):
+            self.task.hr_category_ids = [(4, self.hr_category_2.id)]
+
+    def test_task_project_wo_categories(self):
+        self.project.hr_category_ids = False
+        self.assertTrue(self.task.allowed_hr_category_ids)
+        # This operation shouldn't give error
+        self.task.hr_category_ids = [(4, self.hr_category.id)]
