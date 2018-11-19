26ea57c1e8f2193dbfd8a598e449a4c158f5e2cd
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-19 16:10:12 +0200

    [MIG] project_key: Migration to 12.0

--- project_key/__manifest__.py
+++ project_key/__manifest__.py
@@ -3,9 +3,9 @@
 
 {
     "name": "Project key",
-    "summary": "Module decorates projects and tasks with ``key`` field",
+    "summary": "Module decorates projects and tasks with Project Key",
     "category": "Project",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "author": "Modoolar, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project/",
