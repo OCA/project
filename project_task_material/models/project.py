601ede9596f7f20a72c20c8af69e635501a197c8
Anand Kansagra <kansagraanand@hotmail.com>
2018-12-05 10:22:45 +0530

    [MIG] project_task_material: Migration to 12.0

--- project_task_material/models/project.py
+++ project_task_material/models/project.py
@@ -2,21 +2,25 @@
 # Copyright 2015 - Antiun Ingeniería S.L. - Sergio Teruel
 # Copyright 2016 - Tecnativa - Vicent Cubells
 # Copyright 2018 - Brain-tec AG - Carlos Jesus Cebrian
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
 class Task(models.Model):
+    """Added Material Used in the Project Task."""
+
     _inherit = "project.task"
 
     material_ids = fields.One2many(
         comodel_name='project.task.material', inverse_name='task_id',
-        string='Material used')
+        string='Material Used')
 
 
 class ProjectTaskMaterial(models.Model):
+    """Added Product and Quantity in the Task Material Used."""
+
     _name = "project.task.material"
     _description = "Task Material Used"
 
