ecdf1596c77a60024a85497bc4c76a28dde1a044
cubells <vicent@vcubells.net>
2018-12-11 13:02:08 +0100

    project_category: Migration to 11.0

---
+++ project_category/models/project_project.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+
+    type_id = fields.Many2one(
+        comodel_name='project.type',
+        string='Type',
+        copy=False,
+    )
