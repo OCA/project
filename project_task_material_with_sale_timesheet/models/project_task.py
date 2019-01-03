145da2144858e06b7f38c04888d7f1e6ce3b2a23
jcmontoya <jcarlosmontoyach@gmail.com>
2019-01-03 17:10:27 +0100

    [Add][11.0] project_task_material_with_sale_timesheet compatibility

---
+++ project_task_material_with_sale_timesheet/models/project_task.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Praxya - Juan Carlos Montoya
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, models
+
+
+class ProjectTaskMaterial(models.Model):
+    _inherit = "project.task.material"
+
+    @api.multi
+    def create_analytic_line(self):
+        super(ProjectTaskMaterial, self.with_context(
+            norecompute_amount=True
+        )).create_analytic_line()
