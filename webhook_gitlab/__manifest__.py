ceef1c025406a64aae7d65e24002e1a4b3c545bf
Alan Ramos <alan.ramos@jarsa.com.mx>
2019-01-04 22:15:18 -0600

    [REF] helpdesk_ticket_merge: changes to migrate to V12

--- webhook_gitlab/__manifest__.py
+++ webhook_gitlab/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018, Jarsa Sistemas, S.A. de C.V.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Webhook for Gitlab',
