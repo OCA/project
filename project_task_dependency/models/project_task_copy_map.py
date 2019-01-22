f21a8ecbe7ee16739547d8397c15bf474a2208db
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-01-22 13:50:47 +0100

    [MIG] Migrate module project_task_dependency to v12.0

--- project_task_dependency/models/project_task_copy_map.py
+++ project_task_dependency/models/project_task_copy_map.py
@@ -6,6 +6,7 @@ from odoo import fields, models
 
 class ProjectTaskCopyMap(models.TransientModel):
     _name = 'project.task.copy.map'
+    _description = 'Project Task Copy Map'
 
     old_task_id = fields.Many2one(
         comodel_name='project.task',
