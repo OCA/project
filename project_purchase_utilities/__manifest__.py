1c28d6f10bcb4c3928729244396810022d400101
oihane <oihanecruce@gmail.com>
2019-01-28 17:28:06 +0100

    [ADD] New module project_purchase_utilities (#141)
    * [ADD] New module project_purchase_utilities
    
    * [IMP] project_purchase_utilities: add tests
    
    * [IMP] project_purchase_utilities: improved tests

---
+++ project_purchase_utilities/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Purchase Utilities",
+    "version": "11.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "purchase",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "data": [
+        "views/project_project_view.xml",
+    ],
+    "installable": True,
+}
