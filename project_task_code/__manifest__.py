b617a5fc9621e94fc1b403711ab6c14ddcfeb7bd
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-02-11 09:16:53 +0200

    [FIX] project_task_code: invalid variable

--- project_task_code/__manifest__.py
+++ project_task_code/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Sequential Code for Tasks',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Project Management',
     'author': 'OdooMRP team, '
               'AvanzOSC, '
