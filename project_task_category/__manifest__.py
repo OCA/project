4d0fc7fac48e87832b415f0d85d14bbac943568c
Ruter Lv <ruter.lv@elico-corp.com>
2019-03-29 14:53:22 +0800

    [MIG] Migrate project_task_category from 10.0 to 12.0

--- project_task_category/__manifest__.py
+++ project_task_category/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2017-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Project Task Categories',
     'summary': 'Allow unique category for Tasks',
-    'version': '10.0.1.1.0',
+    'version': '12.0.1.1.0',
     'author': 'Elico Corp, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'category': 'Project Management',
     'website': 'https://github.com/OCA/project',
     'depends': [
--- project_task_category/models/project_task.py
+++ project_task_category/models/project_task.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2017 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2017-2019 Elico Corp (https://www.elico-corp.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import fields, models
 
