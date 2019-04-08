601f5e6c007a6efd3aa18ea25b956e1b7e0b105f
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-04-08 13:14:16 +0300

    [IMP] Don't allow quick-create on assignments

--- project_role/__manifest__.py
+++ project_role/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Project Roles',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'Project',
     'website': 'https://github.com/OCA/project',
     'author':
