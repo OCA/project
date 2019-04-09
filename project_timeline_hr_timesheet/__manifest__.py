273acd00aeffafbd8c21a3b95273b77823703fa3
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-04-09 08:49:42 +0300

    [FIX] project_timeline_hr_timesheet: fix scss

--- project_timeline_hr_timesheet/__manifest__.py
+++ project_timeline_hr_timesheet/__manifest__.py
@@ -8,7 +8,7 @@
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/project',
     'category': 'Project Management',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'depends': [
         'project_timeline',
         'hr_timesheet'
