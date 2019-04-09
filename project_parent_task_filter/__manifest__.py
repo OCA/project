0e8453ed27f84b47bdca2b412e725f81512b1fda
emagdalena <emagdalena@c2i.es>
2019-04-09 02:37:14 +0200

    [12.0][ADD]project_parent_task_filter

--- project_parent_task_filter/__manifest__.py
+++ project_parent_task_filter/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 C2i Change 2 improve - Eduardo Magdalena <emagdalena@c2i.es>
+# Copyright 2017 C2i Change 2 improve - Eduardo Magdalena <emagdalena@c2i.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
