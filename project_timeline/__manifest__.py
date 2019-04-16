990167d9c82e9d399f8c7f873bdad7124ea3feca
emagdalena <emagdalena@c2i.es>
2019-04-16 15:12:37 +0200

    [12.0][IMP] project_timeline - Enable subtasks

--- project_timeline/__manifest__.py
+++ project_timeline/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Project timeline",
     "summary": "Timeline view for projects",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Project Management",
     "website": "https://github.com/OCA/project",
     "author": "Tecnativa, Onestein, "
