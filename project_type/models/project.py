86da91fc4469ecf04771453ffd5e04ccc102c92e
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2019-04-18 16:09:25 -0500

    [FIX] Project Type Field Name
    Changed the field 'project_type' to 'project_type_id'

--- project_type/models/project.py
+++ project_type/models/project.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class Project(models.Model):
     _inherit = 'project.project'
 
-    project_type = fields.Many2one('project.type',
+    project_type_id = fields.Many2one('project.type',
                                    string="Project Type",
                                    help="The type of Project",
                                    required=True)
