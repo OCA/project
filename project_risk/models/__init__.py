ddeca8597c7ecb099da94614c163c2f9d12ed547
tarteo <d.sluijk@onestein.nl>
2019-04-26 14:35:16 +0200

    [ADD] project_risk
    [FIX] Lint
    
    [ADD] Tests
    
    [IMP] Small improvements
    
    [IMP] Clearer roadmap

---
+++ project_risk/models/__init__.py
@@ -0,0 +1,5 @@
+from . import project_risk_category
+from . import project_risk_response_category
+from . import project_risk_response
+from . import project_risk
+from . import project_project
---
+++ project_risk/models/project_risk_response_category.py
@@ -0,0 +1,10 @@
+# Copyright 2019 Onestein
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ProjectRiskResponseCategory(models.Model):
+    _name = 'project.risk.response.category'
+
+    name = fields.Char(required=True)
