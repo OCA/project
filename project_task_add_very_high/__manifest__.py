7b17d950b53218911dbe4984950f86bd2b9175da
Andrea <a.stirpe@onestein.nl>
2019-05-03 11:40:19 +0200

    [FIX] Expand div oe_title to prevent conflicts with other modules

--- project_task_add_very_high/__manifest__.py
+++ project_task_add_very_high/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018 Onestein (<https://www.onestein.eu>)
+# Copyright 2016-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Project Task Add Very High",
     "summary": "Adds extra options 'High' and 'Very High' on tasks",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "development_status": "Production/Stable",
     "author": "Onestein, Odoo Community Association (OCA)",
     "maintainers": ["astirpe"],
@@ -12,6 +12,9 @@
     "category": "Project",
     "website": "https://github.com/OCA/project/",
     "depends": ["project"],
+    "data": [
+        "views/project_task_view.xml",
+    ],
     "installable": True,
     "uninstall_hook": "uninstall_hook"
 }
