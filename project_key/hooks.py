ccb05ad482ec1f04a6e5cc9fc68cc84af5222e9f
ernesto <ernesto.tejeda87@gmail.com>
2019-05-21 19:35:25 -0400

    project_key: add test context key

--- project_key/hooks.py
+++ project_key/hooks.py
@@ -6,7 +6,3 @@ def post_init_hook(cr, registry):
     from odoo import api, SUPERUSER_ID
     env = api.Environment(cr, SUPERUSER_ID, {})
     env['project.project']._set_default_project_key()
-    cr.execute(
-        "ALTER TABLE project_project ALTER COLUMN key SET NOT NULL",
-        log_exceptions=False
-    )
