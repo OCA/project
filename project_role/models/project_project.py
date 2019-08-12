a7ea91359da5440e48bcaee69b2b777f44a1edf7
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-08-12 23:15:58 +0300

    [IMP] project_role: hierarchy, archived roles and assignments

--- project_role/models/project_project.py
+++ project_role/models/project_project.py
@@ -1,5 +1,5 @@
-# Copyright 2018 Brainbean Apps (https://brainbeanapps.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
