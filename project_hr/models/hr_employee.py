dd0ecab4d453510d94f61ca3946878b53a9a38ef
Victor M.M. Torres <victor.martin@tecnativa.com>
2019-08-20 15:11:08 +0200

    [MIG] project_hr: Migration to v12

---
+++ project_hr/models/hr_employee.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Tecnativa - Victor M.M. Torres
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+
+    @api.model
+    def create(self, vals):
+        employee = super().create(vals)
+        if employee.category_ids:
+            self.env['project.task'].invalidate_cache()
+        return employee
