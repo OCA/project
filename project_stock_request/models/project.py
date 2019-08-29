0d8a4f7ddeb4122ff6506875767b6d9397f733b8
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-08-29 08:08:59 -0600

    [FIX] LINT Errors

--- project_stock_request/models/project.py
+++ project_stock_request/models/project.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2019 - TODAY, Patrick Wilson
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models
 
 
 class Project(models.Model):
