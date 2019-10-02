f3728d6a62508943ba4e5e222bda0be568065dbd
George Daramouskas <gdaramouskas@therp.nl>
2019-10-02 16:22:48 +0200

    [MIG] Migrate sale_order_project to 12.0

--- sale_order_project/models/sale.py
+++ sale_order_project/models/sale.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2014 Akretion - Sébastien BEAU <sebastien.beau@akretion.com>
-# © 2014 Akretion - Benoît GUILLOT <benoit.guillot@akretion.com>
+# Copyright 2014 Akretion - Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2014 Akretion - Benoît GUILLOT <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import exceptions
@@ -27,13 +26,13 @@ class SaleOrder(models.Model):
     def action_create_project(self):
         project_obj = self.env['project.project']
         for order in self:
-            if order.project_id:
+            if order.analytic_account_id:
                 raise exceptions.UserError(_(
                     'There is a project already related with this sale order.'
                 ))
             vals = order._prepare_project_vals()
             project = project_obj.create(vals)
             order.write({
-                'project_id': project.analytic_account_id.id
+                'analytic_account_id': project.analytic_account_id.id
             })
         return True
