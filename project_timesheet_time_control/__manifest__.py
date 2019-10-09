3036139f473b76b6b7c5f41ac7148fbbcf3b1d1e
Jairo Llopis <yajo.sk8@gmail.com>
2019-10-09 13:01:32 +0100

    [IMP] project_timesheet_time_control: ❤️ 4 UX
    This is a huge improvement in this module's usability, although basic functionality stays the same. Let me summmarize:
    
    - In `account.analytic.line` views, the *Stop* button is replaced by colorful *Resume/Stop* buttons.
    - The new *Resume* button will display a wizard that will duplicate all meaningful fields from the previous `account.analytic.line` record and auto-stop the previous running timer, if any.
    - If the previous running timer is very old, some warnings will be displayed.
    - In `project.project` and `project.task` views, I added the same buttons (well... it's *Start* instead of *Resume*, because you could not have worked before in that project or task, but it behaves the same).
    - If using it from a `project.task`, *Start* will duplicate meaningful fields from your latest line in that task.
    - If using it from a `project.project`, *Start* will duplicate meaningful fields your latest line in that project that had no task associated.
    - All kanban, kanban mobile, list and form views are covered.
    - Access the new wizard directly from a menu item if you prefer.
    - I removed, BTW, some Bootstrap 3 and bad replacements in some views.

--- project_timesheet_time_control/__manifest__.py
+++ project_timesheet_time_control/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Project timesheet time control',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'development_status': 'Mature',
     'category': 'Project',
     'author': 'Tecnativa,'
@@ -16,10 +16,14 @@
     'depends': [
         'hr_timesheet_task_stage',
         'hr_timesheet_task_domain',
+        'web_ir_actions_act_multi',
+        'web_ir_actions_act_view_reload',
     ],
     'data': [
         'views/account_analytic_line_view.xml',
+        'views/project_project_view.xml',
         'views/project_task_view.xml',
+        'wizards/hr_timesheet_switch_view.xml',
     ],
     'license': 'AGPL-3',
     'installable': True,
