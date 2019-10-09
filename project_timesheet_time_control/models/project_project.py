f29b086dd2a87925773aefa06dbf65972b234210
Jairo Llopis <yajo.sk8@gmail.com>
2019-10-09 13:01:32 +0100

    [IMP] project_timesheet_time_control: ❤️ 4 UX
    This is a huge improvement in this module's usability, although basic functionality stays the same. Let me summmarize:
    
    - In `account.analytic.line` views, the *Stop* button is replaced by colorful *Resume/Stop* buttons.
    - The new *Resume* button will display a wizard that will duplicate all meaningful fields from the previous `account.analytic.line` record and auto-stop the previous running timer, if any.
    - If the previous running timer is very old, some warnings will be displayed.
    - In `project.project` and `project.task` views, I added the same buttons (well... it's *Start* instead of *Resume*, because you could not have worked before in that project or task, but it behaves the same).
    - If using it from a `project.task`, *Start* will duplicate meaningful fields from your latest line in that task.
    - If using it from a `project.project`, *Start* will duplicate meaningful fields your latest line in that project that had no task associated.
    - All kanban, kanban mobile, list and form views are covered.
    - Access the new wizard directly from a menu item if you prefer.
    - I removed, BTW, some Bootstrap 3 and bad replacements in some views.

---
+++ project_timesheet_time_control/models/project_project.py
@@ -0,0 +1,63 @@
+# Copyright 2019 Tecnativa - Jairo Llopis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    show_time_control = fields.Selection(
+        selection=[("start", "Start"), ("stop", "Stop")],
+        compute="_compute_show_time_control",
+        help="Indicate which time control button to show, if any.",
+    )
+
+    @api.model
+    def _timesheet_running_domain(self):
+        """Domain to find running timesheet lines."""
+        return self.env["account.analytic.line"]._running_domain() + [
+            ("project_id", "in", self.ids),
+        ]
+
+    @api.depends("allow_timesheets")
+    def _compute_show_time_control(self):
+        """Decide which time control button to show, if any."""
+        grouped = self.env["account.analytic.line"].read_group(
+            domain=self._timesheet_running_domain(),
+            fields=["id"],
+            groupby=["project_id"],
+        )
+        lines_per_project = {group["project_id"][0]: group["project_id_count"]
+                             for group in grouped}
+        button_per_lines = {0: "start", 1: "stop"}
+        for project in self:
+            if not project.allow_timesheets:
+                project.show_time_control = False
+                continue
+            project.show_time_control = button_per_lines.get(
+                lines_per_project.get(project.id, 0),
+                False,
+            )
+
+    def button_start_work(self):
+        """Create a new record starting now, with a running timer."""
+        return {
+            "context": {
+                "default_project_id": self.id,
+                "default_task_id": False,
+            },
+            "name": _("Start work"),
+            "res_model": "hr.timesheet.switch",
+            "target": "new",
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "view_type": "form",
+        }
+
+    @api.multi
+    def button_end_work(self):
+        running_lines = self.env["account.analytic.line"].search(
+            self._timesheet_running_domain(),
+        )
+        return running_lines.button_end_work()
