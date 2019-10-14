b939f41fd28ab53d6a06258ec89ffc74fa1ab055
patrickrwilson <36892066+patrickrwilson@users.noreply.github.com>
2019-10-14 12:48:22 -0500

    [FIX] Test py mispelling

--- project_budget_change_order/tests/test_project_change_order.py
+++ project_budget_change_order/tests/test_project_change_order.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime
-from odoo.addons.account_budget_ocs.tests.common import TestAccountBudgetCommon
+from odoo.addons.account_budget_oca.tests.common import TestAccountBudgetCommon
 
 
 class TestProjectChangeOrder(TestAccountBudgetCommon):
