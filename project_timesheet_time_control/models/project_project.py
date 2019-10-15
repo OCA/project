8c1b81ce460444b1c2d0d4e6ec407a84d5a02f2e
Jairo Llopis <yajo.sk8@gmail.com>
2019-10-15 10:56:57 +0100

    [IMP] project_timesheet_time_control: Refactor with mixin
    Models related to timesheet time controls now inherit from a mixin that adds most needed logic automatically.

--- project_timesheet_time_control/models/project_project.py
+++ project_timesheet_time_control/models/project_project.py
@@ -1,69 +1,30 @@
 # Copyright 2019 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, models
 
 
 class ProjectProject(models.Model):
-    _inherit = "project.project"
-
-    show_time_control = fields.Selection(
-        selection=[("start", "Start"), ("stop", "Stop")],
-        compute="_compute_show_time_control",
-        help="Indicate which time control button to show, if any.",
-    )
+    _name = "project.project"
+    _inherit = ["project.project", "hr.timesheet.time_control.mixin"]
 
     @api.model
-    def _timesheet_running_domain(self):
-        """Domain to find running timesheet lines."""
-        return self.env["account.analytic.line"]._running_domain() + [
-            ("project_id", "in", self.ids),
-        ]
+    def _relation_with_timesheet_line(self):
+        return "project_id"
 
     @api.depends("allow_timesheets")
     def _compute_show_time_control(self):
-        """Decide which time control button to show, if any."""
-        grouped = self.env["account.analytic.line"].read_group(
-            domain=self._timesheet_running_domain(),
-            fields=["id"],
-            groupby=["project_id"],
-        )
-        lines_per_project = {group["project_id"][0]: group["project_id_count"]
-                             for group in grouped}
-        button_per_lines = {0: "start", 1: "stop"}
+        result = super()._compute_show_time_control()
         for project in self:
+            # Never show button if timesheets are not allowed in project
             if not project.allow_timesheets:
                 project.show_time_control = False
-                continue
-            project.show_time_control = button_per_lines.get(
-                lines_per_project.get(project.id, 0),
-                False,
-            )
+        return result
 
     def button_start_work(self):
-        """Create a new record starting now, with a running timer."""
-        return {
-            "context": {
-                "default_project_id": self.id,
-                "default_task_id": False,
-            },
-            "name": _("Start work"),
-            "res_model": "hr.timesheet.switch",
-            "target": "new",
-            "type": "ir.actions.act_window",
-            "view_mode": "form",
-            "view_type": "form",
-        }
-
-    @api.multi
-    def button_end_work(self):
-        running_lines = self.env["account.analytic.line"].search(
-            self._timesheet_running_domain(),
-        )
-        if not running_lines:
-            raise UserError(
-                _("No running timer found in project %s. "
-                  "Refresh the page and check again.") % self.display_name,
-            )
-        return running_lines.button_end_work()
+        result = super().button_start_work()
+        # When triggering from project is usually to start timer without task
+        result["context"].update({
+            "default_task_id": False,
+        })
+        return result
