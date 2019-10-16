516679ffe1506996d8beda7e4d9ed9de27b53bf5
emagdalena <emagdalena@c2i.es>
2019-10-16 15:38:03 +0200

    [IMP] : black, isort

--- project_parent_task_filter/__manifest__.py
+++ project_parent_task_filter/__manifest__.py
@@ -7,14 +7,9 @@
     "version": "12.0.1.2.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
-    "author": "C2i Change 2 improve, "
-              "Odoo Community Association (OCA)",
+    "author": "C2i Change 2 improve, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": [
-        "project",
-    ],
-    "data": [
-        "views/project_task.xml",
-    ],
+    "depends": ["project"],
+    "data": ["views/project_task.xml"],
     "installable": True,
 }
