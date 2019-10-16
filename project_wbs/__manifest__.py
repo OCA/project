fcadaac941750d2d5bbf737153071dac5b35a8fa
mreficent <miquel.raich@eficent.com>
2019-10-16 19:04:01 +0200

    [REF] Don't inherit project.project from account.analytic.account

--- project_wbs/__manifest__.py
+++ project_wbs/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-19 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Luxim d.o.o.
 # Copyright 2017 Matmoz d.o.o.
 # Copyright 2017 Deneroteam.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Work Breakdown Structure',
+    'name': 'Project Work Breakdown Structure',
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Matmoz d.o.o., '
@@ -15,13 +15,11 @@
               'Odoo Community Association (OCA)',
     'website': 'https://www.github.com/OCA/project',
     'depends': [
-        'project',
-        'analytic',
         'account_analytic_parent',
         'account_analytic_sequence',
-        'hr_timesheet'
+        'hr_timesheet',
     ],
-    'summary': 'Project Work Breakdown Structure',
+    'summary': 'Apply Work Breakdown Structure',
     'data': [
         'view/account_analytic_account_view.xml',
         'view/project_project_view.xml',
--- project_wbs/tests/test_project_wbs.py
+++ project_wbs/tests/test_project_wbs.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-19 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Luxim d.o.o.
 # Copyright 2017 Matmoz d.o.o.
 # Copyright 2017 Deneroteam.
