fb252f56663c6c820ae2fa740140c0d6744a16d7
emagdalena <emagdalena@c2i.es>
2019-10-18 18:44:36 +0200

    [IMP] project_task_default_stage: black, isort

--- project_task_default_stage/models/project_task_type.py
+++ project_task_default_stage/models/project_task_type.py
@@ -5,10 +5,11 @@ from odoo import fields, models
 
 
 class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
+    _inherit = "project.task.type"
 
     case_default = fields.Boolean(
-        string='Default for New Projects',
-        help='If you check this field, this stage will be proposed by default '
-             'on each new project. It will not assign this stage to existing '
-             'projects.')
+        string="Default for New Projects",
+        help="If you check this field, this stage will be proposed by default "
+        "on each new project. It will not assign this stage to existing "
+        "projects.",
+    )
--- project_task_default_stage/tests/test_project_task_default_stage.py
+++ project_task_default_stage/tests/test_project_task_default_stage.py
@@ -9,9 +9,7 @@ class TestProjectCaseDefault(TransactionCase):
     # Use case : Prepare some data for current test case
     def setUp(self):
         super(TestProjectCaseDefault, self).setUp()
-        self.project = self.env['project.project'].create({
-            'name': 'Project Test'
-        })
+        self.project = self.env["project.project"].create({"name": "Project Test"})
 
     def test_project_new(self):
         self.assertEqual(len(self.project.type_ids), 7)
