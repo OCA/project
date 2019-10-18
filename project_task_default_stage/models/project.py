e2b7a964e945bc1622dd632722720da46cbc3d15
emagdalena <emagdalena@c2i.es>
2019-10-18 18:44:36 +0200

    [IMP] project_task_default_stage: black, isort

--- project_task_default_stage/models/project.py
+++ project_task_default_stage/models/project.py
@@ -5,12 +5,10 @@ from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     def _get_default_type_common(self):
-        ids = self.env['project.task.type'].search([
-            ('case_default', '=', True)])
+        ids = self.env["project.task.type"].search([("case_default", "=", True)])
         return ids
 
-    type_ids = fields.Many2many(
-        default=lambda self: self._get_default_type_common())
+    type_ids = fields.Many2many(default=lambda self: self._get_default_type_common())
