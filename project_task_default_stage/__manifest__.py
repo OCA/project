3f43be2a66f52e19c25aaa85832f0db245a4792f
emagdalena <emagdalena@c2i.es>
2019-10-18 18:44:36 +0200

    [IMP] project_task_default_stage: black, isort

--- project_task_default_stage/__manifest__.py
+++ project_task_default_stage/__manifest__.py
@@ -3,20 +3,14 @@
 # Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Project Task Default Stage',
-    'summary': 'Recovery default task stage projects from v8',
-    'version': '12.0.1.0.0',
-    'category': 'Project',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "name": "Project Task Default Stage",
+    "summary": "Recovery default task stage projects from v8",
+    "version": "12.0.1.0.0",
+    "category": "Project",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
-    'license': 'AGPL-3',
-    'depends': [
-        'project',
-    ],
-    'data': [
-        'views/project_view.xml',
-        'data/project_data.xml',
-    ],
-    'installable': True,
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project_view.xml", "data/project_data.xml"],
+    "installable": True,
 }
