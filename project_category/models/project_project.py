6ea361a8b9b152d3c323244bb85f45ab64fd52fd
Saran <saranp502@hotmail.com>
2019-10-22 16:48:06 +0700

    [IMP] project_category: black, isort

--- project_category/models/project_project.py
+++ project_category/models/project_project.py
@@ -4,11 +4,11 @@ from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     type_id = fields.Many2one(
-        comodel_name='project.type',
-        string='Type',
+        comodel_name="project.type",
+        string="Type",
         copy=False,
         domain="[('project_ok', '=', True)]",
     )
--- project_category/models/project_type.py
+++ project_category/models/project_type.py
@@ -5,54 +5,34 @@ from odoo.exceptions import ValidationError
 
 
 class ProjectType(models.Model):
-    _name = 'project.type'
-    _description = 'Project Type'
-    _rec_name = 'complete_name'
+    _name = "project.type"
+    _description = "Project Type"
+    _rec_name = "complete_name"
 
-    parent_id = fields.Many2one(
-        comodel_name='project.type',
-        string='Parent Type'
-    )
+    parent_id = fields.Many2one(comodel_name="project.type", string="Parent Type")
     child_ids = fields.One2many(
-        comodel_name='project.type',
-        inverse_name='parent_id',
-        string='Subtypes'
+        comodel_name="project.type", inverse_name="parent_id", string="Subtypes"
     )
-    name = fields.Char(
-        string='Name',
-        required=True,
-        translate=True,
-    )
+    name = fields.Char(string="Name", required=True, translate=True)
     complete_name = fields.Char(
-        string='Complete Name',
-        compute='_compute_complete_name',
-        store=True
+        string="Complete Name", compute="_compute_complete_name", store=True
     )
-    description = fields.Text(
-        translate=True,
-    )
-    project_ok = fields.Boolean(
-        string='Can be applied for projects',
-        default=True
-    )
-    task_ok = fields.Boolean(
-        string='Can be applied for tasks'
-    )
+    description = fields.Text(translate=True)
+    project_ok = fields.Boolean(string="Can be applied for projects", default=True)
+    task_ok = fields.Boolean(string="Can be applied for tasks")
 
-    @api.constrains('parent_id')
+    @api.constrains("parent_id")
     def check_parent_id(self):
         if not self._check_recursion():
-            raise ValidationError(
-                _('You cannot create recursive project types.')
-            )
+            raise ValidationError(_("You cannot create recursive project types."))
 
-    @api.depends('name', 'parent_id.complete_name')
+    @api.depends("name", "parent_id.complete_name")
     def _compute_complete_name(self):
         for project_type in self:
             if project_type.parent_id:
-                project_type.complete_name = '%s / %s' % (
+                project_type.complete_name = "{} / {}".format(
                     project_type.parent_id.complete_name,
-                    project_type.name
+                    project_type.name,
                 )
             else:
                 project_type.complete_name = project_type.name
