bbaaaa08e357492c7a0add14609775002ff5d0f7
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-11-07 14:28:35 +0000

    [FIX] project_role: missing migration

---
+++ project_role/migrations/12.0.2.1.0/pre-migration.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_fields(env, [
+        (
+            'project.project',
+            'project_project',
+            'limit_timesheet_role_to_assignments',
+            'limit_role_to_assignments',
+        ),
+        (
+            'res.company',
+            'res_company',
+            'limit_timesheet_role_to_assignments',
+            'project_limit_role_to_assignments',
+        ),
+    ])
