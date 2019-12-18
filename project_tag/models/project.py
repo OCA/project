06e6d155330d902799931fe172514accae8227f7
Saran <saranp502@hotmail.com>
2019-12-18 17:52:51 +0700

    [IMP] project_tag: black, isort

--- project_tag/models/project.py
+++ project_tag/models/project.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
-    tag_ids = fields.Many2many('project.tags', string="Tags",)
+    tag_ids = fields.Many2many("project.tags", string="Tags")
