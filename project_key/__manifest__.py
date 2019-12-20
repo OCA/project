f492714501559613666ed05de029c30c2c67f660
Saran <saranp502@hotmail.com>
2019-12-20 09:45:34 +0700

    [IMP] project_key: black, isort

--- project_key/__manifest__.py
+++ project_key/__manifest__.py
@@ -5,15 +5,11 @@
     "name": "Project key",
     "summary": "Module decorates projects and tasks with Project Key",
     "category": "Project",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "author": "Modoolar, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project/",
-    "depends": [
-        "project",
-    ],
-    "data": [
-        "views/project_key_views.xml",
-    ],
+    "depends": ["project"],
+    "data": ["views/project_key_views.xml"],
     "post_init_hook": "post_init_hook",
 }
