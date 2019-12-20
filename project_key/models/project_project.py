b549f0fc43d7de939e9c569c4e2fff0bfc16e5b1
Saran <saranp502@hotmail.com>
2019-12-20 09:45:34 +0700

    [IMP] project_key: black, isort

--- project_key/models/project_project.py
+++ project_key/models/project_project.py
@@ -1,7 +1,7 @@
 # Copyright 2017 - 2018 Modoolar <info@modoolar.com>
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.osv import expression
 from odoo.tools import config
 
@@ -10,25 +10,17 @@ class Project(models.Model):
     _inherit = "project.project"
 
     task_key_sequence_id = fields.Many2one(
-        comodel_name='ir.sequence',
-        string='Key Sequence',
-        ondelete="restrict",
+        comodel_name="ir.sequence", string="Key Sequence", ondelete="restrict"
     )
 
-    key = fields.Char(
-        string='Key',
-        size=10,
-        required=False,
-        index=True,
-        copy=False
-    )
+    key = fields.Char(string="Key", size=10, required=False, index=True, copy=False)
 
     _sql_constraints = [
         ("project_key_unique", "UNIQUE(key)", "Project key must be unique")
     ]
 
     @api.multi
-    @api.onchange('name')
+    @api.onchange("name")
     def _onchange_project_name(self):
         for rec in self:
             if rec.key:
@@ -37,12 +29,12 @@ class Project(models.Model):
             if rec.name:
                 rec.key = self.generate_project_key(rec.name)
             else:
-                rec.key = ''
+                rec.key = ""
 
     @api.model
     def create(self, vals):
-        if 'key' not in vals:
-            vals['key'] = self.generate_project_key(vals['name'])
+        if "key" not in vals:
+            vals["key"] = self.generate_project_key(vals["name"])
 
         new_project = super(Project, self).create(vals)
         new_project.create_sequence()
@@ -53,8 +45,8 @@ class Project(models.Model):
     def write(self, values):
         update_key = False
 
-        if 'key' in values:
-            key = values['key']
+        if "key" in values:
+            key = values["key"]
             update_key = self.key != key
 
         res = super(Project, self).write(values)
@@ -76,16 +68,16 @@ class Project(models.Model):
         return super(Project, self).unlink()
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         res = super(Project, self).name_search(name, args, operator, limit)
         if name:
             domain = [
-                '|',
-                ('key', 'ilike', name + '%'),
-                ('id', 'in', [x[0] for x in res])
+                "|",
+                ("key", "ilike", name + "%"),
+                ("id", "in", [x[0] for x in res]),
             ]
             if operator in expression.NEGATIVE_TERM_OPERATORS:
-                domain = ['&', '!'] + domain[1:]
+                domain = ["&", "!"] + domain[1:]
             projects = self.search(domain + (args or []), limit=limit)
             return projects.name_get()
         else:
@@ -98,8 +90,8 @@ class Project(models.Model):
         """
         self.ensure_one()
         sequence_data = self._prepare_sequence_data()
-        sequence = self.env['ir.sequence'].sudo().create(sequence_data)
-        self.write({'task_key_sequence_id': sequence.id})
+        sequence = self.env["ir.sequence"].sudo().create(sequence_data)
+        self.write({"task_key_sequence_id": sequence.id})
         return sequence
 
     def update_sequence(self):
@@ -117,13 +109,11 @@ class Project(models.Model):
         for number_increment and number_next_actual
         """
         values = {
-            'name': "%s %s" % (
-                _("Project task sequence for project "), self.name
-            ),
-            'implementation': 'standard',
-            'code': 'project.task.key.%s' % (self.id,),
-            'prefix': "%s-" % (self.key,),
-            'use_date_range': False,
+            "name": "{} {}".format(_("Project task sequence for project "), self.name),
+            "implementation": "standard",
+            "code": "project.task.key.{}".format(self.id),
+            "prefix": "{}-".format(self.key),
+            "use_date_range": False,
         }
 
         if init:
@@ -132,20 +122,20 @@ class Project(models.Model):
         return values
 
     def get_next_task_key(self):
-        test_project_key = self.env.context.get('test_project_key')
-        if config['test_enable'] and not test_project_key:
+        test_project_key = self.env.context.get("test_project_key")
+        if config["test_enable"] and not test_project_key:
             return False
         return self.sudo().task_key_sequence_id.next_by_id()
 
     def generate_project_key(self, text):
-        test_project_key = self.env.context.get('test_project_key')
-        if config['test_enable'] and not test_project_key:
+        test_project_key = self.env.context.get("test_project_key")
+        if config["test_enable"] and not test_project_key:
             return False
 
         if not text:
-            return ''
+            return ""
 
-        data = text.split(' ')
+        data = text.split(" ")
         if len(data) == 1:
             return data[0][:3].upper()
 
@@ -174,7 +164,7 @@ class Project(models.Model):
         """
 
         self.env.cr.execute(reindex_query, (self.id,))
-        self.env['project.task'].invalidate_cache(['key'], self.task_ids.ids)
+        self.env["project.task"].invalidate_cache(["key"], self.task_ids.ids)
 
     @api.model
     def _set_default_project_key(self):
@@ -185,9 +175,9 @@ class Project(models.Model):
         installation.
         :return:
         """
-        for project in self.with_context(active_test=False).search([
-            ('key', '=', False)
-        ]):
+        for project in self.with_context(active_test=False).search(
+            [("key", "=", False)]
+        ):
             project.key = self.generate_project_key(project.name)
             project.create_sequence()
 
