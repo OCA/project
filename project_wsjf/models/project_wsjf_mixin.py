e90ba4c0aa4e27f1f3cf5f8e743c6f8623cd8de8
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-01-03 10:45:43 -0300

    [FIX] Flake8

--- project_wsjf/models/project_wsjf_mixin.py
+++ project_wsjf/models/project_wsjf_mixin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019  Gabriel Cardoso de Faria - KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields, api
 
 FIBONACCI = [
     (1, '1'),
@@ -11,8 +12,6 @@ FIBONACCI = [
     (13, '13'),
     (21, '21'),
 ]
-
-from odoo import models, fields, api
 
 
 class ProjectWSJFMixin(models.AbstractModel):
