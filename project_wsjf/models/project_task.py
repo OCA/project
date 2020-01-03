40a005c83f29b8637a4ebfa13e3107734b18f000
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-01-03 16:50:24 -0300

    [REF] Replace default_get with inverse function

--- project_wsjf/models/project_task.py
+++ project_wsjf/models/project_task.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019  Gabriel Cardoso de Faria - KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api
+from odoo import models, fields, api
 
 
 class ProjectTask(models.Model):
@@ -12,15 +12,18 @@ class ProjectTask(models.Model):
     ]
     _order = 'wsjf desc, sequence'
 
-    @api.model
-    def default_get(self, fields):
-        defaults = super(ProjectTask, self).default_get(fields)
-        if self.project_id:
-            defaults.update({
-                'business_value': self.project_id.business_value,
-                'time_criticality': self.project_id.time_criticality,
-                'risk_reduction': self.project_id.risk_reduction,
-                'internal_pressure': self.project_id.internal_pressure,
-                'job_size': self.project_id.job_size,
-            })
-        return defaults
+    project_id = fields.Many2one(
+        inverse='_inverse_project_id',
+    )
+
+    @api.multi
+    def _inverse_project_id(self):
+        for record in self:
+            if record.project_id:
+                record.write({
+                    'business_value': record.project_id.business_value,
+                    'time_criticality': record.project_id.time_criticality,
+                    'risk_reduction': record.project_id.risk_reduction,
+                    'internal_pressure': record.project_id.internal_pressure,
+                    'job_size': record.project_id.job_size,
+                })
