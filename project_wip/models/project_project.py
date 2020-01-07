698d400d4b65a11cd546ff7adeae6ee10d7ed5da
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-01-07 16:19:47 -0300

    [ADD] Option to view value stream of individual project

--- project_wip/models/project_project.py
+++ project_wip/models/project_project.py
@@ -8,3 +8,17 @@ from odoo import api, fields, models, _
 class ProjectProject(models.Model):
 
     _inherit = 'project.project'
+
+    @api.multi
+    def view_value_stream(self):
+        self.ensure_one()
+        action = self.env.ref(
+            'project_wip.report_project_value_stream_act_window')
+        action = action.read()[0]
+        action['context'] = {
+            'default_project_id': self.id
+        }
+        action['domain'] = [
+            ('project_id', '=', self.id)
+        ]
+        return action
