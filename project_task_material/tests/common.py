45d891a16eee7e7e128a181a919ad8ea4f0c4ed4
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 11:34:56 +0100

    [IMP] : black, isort

--- project_task_material/tests/common.py
+++ project_task_material/tests/common.py
@@ -13,17 +13,19 @@ class TestProjectCases(TransactionCase):
 
         # Create new User
         # Add it to the `project user` group
-        self.project_user = self.env["res.users"].create({
-            "company_id": self.env.ref("base.main_company").id,
-            "name": "Carlos Project User",
-            "login": "cpu",
-            "email": "cpu@yourcompany.com",
-            "groups_id": [(6, 0, [self.ref('project.group_project_user')])]
-        })
+        self.project_user = self.env["res.users"].create(
+            {
+                "company_id": self.env.ref("base.main_company").id,
+                "name": "Carlos Project User",
+                "login": "cpu",
+                "email": "cpu@yourcompany.com",
+                "groups_id": [(6, 0, [self.ref("project.group_project_user")])],
+            }
+        )
 
         # Refer to a task assigned to the project user
-        self.task = self.env.ref('project.project_task_2')
-        self.product = self.env.ref('product.consu_delivery_03')
+        self.task = self.env.ref("project.project_task_2")
+        self.product = self.env.ref("product.consu_delivery_03")
 
         # Refer to a action from the user created
         self.action = self.task.sudo(self.project_user.id)
