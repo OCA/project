cd495b0ca98fecfee14343d668333d165d678f58
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 11:34:56 +0100

    [IMP] : black, isort

--- project_task_material/tests/test_create_material_lines.py
+++ project_task_material/tests/test_create_material_lines.py
@@ -2,12 +2,12 @@
 # Copyright 2018 - Brain-tec AG - Carlos Jesus Cebrian
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
 
-from .common import TestProjectCases
 from odoo.exceptions import ValidationError
+
+from .common import TestProjectCases
 
 
 class ProjectTaskMaterial(TestProjectCases):
-
     def test_manager_add_task_material_wrong(self):
         """
         TEST CASE 1
@@ -17,21 +17,33 @@ class ProjectTaskMaterial(TestProjectCases):
         """
         try:
             # Material with `quantity = 0.0`
-            self.action.write({"material_ids": [(
-                0, 0, {"product_id": self.product.id, "quantity": 0.0})]})
+            self.action.write(
+                {
+                    "material_ids": [
+                        (0, 0, {"product_id": self.product.id, "quantity": 0.0})
+                    ]
+                }
+            )
         except ValidationError as err:
             self.assertEqual(
                 str(err.args[0]),
-                "Quantity of material consumed must be greater than 0.")
+                "Quantity of material consumed must be greater than 0.",
+            )
 
         try:
             # Material with `negative quantity`
-            self.action.write({"material_ids": [(
-                0, 0, {"product_id": self.product.id, "quantity": -10.0})]})
+            self.action.write(
+                {
+                    "material_ids": [
+                        (0, 0, {"product_id": self.product.id, "quantity": -10.0})
+                    ]
+                }
+            )
         except ValidationError as err:
             self.assertEqual(
                 str(err.args[0]),
-                "Quantity of material consumed must be greater than 0.")
+                "Quantity of material consumed must be greater than 0.",
+            )
 
     def test_manager_add_task_material_right(self):
         """
@@ -41,6 +53,7 @@ class ProjectTaskMaterial(TestProjectCases):
 
         """
         # Material with `quantity = 1.0`
-        self.action.write({"material_ids": [(
-            0, 0, {"product_id": self.product.id, "quantity": 4.0})]})
+        self.action.write(
+            {"material_ids": [(0, 0, {"product_id": self.product.id, "quantity": 4.0})]}
+        )
         self.assertEqual(len(self.task.material_ids.ids), 1)
