a27b3f563371d9ecaf2f2862b7bec42b2fb35a88
Samuel Piquet <samuel.piquet@gmail.com>
2020-01-14 11:34:56 +0100

    [IMP] : black, isort

--- project_task_material/models/project.py
+++ project_task_material/models/project.py
@@ -4,7 +4,7 @@
 # Copyright 2018 - Brain-tec AG - Carlos Jesus Cebrian
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -14,8 +14,10 @@ class Task(models.Model):
     _inherit = "project.task"
 
     material_ids = fields.One2many(
-        comodel_name='project.task.material', inverse_name='task_id',
-        string='Material Used')
+        comodel_name="project.task.material",
+        inverse_name="task_id",
+        string="Material Used",
+    )
 
 
 class ProjectTaskMaterial(models.Model):
@@ -25,17 +27,18 @@ class ProjectTaskMaterial(models.Model):
     _description = "Task Material Used"
 
     task_id = fields.Many2one(
-        comodel_name='project.task', string='Task', ondelete='cascade',
-        required=True)
+        comodel_name="project.task", string="Task", ondelete="cascade", required=True
+    )
     product_id = fields.Many2one(
-        comodel_name='product.product', string='Product', required=True)
-    quantity = fields.Float(string='Quantity')
+        comodel_name="product.product", string="Product", required=True
+    )
+    quantity = fields.Float(string="Quantity")
 
     @api.multi
-    @api.constrains('quantity')
+    @api.constrains("quantity")
     def _check_quantity(self):
         for material in self:
             if not material.quantity > 0.0:
                 raise ValidationError(
-                    _('Quantity of material consumed must be greater than 0.')
+                    _("Quantity of material consumed must be greater than 0.")
                 )
