180cb198e6ec825c887d5873916e5ab4ccd20719
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-11 17:13:38 +0100

    [IMP] project_task_dependency: black, isort

--- project_task_dependency/models/project_task_copy_map.py
+++ project_task_dependency/models/project_task_copy_map.py
@@ -5,15 +5,9 @@ from odoo import fields, models
 
 
 class ProjectTaskCopyMap(models.TransientModel):
-    _name = 'project.task.copy.map'
-    _description = 'Project Task Copy Map'
+    _name = "project.task.copy.map"
+    _description = "Project Task Copy Map"
 
-    old_task_id = fields.Many2one(
-        comodel_name='project.task',
-        ondelete='cascade'
-    )
+    old_task_id = fields.Many2one(comodel_name="project.task", ondelete="cascade")
 
-    new_task_id = fields.Many2one(
-        comodel_name='project.task',
-        ondelete='cascade'
-    )
+    new_task_id = fields.Many2one(comodel_name="project.task", ondelete="cascade")
