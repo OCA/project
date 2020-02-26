6f53330d69048df60ec7da2468c33e04ba996191
alfadil <alfadil.tabar@gamil.com>
2020-02-26 11:46:39 +0300

    [IMP] project_hr: black, isort

--- project_hr/models/project_project.py
+++ project_hr/models/project_project.py
@@ -5,11 +5,11 @@ from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     hr_category_ids = fields.Many2many(
-        comodel_name='hr.employee.category',
+        comodel_name="hr.employee.category",
         string="Employee Categories",
         help="Here you can link the project to several employee categories, "
-             "that will be the allowed in tasks.",
+        "that will be the allowed in tasks.",
     )
