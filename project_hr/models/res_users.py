75cf8b7a450832e5ed52cd7bd3b7378e41ede766
alfadil <alfadil.tabar@gamil.com>
2020-02-26 11:46:39 +0300

    [IMP] project_hr: black, isort

--- project_hr/models/res_users.py
+++ project_hr/models/res_users.py
@@ -5,17 +5,17 @@ from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     hr_category_ids = fields.Many2many(
         comodel_name="hr.employee.category",
         string="HR categories",
         compute="_compute_hr_category_ids",
         help="Technical field for computing dynamically employee categories "
-             "linked to the user in the current company."
+        "linked to the user in the current company.",
     )
 
-    @api.depends('company_id', 'employee_ids', 'employee_ids.category_ids')
+    @api.depends("company_id", "employee_ids", "employee_ids.category_ids")
     def _compute_hr_category_ids(self):
         for user in self:
             user.hr_category_ids = user.employee_ids.filtered(
