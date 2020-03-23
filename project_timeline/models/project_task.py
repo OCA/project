d71c5fca257b87f48e86f9f90ab9e86d34bee076
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-23 17:56:34 +0100

    [MIG] project_timeline: Migration to 13.0

--- project_timeline/models/project_task.py
+++ project_timeline/models/project_task.py
@@ -2,24 +2,13 @@
 # Copyright 2017 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    @api.onchange("user_id")
-    def _onchange_user(self):  # pragma: no cover
-        """Don't change date_start when changing the user_id. This screws up
-        the default value passed by context when creating a record. It's also
-        a nonsense to chain both values.
-        """
-        old_date_start = self.date_start
-        super(ProjectTask, self)._onchange_user()
-        if old_date_start and old_date_start > self.date_start:
-            self.date_start = old_date_start
-
     def update_date_end(self, stage_id):
-        res = super(ProjectTask, self).update_date_end(stage_id)
+        res = super().update_date_end(stage_id)
         res.pop("date_end", None)
         return res
