03d63f6411b50298fdcc4384964d02256dc4f07c
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-23 20:34:54 +0100

    [IMP] project_timeline_task_dependency: black, isort

--- project_timeline_task_dependency/__manifest__.py
+++ project_timeline_task_dependency/__manifest__.py
@@ -2,20 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Project Timeline Task Dependencies',
-    'summary': 'Render arrows between dependencies.',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/oca/project',
-    'category': 'Project Management',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'project_timeline',
-        'project_task_dependency'
-    ],
-    'data': [
-        'views/project_task_view.xml'
-    ],
-    'installable': True,
-    'auto_install': True
+    "name": "Project Timeline Task Dependencies",
+    "summary": "Render arrows between dependencies.",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/oca/project",
+    "category": "Project Management",
+    "version": "13.0.1.0.0",
+    "depends": ["project_timeline", "project_task_dependency"],
+    "data": ["views/project_task_view.xml"],
+    "installable": True,
+    "auto_install": True,
 }
