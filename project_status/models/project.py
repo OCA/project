96773d145f114925656e6fe04a28ae1091e338e2
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-04-04 18:38:24 +0530

    [IMP] project_status: black, isort, prettier

--- project_status/models/project.py
+++ project_status/models/project.py
@@ -1,16 +1,19 @@
-from odoo import models, fields, api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api, fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     @api.model
     def _read_group_status_ids(self, statuses, domain, order):
-        statuse_ids = statuses._search(
-            [], order=order, access_rights_uid=SUPERUSER_ID)
+        statuse_ids = statuses._search([], order=order, access_rights_uid=SUPERUSER_ID)
         return statuses.browse(statuse_ids)
 
     project_status = fields.Many2one(
-        'project.status', string="Project Status",
-        group_expand='_read_group_status_ids', copy=False,
-        ondelete='restrict', index=True)
+        "project.status",
+        string="Project Status",
+        group_expand="_read_group_status_ids",
+        copy=False,
+        ondelete="restrict",
+        index=True,
+    )
