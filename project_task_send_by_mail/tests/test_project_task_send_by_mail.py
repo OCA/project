84dff9b2539a1719ae6bd19aa9b47e0a5dc471ad
David Alonso <david.alonso@solvos.es>
2020-05-18 12:45:08 +0200

    [IMP] project_task_send_by_mail: black, isort, prettier

--- project_task_send_by_mail/tests/test_project_task_send_by_mail.py
+++ project_task_send_by_mail/tests/test_project_task_send_by_mail.py
@@ -8,10 +8,8 @@ class TestProjectTaskSendByMail(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestProjectTaskSendByMail, cls).setUpClass()
-        cls.task = cls.env['project.task'].create({
-            'name': 'task test',
-        })
+        cls.task = cls.env["project.task"].create({"name": "task test",})
 
     def test_send_mail(self):
         result = self.task.action_task_send()
-        self.assertEqual(result['name'], 'Compose Email')
+        self.assertEqual(result["name"], "Compose Email")
