4e64c70b27cfbbfae0e4d40d28ec6ffb41f02970
Manuel Marquez <buzondemam@gmail.com>
2020-05-26 09:28:40 -0400

    [MIG] project_task_category: Migration to 11.0.

--- project_task_category/models/project_category.py
+++ project_task_category/models/project_category.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Elico Corp (https://www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -8,7 +7,7 @@ from odoo import fields, models
 class ProjectCategory(models.Model):
     """Data Model for Project Category."""
 
-    _name = 'project.category'
-    _inherit = 'project.tags'
+    _name = "project.category"
+    _inherit = "project.tags"
 
     description = fields.Char()
