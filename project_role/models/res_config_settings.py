623570d90171c9bd2a935366793958be722706ac
alfadil <alfadil.tabar@gamil.com>
2020-06-02 11:09:39 +0300

    [IMP] project_role : black, isort, prettier

--- project_role/models/res_config_settings.py
+++ project_role/models/res_config_settings.py
@@ -5,13 +5,11 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     project_inherit_assignments = fields.Boolean(
-        related='company_id.project_inherit_assignments',
-        readonly=False,
+        related="company_id.project_inherit_assignments", readonly=False,
     )
     project_limit_role_to_assignments = fields.Boolean(
-        related='company_id.project_limit_role_to_assignments',
-        readonly=False,
+        related="company_id.project_limit_role_to_assignments", readonly=False,
     )
