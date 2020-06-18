52ce4d10614b9c772052a465896c5cd1e1d165bc
alfadil <alfadil.tabar@gmail.com>
2020-06-18 12:34:24 +0300

    [IMP] project_template: black, isort, prettier

--- project_template/__manifest__.py
+++ project_template/__manifest__.py
@@ -2,18 +2,16 @@
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 {
-    'name': "Project Templates",
-    'summary': """Project Templates""",
-    'author': "Patrick Wilson, Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/project",
-    'category': 'Project Management',
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'depends': ['project'],
-    'data': [
-        'views/project.xml',
-    ],
-    'application': False,
-    'development_status': 'Beta',
-    'maintainers': ['patrickrwilson'],
+    "name": "Project Templates",
+    "summary": """Project Templates""",
+    "author": "Patrick Wilson, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "category": "Project Management",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project.xml"],
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["patrickrwilson"],
 }
