0116bedb80c7446f013a1bd6dcab7d7044679d6b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-07 11:45:18 +0200

    [MIG] project_timesheet_time_control: Migration to 13.0
    - Standard procedure
    - README regeneration
    - Start new timer little adjustment
    - Avoid new check for shared m2m tables

--- project_timesheet_time_control/__manifest__.py
+++ project_timesheet_time_control/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Project timesheet time control",
-    "version": "12.0.2.1.0",
+    "version": "13.0.1.0.0",
     "development_status": "Mature",
     "category": "Project",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
