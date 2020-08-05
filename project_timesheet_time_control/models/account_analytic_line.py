520c18cf8b12dbe46e654170c15dfae9bb6d7939
David James <david@djdc.net.au>
2020-08-05 19:53:14 +1000

    [FIX] project_timesheet_time_control: User timezone + comp. with hr_timesheet_activity_begin_end + field for end time
    - take users timezone into account when setting date field
    - Prior to this commit, trying to resume an activity which had a start
      time or end time set would cause the newly created timesheet to throw
      a validation error because time_stop - time_start must equal duration.
      This commit fixes that issues by adding those fields to the ignore list
    - Adds a field to the timesheet to store the end time. It is not
      required to enter the end time, if it is not entered it is calculated
      automatically by comparing the start time and the duration, if only a
      start time and end time are entered, then the duration is calculated
      accordingly. The end time field is set to optional in the tree view.

--- project_timesheet_time_control/models/account_analytic_line.py
+++ project_timesheet_time_control/models/account_analytic_line.py
@@ -5,6 +5,8 @@
 
 from datetime import datetime
 
+from dateutil.relativedelta import relativedelta
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
@@ -13,18 +15,53 @@ class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
     _order = "date_time desc"
 
-    date_time = fields.Datetime(default=fields.Datetime.now, copy=False,)
+    date_time = fields.Datetime(
+        string="Start Time", default=fields.Datetime.now, copy=False
+    )
+    date_time_end = fields.Datetime(
+        string="End Time",
+        compute="_compute_date_time_end",
+        inverse="_inverse_date_time_end",
+    )
     show_time_control = fields.Selection(
         selection=[("resume", "Resume"), ("stop", "Stop")],
         compute="_compute_show_time_control",
         help="Indicate which time control button to show, if any.",
     )
 
+    @api.depends("date_time", "unit_amount", "product_uom_id")
+    def _compute_date_time_end(self):
+        hour_uom = self.env.ref("uom.product_uom_hour")
+        for record in self:
+            if (
+                record.product_uom_id == hour_uom
+                and record.date_time
+                and record.unit_amount
+            ):
+                record.date_time_end = record.date_time + relativedelta(
+                    hours=record.unit_amount
+                )
+            else:
+                record.date_time_end = record.date_time_end
+
+    def _inverse_date_time_end(self):
+        hour_uom = self.env.ref("uom.product_uom_hour")
+        for record in self.filtered(lambda x: x.date_time and x.date_time_end):
+            if record.product_uom_id == hour_uom:
+                record.unit_amount = (
+                    record.date_time_end - record.date_time
+                ).seconds / 3600
+
     @api.model
     def _eval_date(self, vals):
         if vals.get("date_time"):
-            return dict(vals, date=fields.Date.to_date(vals["date_time"]))
+            return dict(vals, date=self._convert_datetime_to_date(vals["date_time"]))
         return vals
+
+    def _convert_datetime_to_date(self, datetime_):
+        if isinstance(datetime_, str):
+            datetime_ = fields.Datetime.from_string(datetime_)
+        return fields.Date.context_today(self, datetime_)
 
     @api.model
     def _running_domain(self):
