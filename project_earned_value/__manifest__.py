ec37f0123b60c4ed11513c3b0e626b168dc2f2ac
Khalid <m.khalid@serpentcs.com>
2020-08-25 17:30:05 +0530

    IMP Code

--- project_earned_value/__manifest__.py
+++ project_earned_value/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2020 - TODAY, Open Source Integrators
+# Copyright (C) 2020 Open Source Integrators
 # Copyright (C) 2020 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "HR Gamification Email Notification",
+    "name": "Project Earned Value",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": """Open Source Integrators,
--- project_earned_value/controllers/main.py
+++ project_earned_value/controllers/main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 - TODAY, Open Source Integrators
+# Copyright (C) 2020 Open Source Integrators
 # Copyright (C) 2020 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.http import request
