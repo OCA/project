439fae3a923637efff714411964772542b7cafee
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2020-10-13 10:36:26 -0300

    [FIX] Flake8

--- project_wsjf/models/project_task.py
+++ project_wsjf/models/project_task.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019  Gabriel Cardoso de Faria - KMEE - www.kmee.com.br
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api
+from odoo import models, api
 
 
 class ProjectTask(models.Model):
