b92c47fdf258363cfdb6139480553074734ed0f7
emagdalena <emagdalena@c2i.es>
2020-10-25 21:55:55 +0100

    [MIG] project_category: Migration to 14.0

--- project_category/tests/test_project_category.py
+++ project_category/tests/test_project_category.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Onestein
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
