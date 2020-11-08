978ed6a1bdebb58db97e136311c7703a84ab7dd0
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-11-08 08:15:35 +0200

    [UPD] Brainbean Apps => CorporateHub
    [UPD] README.rst

--- project_role/__manifest__.py
+++ project_role/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
@@ -6,7 +7,7 @@
     "version": "13.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
-    "author": "Brainbean Apps, " "Odoo Community Association (OCA)",
+    "author": "CorporateHub, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "application": False,
