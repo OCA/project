3c636c37d50ff5181cbbbd4a5eb1e768c2e98c2b
skeller1 <MiStK@gmx.de>
2020-11-15 23:14:16 +0100

    [MIG] project_template: Migration to 14.0

--- project_template/tests/test_project_template.py
+++ project_template/tests/test_project_template.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests import common
 
