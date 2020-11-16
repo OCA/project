06204481da8b6320d429c1c533761cd09c2521b1
Yoshiro009 <gamso321@gmail.com>
2020-11-16 14:27:14 +0700

    [MIG] project_task_add_very_high Migration to 14.0

---
+++ project_status/models/project.py
@@ -0,0 +1,19 @@
+from odoo import SUPERUSER_ID, api, fields, models
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    @api.model
+    def _read_group_status_ids(self, statuses, domain, order):
+        statuse_ids = statuses._search([], order=order, access_rights_uid=SUPERUSER_ID)
+        return statuses.browse(statuse_ids)
+
+    project_status = fields.Many2one(
+        "project.status",
+        string="Project Status",
+        group_expand="_read_group_status_ids",
+        copy=False,
+        ondelete="restrict",
+        index=True,
+    )
