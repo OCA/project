288ada0ce1a58356011ec254bdb9355b3c5c9591
Yoshiro009 <gamso321@gmail.com>
2020-11-20 13:19:16 +0700

    [14.0][MIG]project_stage_closed

--- project_stage_closed/models/task_stage.py
+++ project_stage_closed/models/task_stage.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 
 
 class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
+    _inherit = "project.task.type"
 
     closed = fields.Boolean(
         help="Tasks in this stage are considered closed.",
