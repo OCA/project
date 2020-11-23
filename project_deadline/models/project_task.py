7633cb4ccf09bf2adc84358f107c49328539669f
Andrea <a.stirpe@onestein.nl>
2020-11-23 15:24:58 +0100

    pre-commit

--- project_deadline/models/project_task.py
+++ project_deadline/models/project_task.py
@@ -1,18 +1,14 @@
 # Copyright 2019 Onestein
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     project_date_start = fields.Date(
-        'Project Start Date',
-        related='project_id.date_start'
+        "Project Start Date", related="project_id.date_start"
     )
 
-    project_date_deadline = fields.Date(
-        'Project Deadline',
-        related='project_id.date'
-    )
+    project_date_deadline = fields.Date("Project Deadline", related="project_id.date")
