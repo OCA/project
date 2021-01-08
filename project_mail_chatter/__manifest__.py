00da55300305026212b57aab99d276408acea548
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-01-08 13:03:43 +0530

    [ADD] Added a project_mail_chatter module.

---
+++ project_mail_chatter/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2015-2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Mail Chatter",
+    "summary": "Display Project log in a chatter view.",
+    "version": "12.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project_view.xml"],
+    "installable": True,
+}
