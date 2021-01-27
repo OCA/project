68169cce1b7a7539be71c1400560bd67d4467689
Yoshiro009 <gamso321@gmail.com>
2021-01-27 14:03:04 +0700

    [14.0][MIG]project_task_code

--- project_task_code/models/project_task.py
+++ project_task_code/models/project_task.py
@@ -1,41 +1,35 @@
 # Copyright 2016 Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     code = fields.Char(
-        string='Task Number',
+        string="Task Number",
         required=True,
-        default='/',
+        default="/",
         readonly=True,
     )
 
     _sql_constraints = [
-        (
-            'project_task_unique_code',
-            'UNIQUE (code)',
-            _('The code must be unique!')
-        ),
+        ("project_task_unique_code", "UNIQUE (code)", _("The code must be unique!")),
     ]
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get('code', '/') == '/':
-                vals['code'] = self.env['ir.sequence'].next_by_code(
-                    'project.task'
-                )
+            if vals.get("code", "/") == "/":
+                vals["code"] = self.env["ir.sequence"].next_by_code("project.task")
         return super().create(vals_list)
 
     def copy(self, default=None):
         self.ensure_one()
         if default is None:
             default = {}
-        default['code'] = self.env['ir.sequence'].next_by_code('project.task')
+        default["code"] = self.env["ir.sequence"].next_by_code("project.task")
         return super().copy(default)
 
     def name_get(self):
@@ -44,6 +38,6 @@ class ProjectTask(models.Model):
 
         for task in result:
             rec = self.browse(task[0])
-            name = '[%s] %s' % (rec.code, task[1])
+            name = "[{}] {}".format(rec.code, task[1])
             new_result.append((rec.id, name))
         return new_result
