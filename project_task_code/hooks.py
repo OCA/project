134cabab60720f9fafa503087f69b6cbbcbdf432
Yoshiro009 <gamso321@gmail.com>
2021-01-27 14:03:04 +0700

    [14.0][MIG]project_task_code

--- project_task_code/hooks.py
+++ project_task_code/hooks.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def pre_init_hook(cr):
@@ -10,10 +10,8 @@ def pre_init_hook(cr):
     code constraint when the module is installed and before the post-init-hook
     is launched.
     """
-    cr.execute('ALTER TABLE project_task '
-               'ADD COLUMN code character varying;')
-    cr.execute('UPDATE project_task '
-               'SET code = id;')
+    cr.execute("ALTER TABLE project_task ADD COLUMN code character varying;")
+    cr.execute("UPDATE project_task SET code = id;")
 
 
 def post_init_hook(cr, registry):
@@ -22,11 +20,14 @@ def post_init_hook(cr, registry):
     corresponding sequence code.
     """
     env = api.Environment(cr, SUPERUSER_ID, dict())
-    task_obj = env['project.task']
-    sequence_obj = env['ir.sequence']
+    task_obj = env["project.task"]
+    sequence_obj = env["ir.sequence"]
     tasks = task_obj.search([], order="id")
     for task_id in tasks.ids:
-        cr.execute('UPDATE project_task '
-                   'SET code = %s '
-                   'WHERE id = %s;',
-                   (sequence_obj.next_by_code('project.task'), task_id,))
+        cr.execute(
+            "UPDATE project_task SET code = %s WHERE id = %s;",
+            (
+                sequence_obj.next_by_code("project.task"),
+                task_id,
+            ),
+        )
