266b56639a19747ccbfadf08b84eb36d74218dfd
Yoshiro009 <gamso321@gmail.com>
2021-01-28 14:05:34 +0700

    [14.0][MIG]project_task_material

--- project_task_material/__manifest__.py
+++ project_task_material/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Project Task Material",
     "summary": "Record products spent in a Task",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Project Management",
     "author": "Daniel Reis," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
