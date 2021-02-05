d3b8e39777b39fcbcebdb8f0e04db9305b654895
Paidy Kumar <pku@dreambits.in>
2021-02-05 19:58:35 +0530

    [MIG] : Migration to 14.0

--- project_task_pull_request/tests/test_project_task_pull_request.py
+++ project_task_pull_request/tests/test_project_task_pull_request.py
@@ -14,16 +14,23 @@ class TestProjectTaskPullRequest(TransactionCase):
 
         project_obj = self.env["project.project"]
         task_obj = self.env["project.task"]
-        done_stage = self.ref("project.project_stage_2")
-        inprogress_stage = self.ref("project.project_stage_1")
+        stages = self.env["project.task.type"]
+        self.stage1 = stages.create({"name": "a"})
+        self.stage3 = stages.create({"name": "c"})
+        self.stage4 = stages.create({"name": "f"})
+        self.done_stage = stages.create({"name": "d"})
+        self.inprogress_stage = stages.create({"name": "e"})
 
         self.project_1 = project_obj.create(
-            {"name": "Test Project 1", "pr_required_states": [(4, done_stage)]}
+            {"name": "Test Project 1", "pr_required_states": [(4, self.done_stage.id)]}
         )
         self.project_2 = project_obj.create(
             {
                 "name": "Test Project 2",
-                "pr_required_states": [(4, done_stage), (4, inprogress_stage)],
+                "pr_required_states": [
+                    (4, self.done_stage.id),
+                    (4, self.inprogress_stage.id),
+                ],
             }
         )
 
@@ -32,7 +39,7 @@ class TestProjectTaskPullRequest(TransactionCase):
                 "name": "Test Task 1",
                 "project_id": self.project_1.id,
                 "pr_uri": False,
-                "stage_id": 1,
+                "stage_id": self.stage1.id,
             }
         )
         self.task_2 = task_obj.create(
@@ -40,7 +47,7 @@ class TestProjectTaskPullRequest(TransactionCase):
                 "name": "Test Task 2",
                 "project_id": self.project_2.id,
                 "pr_uri": False,
-                "stage_id": 1,
+                "stage_id": self.stage1.id,
             }
         )
         self.task_3 = task_obj.create(
@@ -48,24 +55,24 @@ class TestProjectTaskPullRequest(TransactionCase):
                 "name": "Test Task 3",
                 "project_id": self.project_2.id,
                 "pr_uri": "github.com",
-                "stage_id": 1,
+                "stage_id": self.stage1.id,
             }
         )
 
     def test_write_allowed_when_allowed(self):
-        self.task_1.write({"stage_id": 3})
+        self.task_1.write({"stage_id": self.stage3.id})
         self.task_1.refresh()
-        self.assertEquals(3, self.task_1.stage_id.id)
+        self.assertEqual(self.stage3.id, self.task_1.stage_id.id)
 
     def test_write_not_allowed_without_pr(self):
         with self.assertRaises(ValidationError):
-            self.task_1.write({"stage_id": 6})
+            self.task_1.write({"stage_id": self.done_stage.id})
 
     def test_write_not_allowed_without_pr_multiple_stages(self):
         with self.assertRaises(ValidationError):
-            self.task_2.write({"stage_id": 6})
+            self.task_2.write({"stage_id": self.inprogress_stage.id})
 
     def test_write_allowed_with_pr(self):
-        self.task_3.write({"stage_id": 6})
+        self.task_3.write({"stage_id": self.stage4.id})
         self.task_3.refresh()
-        self.assertEquals(6, self.task_3.stage_id.id)
+        self.assertEqual(self.stage4.id, self.task_3.stage_id.id)
