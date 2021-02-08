f0de18c4ed1bda11f5f59239d98b9b90951d414e
paidykumar <pku@dreambits.in>
2021-02-08 12:48:28 +0530

    [MIG] project_milestone: Migration to 14.0

--- project_milestone/models/project.py
+++ project_milestone/models/project.py
@@ -1,13 +1,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
-    milestone_ids = fields.One2many('project.milestone',
-                                    'project_id',
-                                    string="Milestones",
-                                    copy=True)
+    milestone_ids = fields.One2many(
+        "project.milestone", "project_id", string="Milestones", copy=True
+    )
     use_milestones = fields.Boolean(help="Does this project use milestones?")
