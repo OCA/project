0b11fb03c8a66dadf9151d3d87f26f9e34676275
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-03-17 17:08:50 +0530

    [MIG] project_task_digitized_signature: Migration to 13.0

--- project_task_digitized_signature/__manifest__.py
+++ project_task_digitized_signature/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "Project Task Digitized Signature",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
     "license": "AGPL-3",
-    "depends": ["project_task_report", "web_widget_digitized_signature",],
-    "data": ["report/project_task_report.xml", "views/project_task_views.xml",],
+    "depends": ["project_task_report"],
+    "data": ["report/project_task_report.xml", "views/project_task_views.xml"],
     "installable": True,
 }
--- project_task_digitized_signature/models/project.py
+++ project_task_digitized_signature/models/project.py
@@ -1,23 +1,10 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
     customer_signature = fields.Binary(string="Customer acceptance",)
-
-    @api.model
-    def create(self, values):
-        task = super(ProjectTask, self).create(values)
-        if task.customer_signature:
-            values = {"customer_signature": task.customer_signature}
-            task._track_signature(values, "customer_signature")
-        return task
-
-    @api.multi
-    def write(self, values):
-        self._track_signature(values, "customer_signature")
-        return super(ProjectTask, self).write(values)
