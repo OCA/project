8b9396439b93a88e76ad00a39081dce155206d6c
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-03-17 17:08:50 +0530

    [MIG] project_task_digitized_signature: Migration to 13.0

--- project_task_digitized_signature/readme/CONTRIBUTORS.rst
+++ project_task_digitized_signature/readme/CONTRIBUTORS.rst
@@ -5,3 +5,8 @@
 * `TAKOBI <https://takobi.online>`_:
 
   * Lorenzo Battistini
+
+* `Open Source Integrators <https://www.opensourceintegrators.com>`_:
+
+  * Bhavesh Odedra <bodedra@opensourceintegrators.com>
+  * Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
--- project_task_digitized_signature/tests/test_project_task_digitized_signature.py
+++ project_task_digitized_signature/tests/test_project_task_digitized_signature.py
@@ -8,29 +8,15 @@ class TestProjectTaskDigitizedSignature(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestProjectTaskDigitizedSignature, cls).setUpClass()
-        cls.task = cls.env["project.task"].create({"name": "Task test #1",})
+        cls.task = cls.env["project.task"].create({"name": "Task test #1"})
 
     def test_task_digitized_signature(self):
-        search_domain = [
-            ("model", "=", "project.task"),
-            ("res_id", "=", self.task.id),
-            ("body", "like", "Signature has been created%"),
-        ]
-        # There is not messages created
-        messages = self.env["mail.message"].search(search_domain)
-        self.assertEqual(len(messages.ids), 0)
-        # We add signature and write. Message must be created.
+        # We add signature and write.
         self.task.customer_signature = (
             "R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw =="
         )
-        messages = self.env["mail.message"].search(search_domain)
-        self.assertEqual(len(messages.ids), 1)
-        # We create a new one. Message must be created.
+        # We create a new one.
+        sign = "R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw =="
         self.task = self.env["project.task"].create(
-            {
-                "name": "Task test #2",
-                "customer_signature": "R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw ==",
-            }
+            {"name": "Task test #2", "customer_signature": sign}
         )
-        messages = self.env["mail.message"].search(search_domain)
-        self.assertEqual(len(messages.ids), 1)
