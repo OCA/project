fa3090a5e9918d6e50a6b828065fd15587f499d2
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-03-18 19:30:25 -0400

    [IMP] project_risk: black, isort, prettier

--- project_risk/models/project_project.py
+++ project_risk/models/project_project.py
@@ -2,20 +2,17 @@
 # Copyright 2020 Manuel Calero - Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     project_risk_ids = fields.One2many(
-        comodel_name='project.risk',
-        inverse_name='project_id'
+        comodel_name="project.risk", inverse_name="project_id"
     )
 
-    project_risk_count = fields.Integer(
-        compute='_compute_risk_count'
-    )
+    project_risk_count = fields.Integer(compute="_compute_risk_count")
 
     def _compute_risk_count(self):
         for project in self:
@@ -23,12 +20,8 @@ class Project(models.Model):
 
     def view_risk(self):
         self.ensure_one()
-        action = self.env.ref('project_risk.project_risk_action')
+        action = self.env.ref("project_risk.project_risk_action")
         action = action.read()[0]
-        action['context'] = {
-            'default_project_id': self.id
-        }
-        action['domain'] = [
-            ('project_id', '=', self.id)
-        ]
+        action["context"] = {"default_project_id": self.id}
+        action["domain"] = [("project_id", "=", self.id)]
         return action
--- project_risk/models/project_risk_response.py
+++ project_risk/models/project_risk_response.py
@@ -1,16 +1,14 @@
 # Copyright 2019 Onestein
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectRiskResponse(models.Model):
-    _name = 'project.risk.response'
-    _description = 'Project Risks Responses'
+    _name = "project.risk.response"
+    _description = "Project Risks Responses"
 
-    project_risk_id = fields.Many2one(
-        comodel_name='project.risk.response'
-    )
+    project_risk_id = fields.Many2one(comodel_name="project.risk.response")
 
     sequence = fields.Integer()
 
