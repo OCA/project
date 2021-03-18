a1eb36478bedfbcd7fb27f2e9a9ce2b9b21d4469
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-03-18 19:34:56 -0400

    [MIG] project_risk: Migration to 13.0

--- project_risk/__manifest__.py
+++ project_risk/__manifest__.py
@@ -5,7 +5,7 @@
     "license": "AGPL-3",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": ["project"],
     "data": [
         "security/ir_model_access.xml",
