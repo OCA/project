28468fd42397c33ab3781ac180a9bab7b93fdef0
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-03-18 19:30:25 -0400

    [IMP] project_risk: black, isort, prettier

--- project_risk/models/project_risk_category.py
+++ project_risk/models/project_risk_category.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Onestein
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectRiskCategory(models.Model):
-    _name = 'project.risk.category'
-    _description = 'Project Risks Categories'
+    _name = "project.risk.category"
+    _description = "Project Risks Categories"
 
     name = fields.Char(required=True)
