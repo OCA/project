0fe98c19b0535b2346b40dd079eede4049677f46
Pierre Verkest <pierreverkest84@gmail.com>
2021-05-23 15:47:52 +0200

    [14.0] project_stage_closed: deprecated module
    This module is useless as long odoo added the is_closed field
    in version 14.0.
    
    This module is present to make easier migratoin from v13.0 to 14.0

---
+++ project_stage_closed/migrations/2.0.0/post-remove-project-task-type-closed-field.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def migrate(cr, _):
+    logger.info(
+        "Set project.task.type is_closed field using closed field if set "
+        " and drop closed field"
+    )
+    cr.execute(
+        """UPDATE project_task_type SET is_closed = closed
+            WHERE NOT closed is not NULL;
+        """
+        "ALTER TABLE project_task_type DROP COLUMN closed;"
+    )
