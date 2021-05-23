850cb8debe95b60b8b7d5b4aa66c24facc7eede5
Pierre Verkest <pierreverkest84@gmail.com>
2021-05-23 20:48:44 +0200

    remove logging

--- project_stage_closed/migrations/2.0.0/post-remove-project-task-type-closed-field.py
+++ project_stage_closed/migrations/2.0.0/post-remove-project-task-type-closed-field.py
@@ -1,13 +1,4 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
 def migrate(cr, _):
-    logger.info(
-        "Set project.task.type is_closed field using closed field if set "
-        " and drop closed field"
-    )
     cr.execute(
         """UPDATE project_task_type SET is_closed = closed
             WHERE closed is not NULL;
