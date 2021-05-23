3d93605ab7ada0dffb2e8e587b1ea7b4ff3aa104
Pierre Verkest <pierreverkest84@gmail.com>
2021-05-23 20:55:49 +0200

    remove useless commented code

--- project_stage_closed/migrations/2.0.0/pre-remove-project-task-type-closed-field.py
+++ project_stage_closed/migrations/2.0.0/pre-remove-project-task-type-closed-field.py
@@ -3,5 +3,4 @@ def migrate(cr, _):
         """UPDATE project_task_type SET is_closed = closed
             WHERE closed is not NULL;
         """
-        # "ALTER TABLE project_task_type DROP COLUMN closed;"
     )
