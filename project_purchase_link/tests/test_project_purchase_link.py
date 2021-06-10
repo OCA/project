82c586874f761dc851b09de0cd13365550c2ef8a
emagdalena <emagdalena@c2i.es>
2021-06-10 03:39:08 +0200

    [FIX] Tests

--- project_purchase_link/tests/test_project_purchase_link.py
+++ project_purchase_link/tests/test_project_purchase_link.py
@@ -73,8 +73,7 @@ class TestProjectPurchaseUtilities(common.SavepointCase):
         purchase_line_dict = self.project.button_open_purchase_order_line()
         self.assertEqual(purchase_line_dict.get("domain"), purchase_domain)
         invoice_domain = [
-            "&",
-            ("id", "in", [invoice.id]),  # only one test invoice (line)
+            ("id", "in", [invoice.id])  # only one test invoice (line)
         ]
         invoice_dict = self.project.button_open_purchase_invoice()
         self.assertEqual(invoice_dict.get("domain"), invoice_domain)
