f43d93a2fdb943e5dbe96d466143e4485b7677ac
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2021-07-06 16:55:18 +0530

    [FIX] FiXed  a Travis.

--- project_stock_request/models/project.py
+++ project_stock_request/models/project.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 
 
 class Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
-    stock_request_order_ids = fields.One2many('stock.request.order',
-                                              'project_id',
-                                              string='Stock Request Orders')
+    stock_request_order_ids = fields.One2many(
+        "stock.request.order", "project_id", string="Stock Request Orders"
+    )
--- project_stock_request/models/project_task.py
+++ project_stock_request/models/project_task.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
-    stock_request_order_ids = fields.One2many('stock.request.order',
-                                              'project_task_id',
-                                              string='Stock Request Orders')
+    stock_request_order_ids = fields.One2many(
+        "stock.request.order", "project_task_id", string="Stock Request Orders"
+    )
