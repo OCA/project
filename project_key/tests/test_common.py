88a37c2a775d1daf63d4b14951bfac35b082d2ef
Simone Orsi <simahawk@gmail.com>
2021-08-20 13:32:50 +0200

    project_key: boost tests

--- project_key/tests/test_common.py
+++ project_key/tests/test_common.py
@@ -1,18 +1,18 @@
 # Copyright 2017 - 2018 Modoolar <info@modoolar.com>
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
-from odoo.tests.common import HttpCase, TransactionCase
+from odoo.tests.common import HttpCase, SavepointCase
 
 
-class TestCommon(TransactionCase):
-    def setUp(self):
-        super(TestCommon, self).setUp()
-
+class TestMixin(object):
+    @staticmethod
+    def _setup_records(class_or_instance):
+        self = class_or_instance
         self.Project = self.env["project.project"].with_context(test_project_key=True)
         self.Task = self.env["project.task"].with_context(test_project_key=True)
 
-        self.project_action = self.ref("project.open_view_project_all_config")
-        self.task_action = self.ref("project.action_view_task")
+        self.project_action = self.env.ref("project.open_view_project_all_config")
+        self.task_action = self.env.ref("project.action_view_task")
 
         self.project_1 = self.Project.create({"name": "OCA"})
         self.project_2 = self.Project.create({"name": "Odoo", "key": "ODOO"})
@@ -34,11 +34,22 @@ class TestCommon(TransactionCase):
         )
 
     def get_task_url(self, task):
-        return self.get_record_url(task, task._name, self.task_action)
+        return self.get_record_url(task, task._name, self.task_action.id)
 
     def get_project_url(self, project):
-        return self.get_record_url(project, project._name, self.project_action)
+        return self.get_record_url(project, project._name, self.project_action.id)
 
 
-class HttpTestCommon(HttpCase, TestCommon):
-    pass
+class TestCommon(SavepointCase, TestMixin):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls._setup_records(cls)
+
+
+class HttpTestCommon(HttpCase, TestMixin):
+    def setUp(self):
+        super().setUp()
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        self._setup_records(self)
