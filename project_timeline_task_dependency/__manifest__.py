8caa9a35b1a45dec923dfa3683dab9aec5fd7c8b
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-08-25 09:12:57 +0200

    [MIG] project_timeline_task_dependency: Migration to 14.0

--- project_timeline_task_dependency/__manifest__.py
+++ project_timeline_task_dependency/__manifest__.py
@@ -8,7 +8,7 @@
     "license": "AGPL-3",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["project_timeline", "project_task_dependency"],
     "data": ["views/project_task_view.xml"],
     "installable": True,
