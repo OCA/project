5ac117205151246149efe98969b48a65adeb4da0
Danny de Jong <ddejong@therp.nl>
2021-11-25 13:19:02 +0100

    [IMP] project_task_code: Improved the migration script so that no task code of NULL (or '/') remains.

--- project_task_code/migrations/12.0.1.1/pre-migration.py
+++ project_task_code/migrations/12.0.1.1/pre-migration.py
@@ -10,20 +10,27 @@ def uniqify_codes(env):
     env.cr.execute("SELECT DISTINCT(code) FROM project_task")
     for row in env.cr.dictfetchall():
         task_code = row['code']
+        code_is_null = not task_code or task_code == '/'
 
-        if task_code or task_code == '/':
-            env.cr.execute("SELECT id FROM project_task WHERE code = %s", [task_code])
+        if not code_is_null:
+            env.cr.execute("SELECT id FROM project_task WHERE code = %s",
+                [task_code])
         else:
-            env.cr.execute("SELECT id FROM project_task WHERE code IS NULL")
+            env.cr.execute("SELECT id FROM project_task"
+                           "WHERE code IS NULL OR code = '/'")
         similar_task_ids = [x['id'] for x in env.cr.dictfetchall()]
 
-        # Only change the tasks that are not the first of its kind
-        lowest_task_id = min(similar_task_ids)
+        # Only change the tasks that are not the first of its kind and
+        # not with code NULL.
+        preserved_task_id = min(similar_task_ids) if not code_is_null else -1
         for similar_task_id in similar_task_ids:
-            if similar_task_id > lowest_task_id:
+            if similar_task_id > preserved_task_id:
                 new_code = env['ir.sequence'].next_by_code('project.task')
 
-                env.cr.execute("UPDATE project_task SET code = %s WHERE id = %s", [new_code, similar_task_id])
+                env.cr.execute(
+                    "UPDATE project_task SET code = %s WHERE id = %s",
+                    [new_code, similar_task_id]
+                )
 
 
 @openupgrade.migrate()
