367eb6c67aeab84d4088eddaa806f4bd0cefec44
Ruchir Shukla <ruchir@bizzappdev.com>
2022-02-09 19:07:50 +0530

    [MIG]project_task_pull_request: Migrate project_task_pull_request to v15.
    [UPD] Update project_task_pull_request.pot
    
    [UPD] README.rst

--- project_task_pull_request/__manifest__.py
+++ project_task_pull_request/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Task Pull Request",
     "summary": "Adds a field for a PR URI to project tasks",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "Project Management",
     "website": "https://github.com/OCA/project",
     "author": "SMDrugstore, " "Tecnativa, " "Odoo Community Association (OCA)",
