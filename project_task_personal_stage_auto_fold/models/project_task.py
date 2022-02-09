38a4dd9b8641fb54268a128ca96e76c0cb4293ba
Eduardo De Miguel <edu@moduon.team>
2022-02-09 13:55:25 +0100

    [ADD] Auto fold task on Personal Stage when is closed
    This module adds the behavior to change the Personal Stage of the task to the first Folded one when the task reachs an stage with "Mark as Done".
    If the Personal Stage is already folded, does nothing.
    
    Simplifies the behavior of closing a task, go back to Personal Stages and move it to the Pesonal Done stage.
    Chooses Personal Stages with the same name before Closing stages or Folded stages.

---
+++ project_task_personal_stage_auto_fold/models/project_task.py
@@ -0,0 +1,39 @@
+from odoo import models
+
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    def write(self, vals):
+        result = super().write(vals)
+        if (
+            vals.get("stage_id")
+            and self.env["project.task.type"].browse(vals.get("stage_id")).is_closed
+        ):
+            self._fold_personal_stage_task()
+        return result
+
+    def _fold_personal_stage_task(self):
+        # Find all folded personal stages
+        folded_stages = (
+            self.env["project.task.type"]
+            .search(
+                [
+                    ("user_id", "=", self.env.user.id),
+                    "|",
+                    ("fold", "=", True),
+                    ("is_closed", "=", True),
+                ],
+            )
+            .sorted(lambda ptt: ptt.is_closed, reverse=True)
+        )
+        if not folded_stages:
+            return
+        # Allow to find personal stage with same name as shared stage
+        stage_by_name = {stage.name: stage for stage in folded_stages}
+        # Apply best matching personal stage
+        for task in self:
+            task.personal_stage_type_id = stage_by_name.get(
+                task.stage_id.name,
+                folded_stages[0],
+            )
