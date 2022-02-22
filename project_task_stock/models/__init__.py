2ee926a93485ee6dda0206544789991dc48536b7
Víctor Martínez <victor.martinez@tecnativa.com>
2022-02-22 10:25:54 +0100

    [ADD] project_stock: New addon TT34554
    [UPD] Update project_stock.pot
    
    [UPD] README.rst

---
+++ project_task_stock/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_analytic_line
+from . import project_project
+from . import project_task
+from . import stock_move
+from . import stock_scrap
