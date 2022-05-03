885b482fa77a230853c5f279c53f47e2038f46f7
Yu Weng <yweng@elegosoft.com>
2022-05-04 00:12:32 +0200

    [MIG] project_mail_chatter: Migration to 14.0

--- project_mail_chatter/__manifest__.py
+++ project_mail_chatter/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Project Mail Chatter",
     "summary": "Add message chatter on the Project form.",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
