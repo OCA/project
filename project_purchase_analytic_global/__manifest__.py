30b7f3cf3735ceb5149339e9da4b57320949ce10
Maksym Yankin <yankinmk@gmail.com>
2022-05-05 13:25:13 +0300

    [15.0][ADD] project_purchase_analytic_global

---
+++ project_purchase_analytic_global/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Project - Create Purchase Orders with Analytic Account",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainer": "Camptocamp",
+    "license": "AGPL-3",
+    "category": "Project",
+    "complexity": "easy",
+    "depends": ["project_purchase", "purchase_analytic_global"],
+    "website": "https://github.com/OCA/project",
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
