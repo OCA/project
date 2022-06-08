af4b759b0122a9c3e7f0ecfe75482fff8bab2e5c
Er. Vaidehi Vasani <er.vaidehi.vasani@gmail.com>
2022-06-08 10:25:03 +0530

    [MIG] project_department in v15

---
+++ project_department/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Project Department Categorization",
+    "version": "15.0.1.0.0",
+    "author": "Brain-tec AG, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Projects & Services",
+    "website": "https://github.com/OCA/project",
+    "depends": [
+        "project",
+    ],
+    "data": ["views/project.xml"],
+}
---
+++ project_department/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* `Brain-tec AG <https://bt-group.com/>`__
+
+  * Vaidehi Vasani <er.vaidehi.vasani@gmail.com>
