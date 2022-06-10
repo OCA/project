835055fd63b1d3ac8573de775f35fc34a79cd812
Giovanni Francesco Capalbo <giovanni@therp.nl>
2022-06-10 09:25:25 +0200

    [UPD] consistency in licensing and module version change

--- project_parent/models/project.py
+++ project_parent/models/project.py
@@ -1,5 +1,4 @@
 # Copyright 2019 Therp BV <https://therp.nl>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 
 
--- project_parent/post_install.py
+++ project_parent/post_install.py
@@ -1,5 +1,4 @@
 # Copyright 2019 Therp BV <https://therp.nl>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
 def restore_parents(cr, registry):
