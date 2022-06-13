03d925f9290a92cc7da0a9bd5afca1876b55318c
Er. Vaidehi Vasani <er.vaidehi.vasani@gmail.com>
2022-06-13 17:46:14 +0530

    [MIG] project_department in v15

--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -2,13 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
-    "version": "11.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
     "website": "https://github.com/OCA/project",
     "depends": [
-        "analytic_base_department",
         "project",
     ],
     "data": ["views/project.xml"],
