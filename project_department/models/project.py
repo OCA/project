f63a01b4dfa43ff83ff3d1d94b31696b386d23e0
Er. Vaidehi Vasani <er.vaidehi.vasani@gmail.com>
2022-06-13 17:46:14 +0530

    [MIG] project_department in v15

--- project_department/models/project.py
+++ project_department/models/project.py
@@ -4,10 +4,19 @@ from odoo import fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
     project_department_id = fields.Many2one(
-        related='project_id.department_id',
-        string='Project Department',
+        related="project_id.department_id",
+        string="Project Department",
         store=True,
-        readonly=True)
+        readonly=True,
+    )
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    department_id = fields.Many2one(
+        comodel_name="hr.department", string="Project Department"
+    )
