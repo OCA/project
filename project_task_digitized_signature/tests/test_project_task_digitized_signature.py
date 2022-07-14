d86d95525cd2fcde992c2960bae3357ab5fd2ace
David Alonso // Solvos <david.alonso@solvos.es>
2022-07-14 17:02:58 +0200

    [IMP] project_task_digitized_signature: black, isort, prettier

--- project_task_digitized_signature/tests/test_project_task_digitized_signature.py
+++ project_task_digitized_signature/tests/test_project_task_digitized_signature.py
@@ -8,29 +8,29 @@ class TestProjectTaskDigitizedSignature(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestProjectTaskDigitizedSignature, cls).setUpClass()
-        cls.task = cls.env['project.task'].create({
-            'name': 'Task test #1',
-        })
+        cls.task = cls.env["project.task"].create({"name": "Task test #1",})
 
     def test_task_digitized_signature(self):
         search_domain = [
-            ('model', '=', 'project.task'),
-            ('res_id', '=', self.task.id),
-            ('body', 'like', 'Signature has been created%'),
+            ("model", "=", "project.task"),
+            ("res_id", "=", self.task.id),
+            ("body", "like", "Signature has been created%"),
         ]
         # There is not messages created
-        messages = self.env['mail.message'].search(search_domain)
+        messages = self.env["mail.message"].search(search_domain)
         self.assertEqual(len(messages.ids), 0)
         # We add signature and write. Message must be created.
-        self.task.customer_signature = \
-            'R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw =='
-        messages = self.env['mail.message'].search(search_domain)
+        self.task.customer_signature = (
+            "R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw =="
+        )
+        messages = self.env["mail.message"].search(search_domain)
         self.assertEqual(len(messages.ids), 1)
         # We create a new one. Message must be created.
-        self.task = self.env['project.task'].create({
-            'name': 'Task test #2',
-            'customer_signature':
-                'R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw =='
-        })
-        messages = self.env['mail.message'].search(search_domain)
+        self.task = self.env["project.task"].create(
+            {
+                "name": "Task test #2",
+                "customer_signature": "R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw ==",
+            }
+        )
+        messages = self.env["mail.message"].search(search_domain)
         self.assertEqual(len(messages.ids), 1)
