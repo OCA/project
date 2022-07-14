519d8c586a5fe6cb41dbbdc4230d9fee26b65e93
David Alonso // Solvos <david.alonso@solvos.es>
2022-07-14 17:02:58 +0200

    [IMP] project_task_digitized_signature: black, isort, prettier

--- project_task_digitized_signature/models/project.py
+++ project_task_digitized_signature/models/project.py
@@ -5,21 +5,19 @@ from odoo import api, fields, models
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
-    customer_signature = fields.Binary(
-        string='Customer acceptance',
-    )
+    customer_signature = fields.Binary(string="Customer acceptance",)
 
     @api.model
     def create(self, values):
         task = super(ProjectTask, self).create(values)
         if task.customer_signature:
-            values = {'customer_signature': task.customer_signature}
-            task._track_signature(values, 'customer_signature')
+            values = {"customer_signature": task.customer_signature}
+            task._track_signature(values, "customer_signature")
         return task
 
     @api.multi
     def write(self, values):
-        self._track_signature(values, 'customer_signature')
+        self._track_signature(values, "customer_signature")
         return super(ProjectTask, self).write(values)
