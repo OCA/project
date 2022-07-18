f70bbce2ce82ef642d7605c96263f0dbb4e7afd4
Maksym Yankin <yankinmk@gmail.com>
2022-07-18 13:54:13 +0300

    [15.0][ADD] project_forecast_line_bokeh_chart

---
+++ project_forecast_line_bokeh_chart/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Forecast Lines Bokeh Chart",
+    "summary": "Project Forecast Lines Bokeh Chart",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "depends": ["project_forecast_line", "web_widget_bokeh_chart"],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/forecast_line_reporting_views.xml",
+    ],
+    "installable": True,
+}
