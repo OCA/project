50a0132058d00d3483c742ec0d50be10bb11a94c
Maksym Yankin <yankinmk@gmail.com>
2022-07-18 14:17:23 +0300

    [15.0][ADD] project_forecast_line_holidays_public

---
+++ project_forecast_line_holidays_public/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Forecast Lines Holidays Public",
+    "summary": "Project Forecast Lines taking public holidays into account",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "depends": ["project_forecast_line", "hr_holidays_public"],
+    "data": [],
+    "installable": True,
+}
---
+++ project_forecast_line_holidays_public/models/forecast_line.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class ForecastLine(models.Model):
+    _inherit = "forecast.line"
+
+    def prepare_forecast_lines(self, *args, **kwargs):
+        self = self.with_context(exclude_public_holidays=True)
+        return super().prepare_forecast_lines(*args, **kwargs)
+
+    def _cron_recompute_all(self, force_company_id=None):
+        self = self.with_context(exclude_public_holidays=True)
+        return super()._cron_recompute_all(force_company_id)
