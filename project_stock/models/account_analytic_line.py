fb5d5895be32beb738a5692bba9c66dd6eb2575c
Víctor Martínez <victor.martinez@tecnativa.com>
2022-07-25 17:34:17 +0200

    [MIG] project_stock: Migration to 14.0 TT37028

--- project_stock/models/account_analytic_line.py
+++ project_stock/models/account_analytic_line.py
@@ -6,7 +6,9 @@ from odoo import fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    stock_task_id = fields.Many2one(comodel_name="project.task", string="Project Task")
+    stock_task_id = fields.Many2one(
+        comodel_name="project.task", string="Project Task", ondelete="cascade"
+    )
 
     def _timesheet_postprocess_values(self, values):
         """When hr_timesheet addon is installed, in the create() and write() methods,
