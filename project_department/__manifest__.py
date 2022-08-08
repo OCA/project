1f87ba8b19f5482fea6b65aa1f198abd43f3aee2
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-08-08 11:35:43 +0200

    [FIX] project_department: Add dependency to hr

--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
     "website": "https://github.com/OCA/project",
     "depends": [
         "project",
+        "hr",
     ],
     "data": ["views/project.xml"],
 }
