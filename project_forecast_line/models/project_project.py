32563dac7488bb1ba9c6cf06a291a9bc6d1fd82a
Maksym Yankin <yankinmk@gmail.com>
2022-10-18 17:48:33 +0300

    [15.0][IMP] project_forecast_line:
    Add computed fields to written fields to trigger forecast lines update

--- project_forecast_line/models/project_project.py
+++ project_forecast_line/models/project_project.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
 
+from ..utils import get_written_computed_fields
+
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
@@ -9,10 +11,15 @@ class ProjectProject(models.Model):
     def _update_forecast_lines_trigger_fields(self):
         return ["stage_id"]
 
+    # TODO: Stored computed fields updates don't go through write,
+    # but they do go through _write. Check if possible to override
+    # that method instead, to avoid maintaing get_written_computed_fields
     def write(self, values):
         res = super().write(values)
-        written_fields = list(values.keys())
+        written_computed_fields = get_written_computed_fields(
+            self, tuple(sorted(values))
+        )
         trigger_fields = self._update_forecast_lines_trigger_fields()
-        if any(field in written_fields for field in trigger_fields):
+        if any(field in written_computed_fields for field in trigger_fields):
             self.task_ids._update_forecast_lines()
         return res
