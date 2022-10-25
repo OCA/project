4d25c770f1d168d95df5be997d3d34b4a83a0063
otmanelagy <otmanelagy1@gmail.com>
2022-10-25 09:43:27 +0100

    [MIG] project_forecast_line: Migration to 16.0

--- project_forecast_line/tests/test_forecast_line.py
+++ project_forecast_line/tests/test_forecast_line.py
@@ -602,7 +602,7 @@ class TestForecastLineProject(BaseForecastLineTest):
             # form.state = "validate"
             # form.employee_id = self.employee_consultant
             form.holiday_status_id = self.env.ref("hr_holidays.holiday_status_unpaid")
-            form.request_hour_from = "8"
+            # form.request_hour_from = "8"
             form.request_hour_to = "18"
             form.request_date_from = "2022-02-14"
             form.request_unit_half = False
