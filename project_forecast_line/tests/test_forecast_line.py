bcee772bdee339d5869d5b4d5f0be6cfa49bbdbd
otmanelagy <otmanelagy1@gmail.com>
2022-10-25 21:06:02 +0100

    [MIG] project_forecast_line: Migration to 16.0

--- project_forecast_line/tests/test_forecast_line.py
+++ project_forecast_line/tests/test_forecast_line.py
@@ -234,8 +234,8 @@ class TestForecastLineSales(BaseForecastLineTest):
     def test_draft_sale_order_creates_negative_forecast_forecast(self):
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.customer
-            form.state = "draft"
-            form.date_order = "2022-01-10 08:00:00"
+            # form.state = "draft"
+            # form.date_order = "2022-01-10 08:00:00"
             form.default_forecast_date_start = "2022-02-07"
             form.default_forecast_date_end = "2022-02-20"
             with form.order_line.new() as line:
@@ -268,8 +268,7 @@ class TestForecastLineSales(BaseForecastLineTest):
         """a draft sale order with no dates on the line does not create forecast"""
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.customer
-            form.state = "draft"
-            form.date_order = "2022-01-10 08:00:00"
+            # form.date_order = "2022-01-10 08:00:00"
             form.default_forecast_date_start = "2022-02-07"
             form.default_forecast_date_end = False
             with form.order_line.new() as line:
@@ -292,8 +291,7 @@ class TestForecastLineSales(BaseForecastLineTest):
     def test_draft_sale_order_forecast_spread(self):
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.customer
-            form.state = "draft"
-            form.date_order = "2022-01-10 08:00:00"
+            # form.date_order = "2022-01-10 08:00:00"
             form.default_forecast_date_start = "2022-02-07"
             form.default_forecast_date_end = "2022-04-17"
             with form.order_line.new() as line:
@@ -338,8 +336,7 @@ class TestForecastLineSales(BaseForecastLineTest):
     def test_confirm_order_sale_order_no_forecast_line(self):
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.customer
-            form.state = "draft"
-            form.date_order = "2022-01-10 08:00:00"
+            # form.date_order = "2022-01-10 08:00:00"
             form.default_forecast_date_start = "2022-02-14"
             form.default_forecast_date_end = "2022-04-14"
             with form.order_line.new() as line:
@@ -362,8 +359,7 @@ class TestForecastLineSales(BaseForecastLineTest):
     def test_confirm_order_sale_order_create_project_task_with_forecast_line(self):
         with Form(self.env["sale.order"]) as form:
             form.partner_id = self.customer
-            form.state = "draft"
-            form.date_order = "2022-10-10 08:00:00"
+            # form.date_order = "2022-10-10 08:00:00"
             form.default_forecast_date_start = "2022-11-14"
             form.default_forecast_date_end = "2022-12-17"
             with form.order_line.new() as line:
@@ -399,8 +395,7 @@ class TestForecastLineTimesheet(BaseForecastLineTest):
         with freeze_time("2022-01-01"):
             with Form(self.env["sale.order"]) as form:
                 form.partner_id = self.customer
-                form.state = "draft"
-                form.date_order = "2022-01-10 08:00:00"
+                # form.date_order = "2022-01-10 08:00:00"
                 form.default_forecast_date_start = "2022-02-14"
                 form.default_forecast_date_end = "2022-04-17"
                 with form.order_line.new() as line:
