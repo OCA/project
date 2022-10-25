793d1bae11d733f182f725c97c576b5ea7549955
otmanelagy <otmanelagy1@gmail.com>
2022-10-25 10:47:48 +0100

    [MIG] project_forecast_line: Migration to 16.0

--- project_forecast_line/tests/test_forecast_line.py
+++ project_forecast_line/tests/test_forecast_line.py
@@ -598,17 +598,17 @@ class TestForecastLineProject(BaseForecastLineTest):
     def test_forecast_with_holidays(self):
         self.test_task_forecast_lines_consolidated_forecast()
         # with Form(self.env["hr.leave"]) as form:
-            # form.holiday_type = "employee"
-            # form.state = "validate"
-            # form.employee_id = self.employee_consultant
-            # form.holiday_status_id = self.env.ref("hr_holidays.holiday_status_unpaid")
-            # form.request_unit_hour = False
-            # form.request_hour_from = "8"
-            # form.request_hour_to = "18"
-            # form.request_date_from = "2022-02-14"
-            # form.state = 'draft'
-            # form.request_date_to = "2022-02-15"
-            # setattr(form, request_date_to, '2022-02-15')
+        # form.holiday_type = "employee"
+        # form.state = "validate"
+        # form.employee_id = self.employee_consultant
+        # form.holiday_status_id = self.env.ref("hr_holidays.holiday_status_unpaid")
+        # form.request_unit_hour = False
+        # form.request_hour_from = "8"
+        # form.request_hour_to = "18"
+        # form.request_date_from = "2022-02-14"
+        # form.state = 'draft'
+        # form.request_date_to = "2022-02-15"
+        # setattr(form, request_date_to, '2022-02-15')
 
         # leave_request = form.save()
         # validating the leave request will recompute the forecast lines for
