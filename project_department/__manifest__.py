4b045ca97099173da5b83eece852e1bc51074cb5
Aiendry Sarkar <aiendry@aktivsoftware.com>
2022-10-28 10:08:17 +0530

    [MIG] project_department: Migration to 16.0

--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
