ee1288751ef6dd9536f84772e371632f2c013639
Kalpesh Gajera <kgajera@uncannycs.com>
2022-11-02 19:30:33 +0530

    [MIG] project_task_code: Migration to 16.0

--- project_task_code/__manifest__.py
+++ project_task_code/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sequential Code for Tasks",
-    "version": "15.0.1.0.4",
+    "version": "16.0.1.0.0",
     "category": "Project Management",
     "author": "OdooMRP team, "
     "AvanzOSC, "
