3315cf4552841d7a034f7880d01fcbcff9bf3ded
Kalpesh Gajera <kgajera@uncannycs.com>
2022-11-02 18:43:27 +0530

    [MIG] project_duplicate_subtask: Migration to 16.0

--- project_duplicate_subtask/__manifest__.py
+++ project_duplicate_subtask/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2021 ForgeFlow S.L.
+# Copyright (C) 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Project Duplicate subtask",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "summary": "The module adds an action to duplicate tasks with the child subtasks",
