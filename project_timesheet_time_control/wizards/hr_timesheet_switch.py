95e74fa9bd36bac382d164901a53e0ed11d19d0d
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-11-15 12:21:21 +0100

    [FIX] project_timesheet_time_control: Avoid incompatibilities Some modules that add new fields dynamically, as analytic_tag_dimension, will cause an error when trying to read the fields to get them from cache. With this change we avoid that read and we make a copy of the analytic line if the wizard is opened from its and we create a new analytic line if don't.

--- project_timesheet_time_control/wizards/hr_timesheet_switch.py
+++ project_timesheet_time_control/wizards/hr_timesheet_switch.py
@@ -3,13 +3,66 @@
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+from odoo.osv.expression import expression
 
 
 class HrTimesheetSwitch(models.TransientModel):
     _name = "hr.timesheet.switch"
-    _inherit = "account.analytic.line"
     _description = "Helper to quickly switch between timesheet lines"
 
+    def _domain_project_id(self):
+        domain = [("allow_timesheets", "=", True)]
+        if not self.user_has_groups("hr_timesheet.group_timesheet_manager"):
+            return expression.AND(
+                [
+                    domain,
+                    [
+                        "|",
+                        ("privacy_visibility", "!=", "followers"),
+                        ("message_partner_ids", "in", [self.env.user.partner_id.id]),
+                    ],
+                ]
+            )
+        return domain
+
+    analytic_line_id = fields.Many2one(
+        comodel_name="account.analytic.line", string="Origin line"
+    )
+    name = fields.Char(string="Description", required=True)
+    date_time = fields.Datetime(
+        string="Start Time", default=fields.Datetime.now, required=True
+    )
+    date_time_end = fields.Datetime(string="End Time")
+    task_id = fields.Many2one(
+        comodel_name="project.task",
+        string="Task",
+        compute="_compute_task_id",
+        store=True,
+        readonly=False,
+        index=True,
+        domain="""
+            [
+                ('company_id', '=', company_id),
+                ('project_id.allow_timesheets', '=', True),
+                ('project_id', '=?', project_id)
+            ]
+        """,
+    )
+    project_id = fields.Many2one(
+        comodel_name="project.project",
+        string="Project",
+        compute="_compute_project_id",
+        store=True,
+        readonly=False,
+        domain=_domain_project_id,
+    )
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        required=True,
+        readonly=True,
+        default=lambda self: self.env.company,
+    )
     running_timer_id = fields.Many2one(
         comodel_name="account.analytic.line",
         string="Previous timer",
@@ -28,8 +81,16 @@ class HrTimesheetSwitch(models.TransientModel):
         compute="_compute_running_timer_duration",
         help="When the previous timer is stopped, it will save this duration.",
     )
-    # Redefine the relation to avoid using the same table than parent model
-    tag_ids = fields.Many2many(relation="hr_timesheet_switch_line_tag_rel")
+
+    @api.depends("task_id", "task_id.project_id")
+    def _compute_project_id(self):
+        for line in self.filtered(lambda line: not line.project_id):
+            line.project_id = line.task_id.project_id
+
+    @api.depends("project_id")
+    def _compute_task_id(self):
+        for line in self.filtered(lambda line: not line.project_id):
+            line.task_id = False
 
     @api.model
     def _default_running_timer_id(self, employee=None):
@@ -59,76 +120,63 @@ class HrTimesheetSwitch(models.TransientModel):
     def _compute_running_timer_duration(self):
         """Compute duration of running timer when stopped."""
         for one in self:
-            one.running_timer_duration = one._duration(
-                one.running_timer_id.date_time,
-                one.date_time,
-            )
+            one.running_timer_duration = 0.0
+            if one.running_timer_id:
+                one.running_timer_duration = one.running_timer_id._duration(
+                    one.running_timer_id.date_time,
+                    one.date_time,
+                )
 
     @api.model
     def _closest_suggestion(self):
         """Find most similar account.analytic.line record."""
-        try:
-            active = self.env[self.env.context["active_model"]].browse(
-                self.env.context["active_id"]
-            )
-        except KeyError:
-            # If I don't know where's the user, I don't know what to suggest
-            return self.env["account.analytic.line"].browse()
-        # If you're browsing another account.analytic.line, that's the match
-        if active._name == "account.analytic.line":
-            return active
-        # If browsing other models, prepare a search
+        context = self.env.context
+        model = "account.analytic.line"
         domain = [("employee_id", "in", self.env.user.employee_ids.ids)]
-        if active._name == "project.task":
-            domain.append(("task_id", "=", active.id))
-        elif active._name == "project.project":
+        if context.get("active_model") == model:
+            return self.env[model].browse(context["active_id"])
+        elif context.get("active_model") == "project.task":
+            domain.append(("task_id", "=", context["active_id"]))
+        elif context.get("active_model") == "project.project":
             domain += [
-                ("project_id", "=", active.id),
+                ("project_id", "=", context["active_id"]),
                 ("task_id", "=", False),
             ]
         else:
-            # No clues for other records, sorry
-            return self.env["account.analytic.line"].browse()
+            return self.env[model]
         return self.env["account.analytic.line"].search(
             domain,
             order="date_time DESC",
             limit=1,
         )
 
+    def _prepare_default_values(self, account_analytic_line):
+        return {
+            "analytic_line_id": account_analytic_line.id,
+            "name": account_analytic_line.name,
+            "project_id": account_analytic_line.project_id.id,
+            "task_id": account_analytic_line.task_id.id,
+        }
+
     @api.model
     def default_get(self, fields_list):
         """Return defaults depending on the context where it is called."""
         result = super().default_get(fields_list)
         inherited = self._closest_suggestion()
-        assert inherited._name == "account.analytic.line"
-        # Inherit all possible fields from that account.analytic.line record
         if inherited:
-            # Convert inherited to RPC-style values
-            _fields = set(fields_list) & set(inherited._fields) - {
-                # These fields must always be reset
-                "id",
-                "amount",
-                "date_time",
-                "date_time_end",
-                "date",
-                "is_task_closed",
-                "unit_amount",
-                # This field is from sale_timesheet, which is not among
-                # this module dependencies; ignoring it will let you
-                # resume an invoiced AAL if that module is installed,
-                # and it doesn't hurt here
-                "timesheet_invoice_id",
-                # These fields are from the hr_timesheet_activity_begin_end
-                # module. Unless ignored, these fields will cause a validation
-                # error because time_stop - time_start must equal duration.
-                "time_start",
-                "time_stop",
-            }
-            inherited.read(_fields)
-            values = inherited._convert_to_write(inherited._cache)
-            for field in _fields:
-                result[field] = values[field]
+            result.update(self._prepare_default_values(inherited))
         return result
+
+    def _prepare_copy_values(self, record):
+        """Return the values that will be overwritten in new timesheet entry."""
+        return {
+            "name": record.name,
+            "date_time": record.date_time,
+            "date_time_end": record.date_time_end,
+            "project_id": record.project_id.id,
+            "task_id": record.task_id.id,
+            "unit_amount": 0,
+        }
 
     def action_switch(self):
         """Stop old timer, start new one."""
@@ -139,12 +187,13 @@ class HrTimesheetSwitch(models.TransientModel):
             stop_dt=self.date_time,
         ).running_timer_id.button_end_work()
         # Start new timer
-        _fields = self.env["account.analytic.line"]._fields.keys()
-        self.read(_fields)
-        values = self._convert_to_write(self._cache)
-        new = self.env["account.analytic.line"].create(
-            {field: value for (field, value) in values.items() if field in _fields}
-        )
+        if self.analytic_line_id:
+            new = self.analytic_line_id.copy(self._prepare_copy_values(self))
+        else:
+            fields = self.env["account.analytic.line"]._fields.keys()
+            vals = self.env["account.analytic.line"].default_get(fields)
+            vals.update(self._prepare_copy_values(self))
+            new = self.env["account.analytic.line"].create(vals)
         # Display created timer record if requested
         if self.env.context.get("show_created_timer"):
             form_view = self.env.ref("hr_timesheet.hr_timesheet_line_form")
