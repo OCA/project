ccd295567da2342fb02d778ad76832528fe98b81
Urvisha-OSI <udesai@opensourceintegrators.com>
2022-11-18 15:33:00 +0530

    [ADD] Migrated project_purchase_link module:

---
+++ project_purchase_link/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Purchase Link",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "depends": ["project", "purchase", "hr_timesheet"],
+    "website": "https://github.com/OCA/project",
+    "category": "Project",
+    "data": ["views/project_project_view.xml"],
+    "installable": True,
+    "maintainers": ["udesai", "OCA"],
+}
---
+++ project_purchase_link/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2022 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_project_purchase_link
