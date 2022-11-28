b936c0be050ffb0d5d01e0eff4d6c9175c837e88
Urvisha@OSI <udesai@opensourceintegrators.com>
2022-11-28 15:41:54 +0530

    [IMP] project_purchase_link: black, isort, prettier

--- project_purchase_link/__manifest__.py
+++ project_purchase_link/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     "name": "Project Purchase Link",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["project", "purchase", "hr_timesheet"],
     "author": "AvanzOSC, " "Odoo Community Association (OCA)",
-    "website": "http://github.com/OCA/project.git",
+    "website": "https://github.com/OCA/project",
     "category": "Project",
     "data": ["views/project_project_view.xml"],
     "installable": True,
