d654e666b92eefe2ddd7878d15a44f1746664903
Denis Leemann <denis.leemann@camptocamp.com>
2022-12-21 16:07:36 +0100

    [MIG] project_role: Migration to 16.0

--- project_role/__manifest__.py
+++ project_role/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Project Roles",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "CorporateHub, Odoo Community Association (OCA)",
