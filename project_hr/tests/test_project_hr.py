eb089c2fc59a3105c32f8301c8e8693b4881dee5
chandni299 <chandnip445@gmail.com>
2023-02-01 17:34:29 +0100

    [MIG] project_hr: Migration to 15.0

--- project_hr/tests/test_project_hr.py
+++ project_hr/tests/test_project_hr.py
@@ -1,20 +1,43 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
+
+_logger = logging.getLogger(__name__)
 
 
-class TestProjectHr(SavepointCase):
+class TestProjectHr(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user_model = cls.env["res.users"]
-        cls.user = cls.user_model.create(
-            {"login": "test_project_hr", "name": "Test user"}
+        user_group_employee = cls.env.ref("base.group_user")
+        user_group_project_user = cls.env.ref("project.group_project_user")
+        # Test users to use through the various tests
+        Users = cls.env["res.users"].with_context(no_reset_password=True)
+        cls.user1 = Users.create(
+            {
+                "name": "Test User1",
+                "login": "user1",
+                "password": "user1",
+                "email": "user1.projecthr@example.com",
+                "groups_id": [
+                    (6, 0, [user_group_employee.id, user_group_project_user.id])
+                ],
+            }
         )
-        cls.user_2 = cls.user_model.create(
-            {"login": "test_project_hr_2", "name": "Test user 2"}
+        cls.user2 = Users.create(
+            {
+                "name": "Test User2",
+                "login": "user2",
+                "password": "user2",
+                "email": "user2.projecthr@example.com",
+                "groups_id": [
+                    (6, 0, [user_group_employee.id, user_group_project_user.id])
+                ],
+            }
         )
         cls.hr_category = cls.env["hr.employee.category"].create(
             {"name": "Test employee category"}
@@ -30,7 +53,7 @@ class TestProjectHr(SavepointCase):
         cls.employee = cls.env["hr.employee"].create(
             {
                 "name": "Test employee",
-                "user_id": cls.user.id,
+                "user_id": cls.user1.id,
                 "category_ids": [(6, 0, cls.hr_category.ids)],
             }
         )
@@ -42,44 +65,43 @@ class TestProjectHr(SavepointCase):
                 "name": "Test task",
                 "project_id": cls.project.id,
                 "hr_category_ids": [(4, cls.hr_category.id)],
-                "user_id": cls.user.id,
+                "user_ids": [(6, 0, [cls.user1.id])],
             }
         )
 
     def test_user(self):
-        self.assertEqual(self.user.hr_category_ids, self.hr_category)
+        self.assertEqual(self.user1.hr_category_ids, self.hr_category)
         self.employee.category_ids = [(4, self.hr_category_2.id)]
         self.assertEqual(
-            self.user.hr_category_ids, self.hr_category + self.hr_category_2
+            self.user1.hr_category_ids, self.hr_category + self.hr_category_2
         )
         # Check if need invalidate cache
         self.employee.category_ids = [(4, self.hr_category_3.id)]
         self.assertEqual(
-            self.user.hr_category_ids,
+            self.user1.hr_category_ids,
             self.hr_category + self.hr_category_2 + self.hr_category_3,
         )
 
     def test_task(self):
+        # check computed values on task
         self.assertEqual(self.task.employee_id, self.employee)
         self.assertEqual(self.task.allowed_hr_category_ids, self.hr_category)
+        self.assertEqual(self.task.allowed_assigned_user_ids, self.user1)
         self.project.hr_category_ids = [(4, self.hr_category_2.id)]
         self.assertEqual(
             self.task.allowed_hr_category_ids, self.hr_category + self.hr_category_2
         )
-        self.assertEqual(self.task.allowed_assigned_user_ids, self.user)
         self.env["hr.employee"].create(
             {
                 "name": "Test employee 2",
-                "user_id": self.user_2.id,
+                "user_id": self.user2.id,
                 "category_ids": [(6, 0, self.hr_category.ids)],
             }
         )
-        self.assertEqual(self.task.allowed_assigned_user_ids, self.user + self.user_2)
+        self.assertEqual(self.task.allowed_assigned_user_ids, self.user1 + self.user2)
         # Test _check_employee_category_user constraint
         with self.assertRaises(ValidationError):
             self.task.hr_category_ids = [(4, self.hr_category_2.id)]
-        with self.assertRaises(ValidationError):
-            self.task.user_id = self.user_2.id
         # Test _check_employee_category_project constraint
         self.project.hr_category_ids = [(4, self.hr_category_2.id)]
         with self.assertRaises(ValidationError):
