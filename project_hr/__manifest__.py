67f7effb48635024b558bbd564ec3f1eb3c7530e
chandni299 <chandnip445@gmail.com>
2023-02-01 17:34:29 +0100

    [MIG] project_hr: Migration to 15.0

--- project_hr/__manifest__.py
+++ project_hr/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Project HR",
     "summary": "Link HR with project",
     "development_status": "Production/Stable",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
