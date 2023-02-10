960397ee34f905be013742ff6601d9cfdd279849
DinhTien24 <15520880@gm.uit.edu.vn>
2023-02-10 16:21:23 +0700

    [MIG] project_template_milestone: Migration to 15.0

--- project_template_milestone/__manifest__.py
+++ project_template_milestone/__manifest__.py
@@ -8,7 +8,7 @@
     "Open Source Integrators",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["project_template", "project_milestone"],
     "application": False,
