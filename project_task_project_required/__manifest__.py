9e84d61cf827c39bfc0d937c796692fe46ffe002
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-03-06 13:01:21 +0100

    [IMP] project_task_project_required: pre-commit execution

--- project_task_project_required/__manifest__.py
+++ project_task_project_required/__manifest__.py
@@ -3,23 +3,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Project: require Project on Task',
-    'summary': """
+    "name": "Project: require Project on Task",
+    "summary": """
         Set project on task as a mandatory field""",
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'author':
-        'CorporateHub, '
-        'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/project',
-    'depends': [
-        'project',
+    "version": "12.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "CorporateHub, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "depends": [
+        "project",
     ],
-    'data': [
-        'views/project_task.xml',
-        'views/res_config_settings.xml',
+    "data": [
+        "views/project_task.xml",
+        "views/res_config_settings.xml",
     ],
-    'demo': [
-        'demo/res_company.xml',
+    "demo": [
+        "demo/res_company.xml",
     ],
 }
