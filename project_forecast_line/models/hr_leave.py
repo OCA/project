3b63cf56f04a45a591199eb0c3fbb5078b53c494
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2023-04-18 00:27:28 +0200

    [FIX] forecast_line: issue in multicompany
    when running in multi company, sometimes the user making a change is not connected
    using the company of the record. In this case the default value for company_id on
    forecast.line will lead to a wrong forecast.
    
    we fix this by using the best company when updating the forecast (the
    employee's company for task assigned to employees or leaves, the sale
    line's company for sales forecast

--- project_forecast_line/models/hr_leave.py
+++ project_forecast_line/models/hr_leave.py
@@ -49,6 +49,7 @@ class HrLeave(models.Model):
                 continue
             else:
                 forecast_type = "forecast"
+            ForecastLine = ForecastLine.with_company(leave.employee_company_id)
             forecast_vals += ForecastLine._prepare_forecast_lines(
                 name=_("Leave"),
                 date_from=leave.date_from.date(),
