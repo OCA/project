43331ca17c07565b1ce4c91e8d6096dd0c1ba99e
Jairo Llopis <yajo.sk8@gmail.com>
2023-05-10 10:22:01 +0100

    [FIX] project_sequence: make compatible with hr_timesheet
    `hr_timesheet` creates an analytic account by default. The method it uses to create it expects a preexisting name. But since we're making name not required, we're breaking other module's expectations.
    
    To fix this problem, now the name sync is done before writing or creating records, and not after.
    
    To make sure the problem doesn't happen anymore, we keep the `NOT NULL` requirement on project names. We just do it with a manual SQL constraint. This extra protection ensures compatibility with any other modules that expect always a value on the name.
    
    Any possibly misconfigured sequence could produce sequence duplicates. I also add protection against that.
    
    In tests, the project sequence was wrongly reset to 11 only once. Turns out that it survives the test savepoint, so I now reset it before each test instead. Tests are more reliable now. Besides, I added some more.
    
    @moduon MT-1506
    
    wip
    
    wip

--- project_sequence/models/project_project.py
+++ project_sequence/models/project_project.py
@@ -7,19 +7,33 @@ from odoo import api, fields, models
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
+    _sql_constraints = [
+        # Ensure compatibility with other modules that always expect a value in name
+        ("name_required", "CHECK(name IS NOT NULL)", "Project name is required"),
+        (
+            "sequence_code_unique",
+            "UNIQUE(sequence_code)",
+            "Sequence code must be unique",
+        ),
+    ]
 
     sequence_code = fields.Char(
-        readonly=True,
         copy=False,
+        readonly=True,
     )
     name = fields.Char(
+        # We actually require it with the SQL constraint, but it is disabled
+        # here to let users create/write projects without name, and let this module
+        # add a default name if needed
         required=False,
     )
 
-    def _sync_name(self):
-        """Set name if empty."""
-        for rec in self - self.filtered("name"):
-            rec.name = rec.sequence_code
+    def _sync_analytic_account_name(self):
+        """Set analytic account name equal to project's display name."""
+        for rec in self:
+            if not rec.analytic_account_id:
+                continue
+            rec.analytic_account_id.name = rec.display_name
 
     def name_get(self):
         """Prefix name with sequence code if they are different."""
@@ -41,15 +55,33 @@ class ProjectProject(models.Model):
             args = (args or []) + ["|", ("sequence_code", operator, name)]
         return super().name_search(name, args, operator, limit)
 
-    @api.model
-    def create(self, vals):
-        res = super().create(vals)
-        res.sequence_code = self.env["ir.sequence"].next_by_code("project.sequence")
-        res._sync_name()
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Apply sequence code and a default name if not set."""
+        # It is important to set sequence_code before calling super() because
+        # other modules such as hr_timesheet expect the name to always have a value
+        for vals in vals_list:
+            if not vals.get("sequence_code"):
+                vals["sequence_code"] = self.env["ir.sequence"].next_by_code(
+                    "project.sequence"
+                )
+            if not vals.get("name"):
+                vals["name"] = vals["sequence_code"]
+        res = super().create(vals_list)
+        # The analytic account is created with just the project name, but
+        # it is more useful to let it contain the project sequence too
+        res._sync_analytic_account_name()
         return res
 
     def write(self, vals):
-        res = super().write(vals)
-        if "name" in vals:
-            self._sync_name()
-        return res
+        """Sync name and analytic account name when name is changed."""
+        # If name isn't changing, nothing special to do
+        if "name" not in vals and "sequence_name" not in vals:
+            return super().write(vals)
+        # When changing name, we need to update the analytic account name too
+        for one in self:
+            sequence_code = vals.get("sequence_code", one.sequence_code)
+            name = vals.get("name") or sequence_code
+            super().write(dict(vals, name=name))
+        self._sync_analytic_account_name()
+        return True
