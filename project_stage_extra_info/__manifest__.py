e9ec28289e9fb5a9a30b5d937bf4a5aa1d604c12
dsolanki <dhara.solanki@initos.com>
2023-05-12 17:26:35 +0530

    [16.0][ADD] project_stage_extra_info
    This module adds a Description and Is closed stage field on Project stages.

---
+++ project_stage_extra_info/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Project Stage Extra Info",
+    "summary": """
+        Project Stage Extra Info""",
+    "author": "InitOS GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "category": "Project Management",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": [
+        "views/project_stage_view.xml",
+        "views/project_view.xml",
+    ],
+    "application": False,
+    "development_status": "Beta",
+}
---
+++ project_stage_extra_info/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds a Description and Is closed stage field on Project stages. Also adds its filters on Project.
