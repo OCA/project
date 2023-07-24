097a925c299525de2acc5cce35818aa45b37a603
Aungkokolin1997 <lin@quartile.co>
2023-07-24 12:07:24 +0630

    [ADD] project_task_name_with_id

---
+++ project_task_name_with_id/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Task Name with ID",
+    "category": "Project",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "installable": True,
+}
