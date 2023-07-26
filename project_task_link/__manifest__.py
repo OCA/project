2bac479625ec23631754303f4c45ca44585affd2
Aungkokolin1997 <lin@quartile.co>
2023-07-26 10:23:33 +0630

    [ADD] project_task_link

---
+++ project_task_link/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Task Link",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+}
