b6260c4cf656a1af875b1691538692fe2995e54f
Nils Coenen <nico-solutions@users.noreply.github.com>
2023-07-26 15:02:46 +0200

    [REF] Renaming module project_stage_mgmt -> project_task_stage_mgmt

--- project_task_stage_mgmt/__manifest__.py
+++ project_task_stage_mgmt/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    "name": "Project Stage Management",
+    "name": "Project Task Stage Management",
     "version": "16.0.1.0.0",
     "development_status": "Beta",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
