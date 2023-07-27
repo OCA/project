3e9601fefc1f69ba2b89d09476b6624ee510977b
Adasat <a.torres@binhex.es>
2023-07-27 11:26:03 +0000

    [16.0][MIG]project_task_digitized_signature: Migration to 16.0

--- project_task_digitized_signature/__manifest__.py
+++ project_task_digitized_signature/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Task Digitized Signature",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
