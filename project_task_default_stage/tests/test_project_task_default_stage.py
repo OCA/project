cc49f228fc332e2afaa92d6f88174bc52d587ed8
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 08:29:00 +0200

    [IMP] project_task_default_stage: test performance improvement - Switch to setUpClass for avoiding repeat the same setup for each test. - Include context keys for avoiding mail operations overhead.

--- project_task_default_stage/tests/test_project_task_default_stage.py
+++ project_task_default_stage/tests/test_project_task_default_stage.py
@@ -7,9 +7,20 @@ from odoo.tests.common import TransactionCase
 class TestProjectCaseDefault(TransactionCase):
 
     # Use case : Prepare some data for current test case
-    def setUp(self):
-        super(TestProjectCaseDefault, self).setUp()
-        self.project = self.env["project.project"].create({"name": "Project Test"})
+    @classmethod
+    def setUpClass(cls):
+        super(TestProjectCaseDefault, cls).setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        cls.project = cls.env["project.project"].create({"name": "Project Test"})
 
     def test_project_new(self):
         self.assertEqual(len(self.project.type_ids), 7)
