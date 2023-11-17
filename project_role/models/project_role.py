e9116bd419097fe884fb865d80f8c439129a904c
David Ramia <david.ramia@processcontrol.es>
2023-11-17 08:44:21 +0100

    [IMP] project_role: Add unaccent=False to parent_path field

--- project_role/models/project_role.py
+++ project_role/models/project_role.py
@@ -17,9 +17,7 @@ class ProjectRole(models.Model):
     active = fields.Boolean(
         default=True,
     )
-    parent_path = fields.Char(
-        index=True,
-    )
+    parent_path = fields.Char(index=True, unaccent=False)
     parent_id = fields.Many2one(
         string="Parent Role",
         comodel_name="project.role",
