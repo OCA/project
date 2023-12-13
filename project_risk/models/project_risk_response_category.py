f3b9c47735f5be35ebe1f026a322ce6bc55f82ff
jumeldi74 <jumeldi74@gmail.com>
2023-12-13 14:30:40 +0700

    [IMP] project_risk: pre-commit stuff

--- project_risk/models/project_risk_response_category.py
+++ project_risk/models/project_risk_response_category.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Onestein
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectRiskResponseCategory(models.Model):
-    _name = 'project.risk.response.category'
-    _description = 'Project Risks Responses Categories'
+    _name = "project.risk.response.category"
+    _description = "Project Risks Responses Categories"
 
     name = fields.Char(required=True)
