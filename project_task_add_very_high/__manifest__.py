fec6ce90fe2bb328ad9278bcdc5dd529abf5b911
Yves Goldberg <admin@ygol.com>
2024-01-18 11:04:20 +0200

    [MIG] project_task_add_very_high: Migration to 17.0

--- project_task_add_very_high/__manifest__.py
+++ project_task_add_very_high/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Task Add Very High",
     "summary": "Adds extra options 'High' and 'Very High' on tasks",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Production/Stable",
     "author": "Onestein, Odoo Community Association (OCA)",
     "maintainers": ["astirpe"],
