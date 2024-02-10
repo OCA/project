31f94d61b7bea765fa2e3fa627511c6df4767f3a
Cyril VINH-TUNG <cyril@invitu.com>
2024-02-10 11:11:00 -1000

    [MIG] project_task_digitized_signature: Migration to 16.0

--- project_task_digitized_signature/tests/test_project_task_digitized_signature.py
+++ project_task_digitized_signature/tests/test_project_task_digitized_signature.py
@@ -4,7 +4,7 @@
 from odoo.tests import common
 
 
-class TestProjectTaskDigitizedSignature(common.SavepointCase):
+class TestProjectTaskDigitizedSignature(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestProjectTaskDigitizedSignature, cls).setUpClass()
