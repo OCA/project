6d782c19919cc23afd64c6d6dccd74b3c10c4ab7
ntsirintanis <ntsirintanis@therp.nl>
2024-04-15 11:19:10 +0200

    [MIG] project_forecast_line_deadline: Migration to 16.0

--- project_forecast_line_deadline/__manifest__.py
+++ project_forecast_line_deadline/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Project Forecast Line Deadline",
     "summary": "Project Forecast end date based on deadline of task.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "maintainers": ["ntsirintanis"],
     "license": "AGPL-3",
