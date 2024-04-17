5e21b8978d78d7bbfaaa8dce8958c6edd2b6b7a4
wout <wout.swennen@dynapps.be>
2024-04-17 09:26:29 +0200

    [IMP] project_purchase_link: pre-commit stuff

--- project_purchase_link/__manifest__.py
+++ project_purchase_link/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Purchase Link",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["project", "purchase", "hr_timesheet"],
     "author": "AvanzOSC, " "Odoo Community Association (OCA)",
