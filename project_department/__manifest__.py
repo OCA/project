59a045e83f0078b2b27a0d98e7af4c6bd7e3aed3
David-Luis-Mora <d.luis@binhex.cloud>
2024-04-25 14:34:37 +0100

    [MIG] project_department: Migration to 17.0

--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Department Categorization",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
