b6f57de09977778ae68a4afc57ab647b18bc90d3
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-06-07 15:08:03 +0300

    [CHG] Copyright text

--- project_milestone_spent_hours/__manifest__.py
+++ project_milestone_spent_hours/__manifest__.py
@@ -1,4 +1,4 @@
-# © 2024 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
