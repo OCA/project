0ff56c638259c6e871bc0635a5e1fd20ee3d56cc
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-06-11 18:05:33 +0300

    [FIX] Unit test updating milestone with no project
    [FIX] Unit test updating milestone with no project
    
    [FIX] Unit test updating milestone with no project
    
    [CHG] Test on force compute
    
    [FIX] Unit test updating milestone with no project
    
    [FIX] Unit test updating milestone with no project
    
    [FIX] Active field on project.milestone

--- project_milestone_spent_hours/models/project_milestone.py
+++ project_milestone_spent_hours/models/project_milestone.py
@@ -7,12 +7,24 @@ from odoo import api, fields, models
 class ProjectMilestone(models.Model):
     _inherit = "project.milestone"
 
+    active = fields.Boolean("Active", default=True)
     total_hours = fields.Float(
         compute="_compute_total_hours",
         string="Total Hours",
         compute_sudo=True,
         store=True,
     )
+
+    def write(self, vals):
+        res = super(ProjectMilestone, self).write(vals)
+        if "project_id" in vals:
+            self._remove_task_milestones(vals["project_id"])
+        return res
+
+    def _remove_task_milestones(self, project_id):
+        self.with_context(active_test=False).mapped("project_task_ids").filtered(
+            lambda milestone: not project_id or milestone.project_id.id != project_id
+        ).write({"milestone_id": False})
 
     @api.depends(
         "project_task_ids",
