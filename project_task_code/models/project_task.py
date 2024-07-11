741bd82ab49c097e51813195e1480d24db56b104
Dmytro <=>
2024-07-11 18:26:34 +0300

    [UPD] project_task_code: replaced name_search with _name_search extension

--- project_task_code/models/project_task.py
+++ project_task_code/models/project_task.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
+from odoo.osv import expression
 
 
 class ProjectTask(models.Model):
@@ -43,15 +44,8 @@ class ProjectTask(models.Model):
         return new_result
 
     @api.model
-    def name_search(self, name, args=None, operator="ilike", limit=100):
-        result = []
-
-        if name:
-            domain = [("code", operator, name)]
-            tasks = self.search(domain, limit=limit)
-            result = tasks.name_get()
-
-        return (
-            super().name_search(name, args=args, operator=operator, limit=limit)
-            + result
-        )
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        new_args = expression.OR([args or [], [("code", operator, name)]])
+        return super()._name_search(name, new_args, operator, limit, name_get_uid)
