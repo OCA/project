a1a0961f43a514665df7d2f34147ce2c768b0756
PicchiSeba <picchiseba+git@gmail.com>
2024-08-08 10:04:56 +0200

    [MIG] project_task_recurring_activity: Migration to 16.0

--- project_task_recurring_activity/__manifest__.py
+++ project_task_recurring_activity/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Cetmix, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["project"],
     "data": [
