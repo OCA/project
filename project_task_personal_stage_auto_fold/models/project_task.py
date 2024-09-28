154ea2f76fa9e1f2c58f9e9d7b5b7e6fa23abfb7
nikul-serpentcs <nikul.chaudhary.serpentcs@gmail.com>
2024-09-28 12:51:33 +0530

    [MIG] project_task_personal_stage_auto_fold: Migrated to v17

--- project_task_personal_stage_auto_fold/models/project_task.py
+++ project_task_personal_stage_auto_fold/models/project_task.py
@@ -6,14 +6,19 @@ class Task(models.Model):
 
     def write(self, vals):
         result = super().write(vals)
-        if (
-            vals.get("stage_id")
-            and self.env["project.task.type"].browse(vals.get("stage_id")).fold
-        ):
+        stage_id = vals.get("stage_id")
+        if stage_id and self.env["project.task.type"].browse(stage_id).fold:
             self._fold_personal_stage_task()
         return result
 
     def _fold_personal_stage_task(self):
+        """
+        Assigns a folded personal stage to tasks when they are moved to a folded stage.
+        The function finds all folded stages associated with the current user and
+        applies the best matching personal stage based on the task's stage name. If
+        no exact match is found, the first available folded personal stage is assigned.
+        """
+
         # Find all folded personal stages
         folded_stages = (
             self.env["project.task.type"]
@@ -25,13 +30,12 @@ class Task(models.Model):
             )
             .sorted(lambda ptt: ptt.fold, reverse=True)
         )
-        if not folded_stages:
-            return
         # Allow to find personal stage with same name as shared stage
         stage_by_name = {stage.name: stage for stage in folded_stages}
         # Apply best matching personal stage
+        default_stage = folded_stages[0]
         for task in self:
             task.personal_stage_type_id = stage_by_name.get(
                 task.stage_id.name,
-                folded_stages[0],
+                default_stage,
             )
--- project_task_personal_stage_auto_fold/readme/CONFIGURE.rst
+++
@@ -1,4 +0,0 @@
-The task stages "Done" and "Canceled" must match the same name as the equivalent
-personal stages.
-
-The checkbox “Closing stage” must be checked in the stage configuration.
