24721add1e78e3aab401943f1c1a4958d976146b
Víctor Martínez <victor.martinez@tecnativa.com>
2024-10-11 13:08:07 +0200

    [ADD] project_tag_hierarchy: New module
    TT51195

---
+++ project_tag_hierarchy/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Tag Hierarchy",
+    "version": "16.0.1.0.0",
+    "category": "Project Management",
+    "website": "https://github.com/OCA/project",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "views/project_tags_views.xml",
+    ],
+    "maintainers": ["victoralmau"],
+}
