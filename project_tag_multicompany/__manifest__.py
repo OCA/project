52b5705fc6c1f662978e208b19727506a5b0a94b
Víctor Martínez <victor.martinez@tecnativa.com>
2024-10-14 17:12:57 +0200

    [ADD] project_tag_multicompany: New module
    TT51195
    
    [UPD] Update project_tag_multicompany.pot

---
+++ project_tag_multicompany/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Tag Multicompany",
+    "version": "16.0.1.0.0",
+    "category": "Project Management",
+    "website": "https://github.com/OCA/project",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "security/project_tags_security.xml",
+        "views/project_tags_views.xml",
+    ],
+    "maintainers": ["victoralmau"],
+}
