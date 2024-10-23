8dddf34fb79e701b2f00501123a860684343ab24
Carlos Lopez <carlos.lopez@tecnativa.com>
2024-10-23 07:58:01 -0500

    [FIX] project_timeline: add fields in report.project.task.user to prevent errors on timeline view complementary to commit 42bd28c184729eaf07bd7eb399f36049cd31e2af

---
+++ project_timeline/report/project_report.py
@@ -0,0 +1,28 @@
+# Copyright 2024 Tecnativa - Carlos López
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ReportProjectTaskUser(models.Model):
+    _inherit = "report.project.task.user"
+
+    planned_date_start = fields.Datetime(readonly=True)
+    planned_date_end = fields.Datetime(readonly=True)
+
+    def _select(self):
+        return (
+            super()._select()
+            + """,
+            t.planned_date_start,
+            t.planned_date_end"""
+        )
+
+    def _group_by(self):
+        return (
+            super()._group_by()
+            + """,
+            t.planned_date_start,
+            t.planned_date_end
+            """
+        )
