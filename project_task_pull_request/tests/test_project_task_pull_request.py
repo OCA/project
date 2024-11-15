74da599bb39dc816b431d70ced8480e330b4358e
Víctor Martínez <victor.martinez@tecnativa.com>
2024-11-15 09:33:30 +0100

    [MIG] project_task_pull_request: Migration to 17.0
    TT51645

--- project_task_pull_request/tests/test_project_task_pull_request.py
+++ project_task_pull_request/tests/test_project_task_pull_request.py
@@ -2,75 +2,73 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestProjectTaskPullRequest(TransactionCase):
-    post_install = True
-    at_install = False
-
-    def setUp(self):
-        super(TestProjectTaskPullRequest, self).setUp()
-
-        project_obj = self.env["project.project"]
-        task_obj = self.env["project.task"]
-        self.new_stage = self.ref("project.project_stage_0")
-        self.inprogress_stage = self.ref("project.project_stage_1")
-        self.done_stage = self.ref("project.project_stage_2")
-        self.cancel_stage = self.ref("project.project_stage_3")
-
-        self.project_1 = project_obj.create(
-            {"name": "Test Project 1", "pr_required_states": [(4, self.done_stage)]}
+@tagged("post_install", "-at_install")
+class TestProjectTaskPullRequest(BaseCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        project_obj = cls.env["project.project"]
+        task_obj = cls.env["project.task"]
+        cls.new_stage = cls.env.ref("project.project_stage_0")
+        cls.inprogress_stage = cls.env.ref("project.project_stage_1")
+        cls.done_stage = cls.env.ref("project.project_stage_2")
+        cls.cancel_stage = cls.env.ref("project.project_stage_3")
+        cls.project_1 = project_obj.create(
+            {"name": "Test Project 1", "pr_required_states": [(4, cls.done_stage.id)]}
         )
-        self.project_2 = project_obj.create(
+        cls.project_2 = project_obj.create(
             {
                 "name": "Test Project 2",
                 "pr_required_states": [
-                    (4, self.done_stage),
-                    (4, self.inprogress_stage),
+                    (4, cls.done_stage.id),
+                    (4, cls.inprogress_stage.id),
                 ],
             }
         )
-
-        self.task_1 = task_obj.create(
+        cls.task_1 = task_obj.create(
             {
                 "name": "Test Task 1",
-                "project_id": self.project_1.id,
+                "project_id": cls.project_1.id,
                 "pr_uri": False,
-                "stage_id": self.new_stage,
+                "stage_id": cls.new_stage.id,
             }
         )
-        self.task_2 = task_obj.create(
+        cls.task_2 = task_obj.create(
             {
                 "name": "Test Task 2",
-                "project_id": self.project_2.id,
+                "project_id": cls.project_2.id,
                 "pr_uri": False,
-                "stage_id": self.new_stage,
+                "stage_id": cls.new_stage.id,
             }
         )
-        self.task_3 = task_obj.create(
+        cls.task_3 = task_obj.create(
             {
                 "name": "Test Task 3",
-                "project_id": self.project_2.id,
+                "project_id": cls.project_2.id,
                 "pr_uri": "github.com",
-                "stage_id": self.new_stage,
+                "stage_id": cls.new_stage.id,
             }
         )
 
     def test_write_allowed_when_allowed(self):
-        self.task_1.write({"stage_id": self.inprogress_stage})
+        self.task_1.write({"stage_id": self.inprogress_stage.id})
         self.task_1.invalidate_recordset()
-        self.assertEqual(self.inprogress_stage, self.task_1.stage_id.id)
+        self.assertEqual(self.inprogress_stage, self.task_1.stage_id)
 
     def test_write_not_allowed_without_pr(self):
         with self.assertRaises(ValidationError):
-            self.task_1.write({"stage_id": self.done_stage})
+            self.task_1.write({"stage_id": self.done_stage.id})
 
     def test_write_not_allowed_without_pr_multiple_stages(self):
         with self.assertRaises(ValidationError):
-            self.task_2.write({"stage_id": self.inprogress_stage})
+            self.task_2.write({"stage_id": self.inprogress_stage.id})
 
     def test_write_allowed_with_pr(self):
-        self.task_3.write({"stage_id": self.done_stage})
+        self.task_3.write({"stage_id": self.done_stage.id})
         self.task_3.invalidate_recordset()
-        self.assertEqual(self.done_stage, self.task_3.stage_id.id)
+        self.assertEqual(self.done_stage, self.task_3.stage_id)
