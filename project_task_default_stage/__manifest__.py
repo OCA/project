ae528bfc58462dbedd7a9d9cba7130d85212e884
Borruso <gborruso@dinamicheaziendali.it>
2024-11-27 11:28:02 +0100

    [MIG] project_task_default_stage: Migration to 18.0

--- project_task_default_stage/__manifest__.py
+++ project_task_default_stage/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Project Task Default Stage",
     "summary": "Recovery default task stages for projects from v8",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
--- project_task_default_stage/models/project.py
+++ project_task_default_stage/models/project.py
@@ -8,7 +8,7 @@ class ProjectProject(models.Model):
     _inherit = "project.project"
 
     def _get_default_type_common(self):
-        ids = self.env["project.task.type"].search([("case_default", "=", True)])
-        return ids
+        task_types = self.env["project.task.type"].search([("case_default", "=", True)])
+        return task_types
 
     type_ids = fields.Many2many(default=lambda self: self._get_default_type_common())
