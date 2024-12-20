aae5e91539623e440539edca4f1b5d851aca7245
Bhavesh Heliconia <oca@heliconia.in>
2024-12-20 16:38:57 +0530

    [MIG] project_task_tag: Migration to 18.0

--- project_task_tag/__manifest__.py
+++ project_task_tag/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Task default available tags",
     "summary": "Limit tags available on task",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
