1c43fd15e07020c99f15c492f0d992d3aea7b250
Lansana Barry Sow <lbarry@apsl.net>
2025-02-10 12:20:17 +0100

    [16.0][ADD] project_version: New module project_version

---
+++ project_version/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2025 Lansana Barry Sow(APSL-Nagarro)<lbarry@apsl.net>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Version",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "Lansana Barry Sow, APSL-Nagarro, Odoo Community Association (OCA)",
+    "maintainers": ["lbarry-apsl"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_project_views.xml",
+        "views/project_task_views.xml",
+        "views/project_version_views.xml",
+    ],
+}
