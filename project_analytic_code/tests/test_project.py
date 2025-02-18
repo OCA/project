89773acfd0645aeeddb5c66744088ad6b847b701
Pierre Verkest <pierreverkest84@gmail.com>
2025-02-18 12:50:41 +0100

    [MIG] project_analytic_code: Migration to 17.0

--- project_analytic_code/tests/test_project.py
+++ project_analytic_code/tests/test_project.py
@@ -2,18 +2,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import tagged
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
 @tagged("post_install", "-at_install")
-class TestProject(SavepointCase):
+class TestProject(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.project = cls.env.ref("project.project_project_1")
         cls.project.name = "Test project name"
+        cls.plan = cls.env["account.analytic.plan"].create({"name": "Test plan"})
         cls.project.analytic_account_id = cls.env["account.analytic.account"].create(
-            {"name": "Test Analytic account name", "code": "Test Weird code"}
+            {
+                "name": "Test Analytic account name",
+                "code": "Test Weird code",
+                "plan_id": cls.plan.id,
+            }
         )
 
     def test_search_project_by_account_analytic_code(self):
@@ -23,4 +28,16 @@ class TestProject(SavepointCase):
         self.assertEqual(
             result[0][1],
             f"[{self.project.analytic_account_id.code}] {self.project.name}",
+        )
+
+    def test_project_display_name(self):
+        self.assertEqual(
+            self.project.display_name,
+            f"[{self.project.analytic_account_id.code}] {self.project.name}",
+        )
+        self.project.analytic_account_id = False
+
+        self.assertEqual(
+            self.project.display_name,
+            f"{self.project.name}",
         )
