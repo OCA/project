daf4f87a1fae553b68d5425ce1e3838eafe2dadb
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-02-20 00:45:00 +0100

    [ADD] project_task_description_portal
    Show a dedicated task description for portal users.
    
    Why?
    ----
    Sometimes you need to provide a separate task description for portal
    users. Eg original description contains some confidential information or
    is simply too technical for a client.

---
+++ project_task_description_portal/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Task Description Portal",
+    "summary": "Dedicated task description for portal users",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": [
+        "views/project_task_views.xml",
+        "views/project_portal_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
