afa63a3d8466838180000033dcd9f5bae6488dca
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2025-03-13 13:54:00 -0600

    [FIX] project_timeline: Allow to install this module with project_enterprise This PR fixes an issue added in commit
    https://github.com/OCA/project/commit/3689f1dfccf755c5d862c0e779ab0364215dea3a
    
    When the module project_timeline is installed in a database with the module
    project_enterprise, it raises the following error:
    
    psycopg2.errors.DuplicateColumn: column "planned_date_end" specified more than once
    
    This is because the field planned_date_end is defined in both modules.
    
    This PR change the field name from planned_date_end to planned_date_end_custom in order
    to avoid the conflict.

--- project_timeline/report/project_report.py
+++ project_timeline/report/project_report.py
@@ -8,14 +8,14 @@ class ReportProjectTaskUser(models.Model):
     _inherit = "report.project.task.user"
 
     planned_date_start = fields.Datetime(readonly=True)
-    planned_date_end = fields.Datetime(readonly=True)
+    planned_date_end_custom = fields.Datetime(readonly=True)
 
     def _select(self):
         return (
             super()._select()
             + """,
             t.planned_date_start,
-            t.planned_date_end"""
+            t.planned_date_end AS planned_date_end_custom"""
         )
 
     def _group_by(self):
@@ -23,6 +23,6 @@ class ReportProjectTaskUser(models.Model):
             super()._group_by()
             + """,
             t.planned_date_start,
-            t.planned_date_end
+            t.planned_date_end_custom
             """
         )
