329696507478fa0526eb33165d795ff5fb348dc0
David Bañón <david.banon@tecnativa.com>
2025-04-08 11:38:00 +0200

    [ADD] project_task_parent_due_auto: Updates parent task due date when child due date changes.
    TT55845

---
+++ project_task_parent_due_auto/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Tecnativa David Bañón
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project task parent due date auto",
+    "version": "16.0.1.0.0",
+    "category": "Project Management",
+    "summary": "Recalculates parent task's due date when child task changes",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["david-banon-tecnativa"],
+    "website": "https://github.com/OCA/project",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["project"],
+}
