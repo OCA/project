54036e0663681d5f54431fb8880fe1bda415b9ad
Anusha <anusha.pp@calin.co.in>
2025-04-25 06:36:48 +0200

    [MIG] account_fiscal_year_auto_create: Migration to 18.0

--- project_merge/__manifest__.py
+++ project_merge/__manifest__.py
@@ -1,17 +1,17 @@
-# Copyright 2024 Onestein
+# Copyright 2025 Onestein
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Task Merge",
     "summary": "Wizard to merge project tasks",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
     "license": "AGPL-3",
-    "category": "After-Sales",
+    "category": "Services/Project",
     "depends": ["project"],
     "data": [
         "security/ir.model.access.csv",
-        "wizard/project_task_merge_views.xml",
+        "wizards/project_task_merge_views.xml",
     ],
     "installable": True,
 }
