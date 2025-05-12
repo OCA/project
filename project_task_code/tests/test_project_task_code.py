1e56b13be68ed1462f67b1553e860d9da5377d9a
Enric Tobella <enric.tobella@dixmit.com>
2025-05-12 06:27:47 +0200

    [MIG] project_task_code: Finish Migration

--- project_task_code/tests/test_project_task_code.py
+++ project_task_code/tests/test_project_task_code.py
@@ -1,16 +1,16 @@
 # Copyright 2016 Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import odoo.tests.common as common
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestProjectTaskCode(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.project_task_model = self.env["project.task"]
-        self.ir_sequence_model = self.env["ir.sequence"]
-        self.task_sequence = self.env.ref("project_task_code.sequence_task")
-        self.project_task = self.env.ref("project.project_1_task_1")
+class TestProjectTaskCode(BaseCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project_task_model = cls.env["project.task"]
+        cls.ir_sequence_model = cls.env["ir.sequence"]
+        cls.task_sequence = cls.env.ref("project_task_code.sequence_task")
 
     def test_old_task_code_assign(self):
         project_tasks = self.project_task_model.search([])
@@ -20,24 +20,17 @@ class TestProjectTaskCode(common.TransactionCase):
     def test_new_task_code_assign(self):
         number_next = self.task_sequence.number_next_actual
         code = self.task_sequence.get_next_char(number_next)
-        project_task = self.project_task_model.create(
-            {
-                "name": "Testing task code",
-            }
-        )
+        project_task = self.project_task_model.create({"name": "Testing task code"})
         self.assertNotEqual(project_task.code, "/")
         self.assertEqual(project_task.code, code)
 
     def test_name_get(self):
         number_next = self.task_sequence.number_next_actual
         code = self.task_sequence.get_next_char(number_next)
-        project_task = self.project_task_model.create(
-            {
-                "name": "Task Testing Get Name",
-            }
-        )
+        project_task = self.project_task_model.create({"name": "Task Testing Get Name"})
         result = project_task.display_name
-        self.assertEqual(result, f"[{code}] Task Testing Get Name")
+        # Check by regex, as the display name can be modified in other modules
+        self.assertRegex(result, f"\\[{code}\\]")
 
     def test_name_search(self):
         project_task = self.env["project.task"].create(
