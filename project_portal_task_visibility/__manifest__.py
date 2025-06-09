c317cbe59e3434eee6d0df89ef347b869b31c782
Quirino Leone <quirinoleone95@gmail.com>
2025-06-09 13:01:23 +0200

    [ADD]project_portal_task_visibility: show task only to followers

---
+++ project_portal_task_visibility/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Project Portal Task Visibility",
+    "version": "16.0.1.0.0",
+    "depends": [
+        "project",
+        "portal",
+    ],
+    "website": "https://github.com/OCA/project",
+    "author": "PyTech SRL, Odoo Community Association (OCA)",
+    "category": "Project",
+    "license": "AGPL-3",
+    "data": ["security/project_portal_task_security.xml"],
+    "installable": True,
+    "application": False,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+}
