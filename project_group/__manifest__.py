71f9920738098f1b639afdb20d17fcc795798913
Ruchir Shukla <ruchir@bizzappdev.com>
2025-07-08 14:41:55 +0530

    [MIG] project_group: Migration to 18.0

--- project_group/__manifest__.py
+++ project_group/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Group",
     "summary": """Add groups for filtering on projects""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
