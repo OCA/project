f9e23c1930dae11b425fb3d6a04fb969e37f2001
Adasat Torres de León <adasat.torres.dev@gmail.com>
2025-07-15 19:02:35 +0100

    [REM] project_scrum: This addon exist on project-agile repository.

--- project_scrum/readme/CONTRIBUTORS.rst
+++
@@ -1,4 +0,0 @@
-* `Binhex <https://www.binhex.cloud>_`:
-
-  * Adasat Torres de León <a.torres@binhex.cloud>
-
--- project_scrum/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import test_project_sprint
-from . import test_project_project
-from . import test_project_task
