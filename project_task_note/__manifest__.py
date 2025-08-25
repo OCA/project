99d79eba41daafcfa1c13f4f92c3b17f35df71cc
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-08-25 13:50:46 +0200

    [IMP] project_task_note: update maintainer

--- project_task_note/__manifest__.py
+++ project_task_note/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     "name": "Project task notes",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Project Management",
     "summary": "Add notes in project tasks",
     "author": "Tecnativa, Odoo Community Association (OCA)",
-    "maintainers": ["pilar-vargas"],
+    "maintainers": ["pilarvargas-tecnativa"],
     "website": "https://github.com/OCA/project",
     "license": "AGPL-3",
     "installable": True,
