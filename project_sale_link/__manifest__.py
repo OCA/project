82c4445161a7a12d378cf363e788b0f3b52e5154
Mathieu Benoit <mathben@technolibre.ca>
2025-10-14 23:39:19 -0400

    [ADD] project_sale_link
    - copying project_purchase_link and rename purchase to sale

---
+++ project_sale_link/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Gontzal Gomez - AvanzOSC
+# Copyright 2025 Mathieu Benoit - TechnoLibre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Sale Link",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project", "sale"],
+    "author": "AvanzOSC, " "TechnoLibre, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "category": "Project",
+    "data": ["views/project_project_view.xml"],
+    "installable": True,
+}
---
+++ project_sale_link/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2025 Oihane Crucelaegui - AvanzOSC
+# Copyright 2025 Mathieu Benoit - TechnoLibre
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import project_project
