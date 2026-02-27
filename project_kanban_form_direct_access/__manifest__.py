adf8a188254303bafbad1ea9c9d07adc39985a32
Saran440 <saranl@ecosoft.co.th>
2026-02-27 10:11:29 +0700

    [MIG] project_kanban_form_direct_access: Migration to 18.0

--- project_kanban_form_direct_access/__manifest__.py
+++ project_kanban_form_direct_access/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "depends": ["project"],
