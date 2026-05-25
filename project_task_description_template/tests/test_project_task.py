21dbe37bfa99426f0758377ff90d5a845d616b7c
Eduardo de Miguel <edu@moduon.team>
2026-05-25 12:51:09 +0200

    [FIX] project_task_description_template: Fix tests

--- project_task_description_template/tests/test_project_task.py
+++ project_task_description_template/tests/test_project_task.py
@@ -21,7 +21,7 @@ class TestDescriptionTemplate(BaseCommon):
         record._onchange_description_template_id()
         self.assertEqual(
             record.description,
-            Markup("<p>Existing Description</p><p>- Sample Description</p>"),
+            Markup("<p>Existing Description</p><span>- Sample Description</span>"),
             "Onchange method failed to append description correctly.",
         )
 
@@ -31,6 +31,6 @@ class TestDescriptionTemplate(BaseCommon):
         record._onchange_description_template_id()
         self.assertEqual(
             record.description,
-            Markup("<p>- Sample Description</p>"),
+            Markup("<span>- Sample Description</span>"),
             "Onchange method failed with empty initial description.",
         )
