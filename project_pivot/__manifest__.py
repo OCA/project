2af5376ea83a8c106126f8048559da23b13d3790
Mario Pérez Urbaneja <mariop.urbaneja@netkia.es>
2026-05-25 15:53:30 +0200

    [MIG] project_pivot: Migration to 19.0

--- project_pivot/__manifest__.py
+++ project_pivot/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Pivot view for projects",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Tecnativa, Odoo Community Association (OCA)",
