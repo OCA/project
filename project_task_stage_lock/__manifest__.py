f0e99ce9eebf767e9a3afd243cc0b230336129a1
uncannycs <uncannycs@gmail.com>
2026-06-17 19:01:56 +0530

    [19.0] [MIG] project_task_stage_lock : Migration to 19.0

--- project_task_stage_lock/__manifest__.py
+++ project_task_stage_lock/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Project Task Stage Lock",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "summary": """
     Locks the Stages in the Kanban view of the project task
