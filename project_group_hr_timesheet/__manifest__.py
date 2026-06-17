f9ecf91fc26f5952e643023371498a452aba1cbb
uncannycs <uncannycs@gmail.com>
2026-06-17 16:52:48 +0530

    [19.0] [MIG] project_group_hr_timesheet : Migration to 19.0.

--- project_group_hr_timesheet/__manifest__.py
+++ project_group_hr_timesheet/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Group Hr Timesheet",
     "summary": """This module makes project group work properly with timesheets""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
