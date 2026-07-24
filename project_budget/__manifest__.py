908006d0dc2668bc422ed3e9690ab5bf502cf7c7
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-07-24 18:00:48 +0530

    [19.0][MIG]project_budget: Migration to 19.0

--- project_budget/__manifest__.py
+++ project_budget/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Patrick Wilson,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "project",
