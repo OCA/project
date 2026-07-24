447915bacfec02d8a692a82d40a092e0b6fc2325
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-07-24 18:06:52 +0530

    [19.0][MIG]project_kanban_form_direct_access: Migration to 19.0

--- project_kanban_form_direct_access/__manifest__.py
+++ project_kanban_form_direct_access/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "depends": ["project"],
