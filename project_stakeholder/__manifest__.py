b80241dc1d22cc6f28bf794ad9653026ee777363
ManfredNelvin <manfrednelvin@gmail.com>
2026-07-30 15:00:06 +0530

    [MIG] project_stakeholder: Migration to 19.0

--- project_stakeholder/__manifest__.py
+++ project_stakeholder/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Project Stakeholder",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Camptocamp, Odoo Community Association (OCA)",
